e7814ef6110b8016d4ff4b5c66c9b92917d37493
manasi <manasi@bd4681a2-3e9a-0410-b5d1-a8caa14fa32b>
2011-07-21 20:39:49 +0000

    removed pre-computed surface normal derivatives from itkParticleImageDomainWithGradients.h
    git-svn-id: https://www.nitrc.org/svn/shapeworks@18 bd4681a2-3e9a-0410-b5d1-a8caa14fa32b

--- code/ITKParticleSystem/itkParticleImageDomainWithGradients.h
+++ code/ITKParticleSystem/itkParticleImageDomainWithGradients.h
@@ -20,7 +20,6 @@
 #include "itkParticleImageDomain.h"
 #include "itkVectorLinearInterpolateImageFunction.h"
 #include "itkGradientImageFilter.h"
-#include "itkDerivativeImageFilter.h"
 #include "itkFixedArray.h"
 
 namespace itk
@@ -55,7 +54,6 @@ public:
   typedef typename GradientImageFilterType::OutputImageType GradientImageType;
   typedef VectorLinearInterpolateImageFunction<GradientImageType, typename PointType::CoordRepType>
   GradientInterpolatorType;
-  typedef DerivativeImageFilter< ImageType, ImageType > DerivativeFilterType;
 
   typedef FixedArray<T, 3> VectorType;
   typedef vnl_vector_fixed<T, 3> VnlVectorType;
@@ -83,9 +81,6 @@ public:
     m_GradientImage = filter->GetOutput();
     
     m_GradientInterpolator->SetInputImage(m_GradientImage);
-
-    ComputeNormalMaps();
-    ComputeNormalPartialDerivatives();
   }
   itkGetObjectMacro(GradientImage, GradientImageType);
 
@@ -132,148 +127,10 @@ public:
     else return false;
   }
   
-  inline void ComputeNormalMaps()
-  {
-    m_NormalImageX = ImageType::New();
-    m_NormalImageY = ImageType::New();
-    m_NormalImageZ = ImageType::New();
-
-    typename itk::ImageDuplicator<ImageType>::Pointer imgDuplicator = itk::ImageDuplicator<ImageType>::New();
-    imgDuplicator->SetInputImage(Superclass::GetImage());
-    imgDuplicator->Update();
-
-    m_NormalImageX = imgDuplicator->GetOutput();
-    m_NormalImageY = imgDuplicator->GetOutput();
-    m_NormalImageZ = imgDuplicator->GetOutput();
-
-    typedef itk::ImageRegionIterator< ImageType > IteratorType;
-    IteratorType itX(m_NormalImageX,m_NormalImageX->GetRequestedRegion());
-    IteratorType itY(m_NormalImageY,m_NormalImageY->GetRequestedRegion());
-    IteratorType itZ(m_NormalImageZ,m_NormalImageZ->GetRequestedRegion());
-    PointType it_point;
-    VnlVectorType vecNormal;
-
-    for (itX = itX.Begin(); !itX.IsAtEnd(); ++itX,++itY,++itZ)
-    {
-      m_NormalImageX->TransformIndexToPhysicalPoint(itX.GetIndex(),it_point);
-      vecNormal = SampleNormalVnl(it_point);
-      itX.Set(vecNormal[0]);
-      itY.Set(vecNormal[1]);
-      itZ.Set(vecNormal[2]);
-    }    
-  }
-
-  inline void ComputeNormalPartialDerivatives()
-  {
-    pdFilter_nx_x = DerivativeFilterType::New();
-    pdFilter_nx_y = DerivativeFilterType::New();
-    pdFilter_nx_z = DerivativeFilterType::New();
-                    
-    pdFilter_ny_x = DerivativeFilterType::New();
-    pdFilter_ny_y = DerivativeFilterType::New();
-    pdFilter_ny_z = DerivativeFilterType::New();
-                    
-    pdFilter_nz_x = DerivativeFilterType::New();
-    pdFilter_nz_y = DerivativeFilterType::New();
-    pdFilter_nz_z = DerivativeFilterType::New();
-
-    pdFilter_nx_x->SetInput(m_NormalImageX);
-    pdFilter_nx_y->SetInput(m_NormalImageX);
-    pdFilter_nx_z->SetInput(m_NormalImageX);
-                 
-    pdFilter_ny_x->SetInput(m_NormalImageY);
-    pdFilter_ny_y->SetInput(m_NormalImageY);
-    pdFilter_ny_z->SetInput(m_NormalImageY);
-                 
-    pdFilter_nz_x->SetInput(m_NormalImageZ);
-    pdFilter_nz_y->SetInput(m_NormalImageZ);
-    pdFilter_nz_z->SetInput(m_NormalImageZ);
-
-    pdFilter_nx_x->SetDirection(0);
-    pdFilter_nx_y->SetDirection(1);
-    pdFilter_nx_z->SetDirection(2);
-                 
-    pdFilter_ny_x->SetDirection(0);
-    pdFilter_ny_y->SetDirection(1);
-    pdFilter_ny_z->SetDirection(2);
-                 
-    pdFilter_nz_x->SetDirection(0);
-    pdFilter_nz_y->SetDirection(1);
-    pdFilter_nz_z->SetDirection(2);
-
-    pdFilter_nx_x->Update();
-    pdFilter_nx_y->Update();
-    pdFilter_nx_z->Update();
-                 
-    pdFilter_ny_x->Update();
-    pdFilter_ny_y->Update();
-    pdFilter_ny_z->Update();
-                 
-    pdFilter_nz_x->Update();
-    pdFilter_nz_y->Update();
-    pdFilter_nz_z->Update();
-  }
-
-  inline float SampleNormalPartialDerivative(unsigned int nDir, unsigned int dDir, const PointType &pt) const
-  {
-    switch (nDir)
-    {
-    case 1:
-      switch (dDir)
-      {
-      case 1:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_ny_y->GetOutput());
-        break;
-      case 2:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_ny_z->GetOutput());
-        break;
-      case 0:
-      default:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_ny_x->GetOutput());
-        break;
-      }
-      break;
-    case 2:
-      switch (dDir)
-      {
-      case 1:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_nz_y->GetOutput());
-        break;
-      case 2:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_nz_z->GetOutput());
-        break;
-      case 0:
-      default:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_nz_x->GetOutput());
-        break;
-      }
-      break;
-    case 0:
-    default:
-      switch (dDir)
-      {
-      case 1:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_nx_y->GetOutput());
-        break;
-      case 2:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_nx_z->GetOutput());
-        break;
-      case 0:
-      default:
-        dirDerivativeInterpolator->SetInputImage(pdFilter_nx_x->GetOutput());
-        break;
-      }
-      break;
-    }
-
-    return dirDerivativeInterpolator->Evaluate(pt);
-  }
-
 protected:
   ParticleImageDomainWithGradients()
   {
     m_GradientInterpolator = GradientInterpolatorType::New();
-    dirDerivativeInterpolator = ScalarInterpolatorType::New();
   }
 
   void PrintSelf(std::ostream& os, Indent indent) const
@@ -290,25 +147,6 @@ private:
 
   typename GradientImageType::Pointer m_GradientImage;
   typename GradientInterpolatorType::Pointer m_GradientInterpolator;
-
-  typename ImageType::Pointer m_NormalImageX;
-  typename ImageType::Pointer m_NormalImageY;
-  typename ImageType::Pointer m_NormalImageZ;
-  
-  typename DerivativeFilterType::Pointer  pdFilter_nx_x;
-  typename DerivativeFilterType::Pointer  pdFilter_nx_y;
-  typename DerivativeFilterType::Pointer  pdFilter_nx_z;
-
-  typename DerivativeFilterType::Pointer  pdFilter_ny_x;
-  typename DerivativeFilterType::Pointer  pdFilter_ny_y;
-  typename DerivativeFilterType::Pointer  pdFilter_ny_z;
-
-  typename DerivativeFilterType::Pointer  pdFilter_nz_x;
-  typename DerivativeFilterType::Pointer  pdFilter_nz_y;
-  typename DerivativeFilterType::Pointer  pdFilter_nz_z;
-
-
-  typename ScalarInterpolatorType::Pointer dirDerivativeInterpolator;
 };
 
 } // end namespace itk
