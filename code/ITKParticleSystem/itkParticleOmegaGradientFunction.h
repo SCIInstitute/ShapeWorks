aecaeced6d1f7140378b18b6b11e99650f3b5b36
Alan Morris <akenmorris@gmail.com>
2013-05-10 14:10:10 -0600

    Removed cutting plane, adjusted spheres.
    Temporarily removed cutting plane code.  Adjusted sphere weight down
    to 0.01 from 1.0.

--- code/ITKParticleSystem/itkParticleOmegaGradientFunction.h
+++ code/ITKParticleSystem/itkParticleOmegaGradientFunction.h
@@ -1,17 +1,17 @@
 /*=========================================================================
-  Program:   ShapeWorks: Particle-based Shape Correspondence & Visualization
-  Module:    $RCSfile: itkParticleOmegaGradientFunction.h,v $
-  Date:      $Date: 2011/03/24 01:17:33 $
-  Version:   $Revision: 1.2 $
-  Author:    $Author: wmartin $
+   Program:   ShapeWorks: Particle-based Shape Correspondence & Visualization
+   Module:    $RCSfile: itkParticleOmegaGradientFunction.h,v $
+   Date:      $Date: 2011/03/24 01:17:33 $
+   Version:   $Revision: 1.2 $
+   Author:    $Author: wmartin $
 
-  Copyright (c) 2009 Scientific Computing and Imaging Institute.
-  See ShapeWorksLicense.txt for details.
+   Copyright (c) 2009 Scientific Computing and Imaging Institute.
+   See ShapeWorksLicense.txt for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notices for more information.
-=========================================================================*/
+   =========================================================================*/
 #ifndef __itkParticleOmegaGradientFunction_h
 #define __itkParticleOmegaGradientFunction_h
 
@@ -39,12 +39,12 @@ class ParticleOmegaGradientFunction
   : public ParticleEntropyGradientFunction<TGradientNumericType, VDimension>
 {
 public:
- /** Standard class typedefs. */
+  /** Standard class typedefs. */
   typedef ParticleOmegaGradientFunction Self;
   typedef SmartPointer<Self>  Pointer;
   typedef SmartPointer<const Self>  ConstPointer;
   typedef ParticleEntropyGradientFunction<TGradientNumericType, VDimension> Superclass;
-  itkTypeMacro( ParticleOmegaGradientFunction, ParticleEntropyGradientFunction);
+  itkTypeMacro( ParticleOmegaGradientFunction, ParticleEntropyGradientFunction );
 
   /** Inherit some parent typedefs. */
   typedef typename Superclass::GradientNumericType GradientNumericType;
@@ -52,42 +52,42 @@ public:
   typedef typename Superclass::VectorType VectorType;
   typedef typename Superclass::PointType PointType;
   typedef typename Superclass::GradientVectorType GradientVectorType;
-  
+
   typedef ParticleMeanCurvatureAttribute<TGradientNumericType, VDimension>
-  MeanCurvatureCacheType;
+    MeanCurvatureCacheType;
 
   typedef typename ParticleImageDomainWithCurvature<TGradientNumericType, VDimension>
-  ::VnlMatrixType VnlMatrixType;
+    ::VnlMatrixType VnlMatrixType;
 
   /** Method for creation through the object factory. */
-  itkNewMacro(Self);
+  itkNewMacro( Self );
 
   /** Dimensionality of the domain of the particle system. */
-  itkStaticConstMacro(Dimension, unsigned int, VDimension);
+  itkStaticConstMacro( Dimension, unsigned int, VDimension );
 
   /** The first argument is a pointer to the particle system.  The second
       argument is the index of the domain within that particle system.  The
       third argument is the index of the particle location within the given
       domain. */
-  inline virtual VectorType Evaluate(unsigned int a, unsigned int b, const ParticleSystemType *c,
-                              double& d) const 
+  inline virtual VectorType Evaluate( unsigned int a, unsigned int b, const ParticleSystemType* c,
+                                      double& d ) const
   {
     double e;
-    return this->Evaluate(a, b, c, d, e);
+    return this->Evaluate( a, b, c, d, e );
   }
-  virtual VectorType Evaluate(unsigned int, unsigned int, const ParticleSystemType *,
-                              double&, double & ) const;
+  virtual VectorType Evaluate( unsigned int, unsigned int, const ParticleSystemType*,
+                               double&, double & ) const;
 
-  virtual void BeforeEvaluate(unsigned int, unsigned int, const ParticleSystemType *);
+  virtual void BeforeEvaluate( unsigned int, unsigned int, const ParticleSystemType* );
 
-  inline virtual double Energy(unsigned int a, unsigned int b, const ParticleSystemType *c) const
+  inline virtual double Energy( unsigned int a, unsigned int b, const ParticleSystemType* c ) const
   {
     double d, e;
-    this->Evaluate(a, b, c, d, e);
-    return e;  
+    this->Evaluate( a, b, c, d, e );
+    return e;
   }
-  
-  inline double ComputeKappa(double mc, unsigned int d, double planeDist) const
+
+  inline double ComputeKappa( double mc, unsigned int d, double planeDist ) const
   {
 
     // NOTE: Should rethink this to scale nonlinearly with increasing distance
@@ -95,22 +95,21 @@ public:
     // over/undersampling regions that are statistical outliers wrt curvature.
     // --jc
 
-    
     //  double myKappa =  (1.0 + m_Rho * m_MeanCurvatureCache->operator[](
     //  this->GetDomainNumber())->operator[](idx) * (m_SamplesPerCurvature /
     //  twopi)) /
     //  ( m_SamplesPerCurvature * beta);
-    double maxmc = m_MeanCurvatureCache->GetMeanCurvature(d)
-      + 2.0 * m_MeanCurvatureCache->GetCurvatureStandardDeviation(d);
-    double minmc = m_MeanCurvatureCache->GetMeanCurvature(d)
-      - 2.0 * m_MeanCurvatureCache->GetCurvatureStandardDeviation(d);
+    double maxmc = m_MeanCurvatureCache->GetMeanCurvature( d )
+                   + 2.0 * m_MeanCurvatureCache->GetCurvatureStandardDeviation( d );
+    double minmc = m_MeanCurvatureCache->GetMeanCurvature( d )
+                   - 2.0 * m_MeanCurvatureCache->GetCurvatureStandardDeviation( d );
 
-    double kappa = 1.0 +  m_Rho * (mc - minmc) / (maxmc - minmc);
+    double kappa = 1.0 + m_Rho * ( mc - minmc ) / ( maxmc - minmc );
 
 //    double MAG =  m_Rho;
 //    double D   = 40;
 //    if (fabs(planeDist) < D)
-//      {      
+//      {
 //      kappa = MAG + ((1.0 - MAG) / D) * fabs(planeDist);
 //      }
 //    else
@@ -118,11 +117,11 @@ public:
 //      kappa = 1.0;
 //      }
 
-    return kappa;    
+    return kappa;
   }
-  
+
   /** */
-  virtual void AfterIteration()  {  }
+  virtual void AfterIteration() {  }
 
   /** */
   virtual void BeforeIteration()
@@ -135,21 +134,21 @@ public:
   virtual double EstimateSigma( unsigned int, unsigned int,
                                 const typename ParticleSystemType::PointVectorType &,
                                 const std::vector<double> &,
-                                const PointType &, double,  double,
-                                int &err, double &,unsigned int) const;
+                                const PointType &, double, double,
+                                int &err, double &, unsigned int ) const;
 
   /** */
   //  void ComputeKappaValues();
-  
+
   /** Access the cache of curvature-based weight values for each particle
       position. */
-  void SetMeanCurvatureCache( MeanCurvatureCacheType *s)
+  void SetMeanCurvatureCache( MeanCurvatureCacheType* s )
   {    m_MeanCurvatureCache = s;  }
-  MeanCurvatureCacheType *GetMeanCurvatureCache()
-  {   return  m_MeanCurvatureCache.GetPointer();  }
-  const MeanCurvatureCacheType *GetMeanCurvatureCache() const
-  {   return  m_MeanCurvatureCache.GetPointer();  }
- 
+  MeanCurvatureCacheType* GetMeanCurvatureCache()
+  {   return m_MeanCurvatureCache.GetPointer();  }
+  const MeanCurvatureCacheType* GetMeanCurvatureCache() const
+  {   return m_MeanCurvatureCache.GetPointer();  }
+
   /** Set/Get the parameters in the kappa function. */
   //  void SetGamma(double g)
   //  { m_Gamma = g; }
@@ -159,8 +158,8 @@ public:
 //   { m_CurvatureScale = g; }
 //  void SetSamplesPerCurvature(double g)
 //  { m_SamplesPerCurvature = g; }
-  void SetRho(double g)
-  { m_Rho= g; }
+  void SetRho( double g )
+  { m_Rho = g; }
   //  double GetGamma() const
   //  { return m_Gamma; }
   //  double GetBeta() const
@@ -173,11 +172,11 @@ public:
   { return m_Rho; }
 
 protected:
-  ParticleOmegaGradientFunction() :  m_Counter(0),
-                                               m_Rho(1.0) {}
+  ParticleOmegaGradientFunction() :  m_Counter( 0 ),
+    m_Rho( 1.0 ) {}
   virtual ~ParticleOmegaGradientFunction() {}
-  void operator=(const ParticleOmegaGradientFunction &);
-  ParticleOmegaGradientFunction(const ParticleOmegaGradientFunction &);
+  void operator=( const ParticleOmegaGradientFunction & );
+  ParticleOmegaGradientFunction( const ParticleOmegaGradientFunction & );
   typename MeanCurvatureCacheType::Pointer m_MeanCurvatureCache;
   //  double m_Gamma;
   //  double m_Beta;
@@ -186,15 +185,12 @@ protected:
   unsigned int m_Counter;
   double m_Rho;
   double m_avgKappa;
-  
+
   double m_CurrentSigma;
   typename ParticleSystemType::PointVectorType m_CurrentNeighborhood;
 
   std::vector<double> m_CurrentWeights;
-    
-  
 };
-
 } //end namespace
 
 #if ITK_TEMPLATE_EXPLICIT
@@ -205,4 +201,4 @@ protected:
 # include "itkParticleOmegaGradientFunction.txx"
 #endif
 
-#endif
+#endif // ifndef __itkParticleOmegaGradientFunction_h
