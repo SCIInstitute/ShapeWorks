7bfa8abc617df1d48a5d4aa2c5899bdeac41be52
Alan Morris <akenmorris@gmail.com>
2014-02-20 17:08:00 -0700

    Prototyping.

---
+++ src/Visualization/Viewer.h
@@ -0,0 +1,27 @@
+#include <vtkSmartPointer.h>
+
+
+class vtkRenderer;
+class CustomImagePlaneWidget;
+class vtkRenderWindowInteractor;
+class vtkImageData;
+
+class Viewer
+{
+public:
+  Viewer();
+  ~Viewer();
+
+  vtkRenderer *getRenderer();
+  void setInteractor(vtkRenderWindowInteractor *interactor);
+
+  void addInput(vtkImageData *imageData);
+
+
+private:
+
+  bool initialized;
+  vtkSmartPointer<vtkRenderer> renderer;
+
+  std::vector<vtkImageData*> inputs;
+};
