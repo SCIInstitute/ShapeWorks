7a23a652c21c28c22e40564e8e6bed69d313a39d
Cameron Christensen <cam@sci.utah.edu>
2019-07-30 23:47:51 -0600

    rewrite superbuild-linux script

--- superbuild-linux.sh
+++ superbuild-linux.sh
@@ -1,12 +1,29 @@
+#!/bin/bash
+
 echo "ShapeWorks superbuild for Linux"
 
 # Call this script by specifying arguments in the same command.
 # Ex:
-#   BUILD_VIEW2=0 NUM_PROCS=16 ./superbuild-linux.sh
+#   NUM_PROCS=16 ./superbuild-linux.sh
+#
+# Arguments:
+#  BUILD_CLEAN:   whether or not to remove all build directories and clone new dependencies
+#
+#  VXL_DIR:       if you already have VXL its install path can be specified
+#  VTK_DIR:       if you already have VTK its install path can be specified
+#  ITK_DIR:       if you already have ITK its install path can be specified
+#
+#  BUILD_DIR:     by default creates a subdirectory called build in the current directory
+#  INSTALL_DIR:   by default installs ShapeWorks and all external dependencies in a subdirectory called install
+#  NUM_PROCS:     number of processors to use for parallel builds (default is 4)
+#
+#  BUILD_POST:    whether or not to build any applications in Post (default is to build them all)
 #
 
+
 # helper functions
-. ./superbuild-utils.sh
+SRC=$(dirname "$(readlink -f "$0")")
+. ${SRC}/superbuild-utils.sh
 
 # force rebuild of everything
 if [ -z $BUILD_CLEAN ]; then
@@ -29,17 +46,12 @@ if [ -z $BUILD_POST ]; then
   BUILD_POST=1
 fi
 
-# vtk will be built with QT only if this is ON
-if [ -z $BUILD_VIEW2 ]; then
-  BUILD_VIEW2=0
-fi
-
-if [ $BUILD_VIEW2 != 0 ]; then
-  if ! [ -x "$(command -v qmake)" ]; then
-    echo 'Error: Please make sure Qt4 is installed and that qmake is in the path.' >&2
-    echo 'Please Download Qt4 from: https://download.qt.io/archive/qt/4.8/4.8.7/' >&2
-    exit 1
-  fi
+# Look for Qt in order to build GUI applications
+HAVE_QT=1
+if ! [ -x "$(command -v qmake)" ]; then
+  echo 'Qt not found. For GUI applications, please make sure Qt4 is installed and that qmake is in the path.' >&2
+  echo 'Download Qt4 from: https://download.qt.io/archive/qt/4.8/4.8.7/' >&2
+  HAVE_QT=0
 fi
 
 # builds faster if more cores are available
@@ -47,67 +59,42 @@ if [ -z $NUM_PROCS ]; then
   NUM_PROCS=4
 fi
 
-# Anaconda is highly recommended in order to build multiple versions of various software. See INSTALL.md for details.
-if [ -z $USING_CONDA_SANDBOX ]; then
-  USING_CONDA_SANDBOX=0
+## create build and install directories
+if [ -z $BUILD_DIR ]; then
+  BUILD_DIR=$(pwd)/build
 fi
+if [ -z $INSTALL_DIR ]; then
+  INSTALL_DIR=$(pwd)/install
+fi
+mkdir ${BUILD_DIR}
+mkdir ${INSTALL_DIR}
 
-## which dependencies should be built ##
-if [ -z $Build_VXL ]; then
-  Build_VXL=1
-fi
-if [ -z $Build_VTK ]; then
-  Build_VTK=1
-fi
-if [ -z $Build_ITK ]; then
-  Build_ITK=1
-fi
-basePath=$(pwd)
-depDir=${root}/shapeworks-dependencies
-## create build and install directories for ShapeWorks deps ##
-if [ $USING_CONDA_SANDBOX ]; then
-  root=${CONDA_PREFIX}
-  INSTALL_PREFIX="${CONDA_PREFIX}"
-else
-  root=$(pwd)
-  INSTALL_PREFIX="${depDir}/install"
-fi
-
-if [ $BUILD_CLEAN != 0 ]; then rm -rf ${depDir}; fi
-mkdir ${depDir}
-mkdir ${depDir}/install
-
-######## VXL ########
-if [ $Build_VXL -eq 1 ]
-then
+## build dependencies if their locations were not specified
+if [ -z $VXL_DIR ]; then
   VXL_VER="github-migration"
-  VXL_BUILD_DIR=${depDir}/vxl-build-${VXL_VER}
-  cd ${depDir}
+  VXL_DIR=${BUILD_DIR}/vxl
+  cd ${BUILD_DIR}
   git clone https://github.com/vxl/vxl.git
-  cd vxl/
+  cd ${VXL_DIR}
   git checkout -f tags/${VXL_VER}
   sed -i '1,10 s/^/#/' CMakeLists.txt
   sed -i 's/JPEG_FOUND/JPEG_FOUND_DISABLE/' core/vil/CMakeLists.txt
   sed -i 's/JPEG_FOUND/JPEG_FOUND_DISABLE/' core/vil1/CMakeLists.txt
-  cd ${depDir}
-
-  if [ $BUILD_CLEAN != 0 ]; then rm -rf ${VXL_BUILD_DIR}; fi
-  mkdir ${VXL_BUILD_DIR}
-  cd ${VXL_BUILD_DIR}
-  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_CORE_VIDEO:BOOL=OFF -DBUILD_BRL:BOOL=OFF -DBUILD_CONTRIB:BOOL=OFF -DVNL_CONFIG_LEGACY_METHODS=ON -DVCL_STATIC_CONST_INIT_FLOAT=0 -Wno-dev ../vxl/
+  
+  if [ $BUILD_CLEAN != 0 ]; then rm -rf build; fi
+  mkdir build
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_CORE_VIDEO:BOOL=OFF -DBUILD_BRL:BOOL=OFF -DBUILD_CONTRIB:BOOL=OFF -DVNL_CONFIG_LEGACY_METHODS=ON -DVCL_STATIC_CONST_INIT_FLOAT=0 -Wno-dev ${VXL_DIR}
   make -j${NUM_PROCS} install
 fi
 
-######## VTK ########
-if [ $Build_VTK -eq 1 ]
-then
+if [ -z $VTK_DIR ]; then
   VTK_VER="v5.10.1"
-  VTK_BUILD_DIR=${depDir}/vtk-build-${VTK_VER}
-  cd ${depDir}
+  VTK_DIR=${BUILD_DIR}/vtk
+  cd ${BUILD_DIR}
   git clone https://gitlab.kitware.com/vtk/vtk.git
-  cd vtk/
+  cd ${VTK_DIR}
   git checkout -f tags/${VTK_VER}
-  cd ${depDir}
 
   ## fix for deprecated sstream in vtkIOStream.h, needed for gcc 4.8.5 in Red Hat Linux
   sed -i "108,112 s/^/\/\//" vtk/Common/vtkIOStream.h
@@ -116,54 +103,40 @@ then
   sed -i "115i using std::basic_ostringstream;" vtk/Common/vtkIOStream.h
   sed -i "116i using std::basic_stringstream;" vtk/Common/vtkIOStream.h
 
-  if [ $BUILD_CLEAN != 0 ]; then rm -rf ${VTK_BUILD_DIR}; fi
-  mkdir ${VTK_BUILD_DIR}
-  cd ${VTK_BUILD_DIR}
-  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DVTK_USE_QT:BOOL=${BUILD_VIEW2} -DCMAKE_C_FLAGS=-DGLX_GLXEXT_LEGACY -DCMAKE_CXX_FLAGS=-DGLX_GLXEXT_LEGACY -Wno-dev ../vtk/
+  if [ $BUILD_CLEAN != 0 ]; then rm -rf build; fi
+  mkdir build
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DVTK_USE_QT:BOOL=${HAVE_QT} -DCMAKE_C_FLAGS=-DGLX_GLXEXT_LEGACY -DCMAKE_CXX_FLAGS=-DGLX_GLXEXT_LEGACY -Wno-dev ${VTK_DIR}
   make -j${NUM_PROCS} install
 fi
 
-######## ITK ########
-if [ $Build_ITK -eq 1 ]
-then
+if [ -z $ITK_DIR ]; then
   ITK_VER="v4.7.2"
-  ITK_BUILD_DIR=${depDir}/itk-build-${ITK_VER}
-  cd ${depDir}
+  ITK_DIR=${BUILD_DIR}/ITK
+  cd ${BUILD_DIR}
   git clone https://github.com/InsightSoftwareConsortium/ITK.git
-  cd ITK/
+  cd ${ITK_DIR}
   git checkout -f tags/${ITK_VER}
-  cd ${depDir}
 
-  if [ $BUILD_CLEAN != 0 ]; then rm -rf ${ITK_BUILD_DIR}; fi
-  mkdir ${ITK_BUILD_DIR}
-  cd ${ITK_BUILD_DIR}
-  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DITK_USE_SYSTEM_VXL=on -Wno-dev ../ITK/
+  if [ $BUILD_CLEAN != 0 ]; then rm -rf build; fi
+  mkdir build
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DITK_USE_SYSTEM_VXL=on -Wno-dev ${ITK_DIR}
   make -j${NUM_PROCS} install
 fi
 
-######## ShapeWorks ########
-# we make an assumption here that you are executing the superbuild from the base SW source directory
-cd ${basePath}
+## Build ShapeWorks
+cd ${BUILD_DIR}
 if [ $BUILD_CLEAN != 0 ]; then rm -rf shapeworks-build; fi
 mkdir shapeworks-build/
 cd shapeworks-build/
 
-if ! [ $USING_CONDA_SANDBOX ]; then
-  ITK_DIR="-DITK_DIR=${ITK_BUILD_DIR}"
-  VTK_DIR="-DVTK_DIR=${VTK_BUILD_DIR}"
-  VXL_DIR="-DVXL_DIR=${VXL_BUILD_DIR}"
-fi
-
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX} ${ITK_DIR} ${VXL_DIR} ${VTK_DIR} -DBuild_Post:BOOL=${BUILD_POST} -DBuild_View2:BOOL=${BUILD_VIEW2} -Wno-dev -Wno-deprecated ../
+cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DITK_DIR=${INSTALL_DIR} -DVXL_DIR=${INSTALL_DIR} -DVTK_DIR=${INSTALL_DIR} -DBuild_Post:BOOL=${BUILD_POST} -DBuild_View2:BOOL=${HAVE_QT} -Wno-dev -Wno-deprecated ${SRC}
 make -j${NUM_PROCS} install
 
-cd ${root}
-
-if [ $USING_CONDA_SANDBOX ]; then
-  # Inform users of ShapeWorks install path:
-  MESSAGE("ShapeWorks executables have been installed in this directory. Run them using ./<executable_name>")
-  MESSAGE("[IMPORTANT] first you must ensure shared libraries can be found by the executables:")
-MESSAGE("  export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$CONDA_PREFIX/lib/vtk-5.10:$LD_LIBRARY_PATH")
-  MESSAGE("To add the executables to your default PATH, use this:") # Because non-standard install location is not in PATH
-  MESSAGE("  export PATH=$CONDA_PREFIX:$PATH")
-fi
+# Inform users of ShapeWorks install path:
+echo "-----------------------------------------"
+echo "ShapeWorks has been installed in ${INSTALL_DIR}."
+echo "Set LD_LIBRARY_PATH for shared libraries to be found:"
+echo "  export LD_LIBRARY_PATH=`pwd`/install/lib:`pwd`/install/lib/vtk-5.10$LD_LIBRARY_PATH"
+echo "-----------------------------------------"
