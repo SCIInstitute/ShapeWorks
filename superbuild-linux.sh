99bba182ea81e8bf780c03f73381c18918c61be7
Cameron Christensen <cam@sci.utah.edu>
2019-08-01 22:05:17 -0600

    even more cleanup and standardization of superbuild-linux.sh

--- superbuild-linux.sh
+++ superbuild-linux.sh
@@ -26,23 +26,23 @@ echo "ShapeWorks superbuild for Linux"
 SRC=$(dirname "$(readlink -f "$0")")
 . ${SRC}/superbuild-utils.sh
 
-if [ -z $BUILD_CLEAN ]; then BUILD_CLEAN=0; fi
-if [ -z $HAVE_QT ];     then HAVE_QT=0;     fi
-if [ -z $BUILD_POST ];  then BUILD_POST=1;  fi
-if [ -z $NUM_PROCS ];   then NUM_PROCS=4;   fi
-if [ $HAVE_QT == 1 ]; then
+if [[ -z $BUILD_CLEAN ]]; then BUILD_CLEAN=0; fi
+if [[ -z $HAVE_QT ]];     then HAVE_QT=0;     fi
+if [[ -z $BUILD_POST ]];  then BUILD_POST=1;  fi
+if [[ -z $NUM_PROCS ]];   then NUM_PROCS=4;   fi
+if [[ $HAVE_QT = 1 ]]; then
   echo 'For GUI applications, please make sure Qt4 is installed and that qmake is in the path.' >&2
   echo 'Download Qt4 from: https://download.qt.io/archive/qt/4.8/4.8.7/' >&2
 fi
 
 ## create build and install directories
-if [ -z $BUILD_DIR ];   then BUILD_DIR=$(pwd)/build;     fi
-if [ -z $INSTALL_DIR ]; then INSTALL_DIR=$(pwd)/install; fi
+if [[ -z $BUILD_DIR ]];   then BUILD_DIR=$(pwd)/build;     fi
+if [[ -z $INSTALL_DIR ]]; then INSTALL_DIR=$(pwd)/install; fi
 mkdir -p ${BUILD_DIR}
 mkdir -p ${INSTALL_DIR}
 
 ## build dependencies if their locations were not specified
-if [ -z $VXL_DIR ]; then
+if [[ -z $VXL_DIR ]]; then
   VXL_VER="github-migration"
   VXL_DIR=${BUILD_DIR}/vxl
   cd ${BUILD_DIR}
@@ -53,13 +53,13 @@ if [ -z $VXL_DIR ]; then
   sed -i 's/JPEG_FOUND/JPEG_FOUND_DISABLE/' core/vil/CMakeLists.txt
   sed -i 's/JPEG_FOUND/JPEG_FOUND_DISABLE/' core/vil1/CMakeLists.txt
   
-  if [ $BUILD_CLEAN != 0 ]; then rm -rf build; fi
+  if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_CORE_VIDEO:BOOL=OFF -DBUILD_BRL:BOOL=OFF -DBUILD_CONTRIB:BOOL=OFF -DVNL_CONFIG_LEGACY_METHODS=ON -DVCL_STATIC_CONST_INIT_FLOAT=0 -Wno-dev ${VXL_DIR}
   make -j${NUM_PROCS} install
 fi
 
-if [ -z $VTK_DIR ]; then
+if [[ -z $VTK_DIR ]]; then
   VTK_VER="v5.10.1"
   VTK_DIR=${BUILD_DIR}/vtk
   cd ${BUILD_DIR}
@@ -74,13 +74,13 @@ if [ -z $VTK_DIR ]; then
   sed -i "115i using std::basic_ostringstream;" vtk/Common/vtkIOStream.h
   sed -i "116i using std::basic_stringstream;" vtk/Common/vtkIOStream.h
 
-  if [ $BUILD_CLEAN != 0 ]; then rm -rf build; fi
+  if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DVTK_USE_QT:BOOL=${HAVE_QT} -DCMAKE_C_FLAGS=-DGLX_GLXEXT_LEGACY -DCMAKE_CXX_FLAGS=-DGLX_GLXEXT_LEGACY -Wno-dev ${VTK_DIR}
   make -j${NUM_PROCS} install
 fi
 
-if [ -z $ITK_DIR ]; then
+if [[ -z $ITK_DIR ]]; then
   ITK_VER="v4.7.2"
   ITK_DIR=${BUILD_DIR}/ITK
   cd ${BUILD_DIR}
@@ -88,7 +88,7 @@ if [ -z $ITK_DIR ]; then
   cd ${ITK_DIR}
   git checkout -f tags/${ITK_VER}
 
-  if [ $BUILD_CLEAN != 0 ]; then rm -rf build; fi
+  if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DITK_USE_SYSTEM_VXL=on -Wno-dev ${ITK_DIR}
   make -j${NUM_PROCS} install
@@ -96,7 +96,7 @@ fi
 
 ## Build ShapeWorks
 cd ${BUILD_DIR}
-if [ $BUILD_CLEAN != 0 ]; then rm -rf shapeworks-build; fi
+if [[ $BUILD_CLEAN = 1 ]]; then rm -rf shapeworks-build; fi
 mkdir -p shapeworks-build && cd shapeworks-build
 cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DITK_DIR=${INSTALL_DIR} -DVXL_DIR=${INSTALL_DIR} -DVTK_DIR=${INSTALL_DIR} -DBuild_Post:BOOL=${BUILD_POST} -DBuild_View2:BOOL=${HAVE_QT} -Wno-dev -Wno-deprecated ${SRC}
 make -j${NUM_PROCS} install
@@ -105,5 +105,5 @@ make -j${NUM_PROCS} install
 echo "-----------------------------------------"
 echo "ShapeWorks has been installed in ${INSTALL_DIR}."
 echo "Set LD_LIBRARY_PATH for shared libraries to be found:"
-echo "  export LD_LIBRARY_PATH=`pwd`/install/lib:`pwd`/install/lib/vtk-5.10$LD_LIBRARY_PATH"
+echo "  export LD_LIBRARY_PATH=$(pwd)/install/lib:$(pwd)/install/lib/vtk-5.10$LD_LIBRARY_PATH"
 echo "-----------------------------------------"
