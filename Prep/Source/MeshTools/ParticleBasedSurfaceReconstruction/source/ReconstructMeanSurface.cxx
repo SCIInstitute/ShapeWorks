5f0a385f8c07d6bd72b863a4aa1a3a218f5291da
Shireen Elhabian <shireen@sci.utah.edu>
2019-08-28 19:33:49 -0600

    prelim ReconstructMeanSurface that uses the new template definition of the Reconstruction class

--- Prep/Source/MeshTools/ParticleBasedSurfaceReconstruction/source/ReconstructMeanSurface.cxx
+++ Prep/Source/MeshTools/ParticleBasedSurfaceReconstruction/source/ReconstructMeanSurface.cxx
@@ -4,11 +4,12 @@
 #include <itkImageFileWriter.h>
 
 #include "Params/InputParams.h"
+#include "Vis.h"
 #include "Reconstruction.h"
 #include "Reconstruction.cpp"  //need to include template definition in order for it to be instantiated
 
-#include <type_traits>  // for condition typedefs
-
+#include "itkNrrdImageIOFactory.h"
+#include "itkMetaImageIOFactory.h"
 
 template < template < typename TCoordRep, unsigned > class TTransformType,
            template < typename ImageType, typename TCoordRep > class TInterpolatorType,
@@ -17,9 +18,80 @@ int DoIt(InputParams params)
 {
     typedef itk::ImageFileReader< ImageType >   ReaderType;
     typedef itk::ImageFileWriter< ImageType >   WriterType;
+    typedef Reconstruction < TTransformType, TInterpolatorType, TCoordRep, PixelType, ImageType> ReconstructionType;
+    typedef typename ReconstructionType::PointArrayType                                          PointArrayType;
 
-    Reconstruction < TTransformType, TInterpolatorType, TCoordRep, PixelType, ImageType> reconstructor(0.5,60);
+    // TODO: the below parameters should be obtained from the input ones with good enough defaults
+    float decimationPercent  = 0.3f;
+    double maxAngleDegrees   = 45.0;
+    size_t numClusters       = 5;
+
+    ReconstructionType reconstructor(decimationPercent, maxAngleDegrees, numClusters);
     reconstructor.reset();
+
+    // read local points and world points if given
+    std::vector< PointArrayType > local_pts;  local_pts.clear();
+    std::vector< PointArrayType > global_pts; global_pts.clear();
+
+    // read local points
+    for (unsigned int shapeNo = 0; shapeNo < params.localPointsFilenames.size(); shapeNo++)
+    {
+        std::cout << "Loading local points file: " << params.localPointsFilenames[shapeNo].c_str() << std::endl;
+
+        PointArrayType curShape;
+        Utils::readSparseShape(curShape, const_cast<char*> (params.localPointsFilenames[shapeNo].c_str()));
+
+        local_pts.push_back(curShape);
+    }
+
+
+    // define mean sparse shape -- this is considered as target points in the warp
+    if(params.worldPointsFilenames.size() == 0)
+    {
+        // if no world points are given, they will be estimated using procrustes
+        std::cout << "Computing mean sparse shape .... \n ";
+        global_pts = reconstructor.computeSparseMean(local_pts, params.do_procrustes, params.do_procrustes_scaling);
+    }
+    else
+    {
+        std::cout << "Computing mean sparse shape .... \n ";
+        global_pts = reconstructor.computeSparseMean(local_pts, false, false);
+        global_pts.clear(); // clear
+
+        // read given world points
+        for (unsigned int shapeNo = 0; shapeNo < params.worldPointsFilenames.size(); shapeNo++)
+        {
+            std::cout << "Loading world points file: " << params.localPointsFilenames[shapeNo].c_str() << std::endl;
+
+            PointArrayType curShape;
+            Utils::readSparseShape(curShape, const_cast<char*> (params.worldPointsFilenames[shapeNo].c_str()));
+
+            global_pts.push_back(curShape);
+        }
+    }
+
+    if(params.display)
+        Vis::visParticles(reconstructor.SparseMean(),params.glyph_radius,std::string("Mean Sparse Shape"));
+
+    // read distance transforms
+    std::vector<typename ImageType::Pointer> distance_transforms;
+    for (unsigned int shapeNo = 0; shapeNo < params.distanceTransformFilenames.size(); shapeNo++)
+    {
+        std::string filename = params.distanceTransformFilenames[shapeNo];
+
+        if (filename.find(".nrrd") != std::string::npos) {
+            itk::NrrdImageIOFactory::RegisterOneFactory();
+        } else if (filename.find(".mha") != std::string::npos) {
+            itk::MetaImageIOFactory::RegisterOneFactory();
+        }
+        typename ReaderType::Pointer reader = ReaderType::New();
+        reader->SetFileName( filename.c_str() );
+        reader->Update();
+        distance_transforms.push_back(reader->GetOutput());
+    }
+
+    // compute the dense shape
+    vtkSmartPointer<vtkPolyData> denseMean = reconstructor.getDenseMean(local_pts, global_pts, distance_transforms);
 }
 
 
