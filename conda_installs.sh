e6704c54d30a578c67d9571ceb2482db696d1e94
Cameron Christensen <cam@sci.utah.edu>
2019-09-03 23:55:21 -0600

    fixup conda_installs script

--- conda_installs.sh
+++ conda_installs.sh
@@ -7,12 +7,23 @@ if [ -z "$PS1" ]; then
   exit 1
 fi
 
-if [[ ! 'command -v conda' ]]; then
-  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-  bash ./Miniconda3-latest-MacOSX-x86_64.sh
+if ! [ -x "$(command -v conda)" ]; then
+  echo "installing anaconda..."
+  if [ "$(uname)" == "Darwin" ]; then
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
+    bash ./Miniconda3-latest-MacOSX-x86_64.sh
+    rm ./Miniconda3-latest-MacOSX-x86_64.sh
+  elif [ "$(uname)" == "Linux" ]; then
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash ./Miniconda3-latest-Linux-x86_64.sh
+    rm ./Miniconda3-latest-Linux-x86_64.sh
+  else
+    echo "ERROR: unknown OS $(uname)"
+    return 1
+  fi
+
   source ~/miniconda3/bin/activate
   conda config --set auto_activate_base false
-  rm ./Miniconda3-latest-MacOSX-x86_64.sh
 fi
 
 #update anaconda
@@ -27,7 +38,9 @@ conda activate shapeworks
 #install shapeworks deps
 conda install --yes -c anaconda cmake geotiff libxrandr-devel-cos6-x86_64 libxinerama-devel-cos6-x86_64 libxcursor-devel-cos6-x86_64 libxi-devel-cos6-x86_64
 conda install --yes -c conda-forge xorg-libx11 libuuid xorg-libsm 
-conda install --yes numpy matplotlib colorama
+conda install --yes -c anaconda numpy
+conda install --yes -c conda-forge matplotlib
+conda install --yes -c conda-forge colorama
 pip install termcolor
 
 conda info
