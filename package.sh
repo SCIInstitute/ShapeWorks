ca360b26841fc5186b35b07a33458f2b09aa42d2
Alan Morris <akenmorris@gmail.com>
2019-09-09 21:34:07 -0600

    More complex packager script, several fixes.

--- package.sh
+++ package.sh
@@ -1,13 +1,29 @@
 #!/bin/bash -x
 
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+VERSION=$1
 ROOT=`pwd`
 
-cd install/bin
+rm -rf "package/$VERSION"
 
+mkdir "package"
+
+cp -a install "package/${VERSION}"
+mkdir -p package/${VERSION}/examples/ellipsoid
+cp -a UseCaseDocumentation/* package/${VERSION}/examples/ellipsoid
+
+cd "package/${VERSION}"
+rm bin/h5cc bin/h5c++ bin/itkTestDriver
+rm -rf include share
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
+    cd bin
     macdeployqt ShapeWorksStudio.app -no-strip
     install_name_tool -add_rpath @executable_path/../Frameworks ShapeWorksStudio.app/Contents/MacOS/ShapeWorksStudio
     install_name_tool -add_rpath @executable_path/../../../../lib ShapeWorksStudio.app/Contents/MacOS/ShapeWorksStudio
@@ -17,11 +33,17 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	install_name_tool -add_rpath $QT_LIB_LOCATION $i
     done
 
-    
+    # Fix transitive loaded libs
+    cd ../lib
+    for i in *.dylib ; do
+	install_name_tool -change ${PWD}/libitkgdcmopenjp2-5.0.1.dylib @rpath/libitkgdcmopenjp2-5.0.1.dylib $i
+    done
+    cd ..
 fi
+
+cd ${ROOT}/package
+zip -r ${VERSION}.zip ${VERSION}
 
 cd $ROOT
 
-mkdir -p install/examples/ellipsoid
-cp -a UseCaseDocumentation/* install/examples/ellipsoid
 
