9b506c67bffe68eadb8994fa255772fee26472ca
AtefehKashani <root@atefeh.sci.utah.edu>
2019-10-14 16:50:54 -0600

    changed the output format of the transformation  matrix

--- Prep/Source/Alignment/ICPRigid3DImageRegistration/source/ICPRigid3DImageRegistration.cxx
+++ Prep/Source/Alignment/ICPRigid3DImageRegistration/source/ICPRigid3DImageRegistration.cxx
@@ -288,7 +288,13 @@ int main(int argc, char * argv [] )
         ofs.open (filename); // the file needs to be overwritten in case running the program on the same file several times
 
         // for ease of automatic parsing the generated param file
-        ofs <<"The Transformation matrix is:\n"<< *m;
+       // ofs <<"The Transformation matrix is:\n"<< *m;
+        for (r=0;r<3;r++){
+            for (c =0;c<4;c++){
+                ofs<< m[r][c]<<"\t";
+            }
+            ofs << "\n";
+        }
         ofs.close();
 
         typedef itk::Rigid3DTransformSurrogate<double>  TransformType;
