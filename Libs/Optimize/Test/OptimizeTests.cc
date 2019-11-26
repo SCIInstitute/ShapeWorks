8544862dbe558947f8d9061f2f62d117cc216d4d
Alan Morris <akenmorris@gmail.com>
2019-11-25 23:10:59 -0700

    Adding procrustes scaling test.

--- Libs/Optimize/Test/OptimizeTests.cc
+++ Libs/Optimize/Test/OptimizeTests.cc
@@ -1,5 +1,32 @@
 #include <gtest/gtest.h>
 
+#include "TestConfiguration.h"
+
+#include "ShapeWorksRunApp.h"
+#include "itkParticleShapeStatistics.h"
+
 TEST(OptimizeTests, sample_test) {
-  ASSERT_TRUE(true);
+
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/sphere");
+  chdir(test_location.c_str());
+  std::string paramfile = std::string("sphere.xml");
+  ShapeWorksRunApp<> app(paramfile.c_str());
+  app.Run();
+
+  ParticleShapeStatistics<3> stats;
+
+  stats.ReadPointFiles("analyze.xml");
+  stats.ComputeModes();
+  stats.PrincipalComponentProjections();
+
+  auto values = stats.Eigenvalues();
+  for (int i = 0; i < values.size(); i++) {
+    std::cerr << "Eigenvalue " << i << " : " << values[i] << "\n";
+  }
+
+  // check the first mode of variation.
+  // If Procrustes scaling is working, this should be tiny.
+  // Otherwise it is quite large (>8000).
+  double value = values[values.size() - 1];
+  ASSERT_LT(value, 1);
 }
