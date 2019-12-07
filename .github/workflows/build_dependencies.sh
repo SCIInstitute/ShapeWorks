e8404df0d988926775e55c300927b291e1f71be5
Alan Morris <akenmorris@gmail.com>
2019-12-06 20:51:47 -0700

    Adding message

--- .github/workflows/build_dependencies.sh
+++ .github/workflows/build_dependencies.sh
@@ -6,6 +6,8 @@ BUILD_DIR=$HOME/build
 if [ -d "$HOME/install" ]; then
     echo "Dependencies already built”
     exit 0
+else
+    echo “No cached dependencies, rebuilding”
 fi
 
 #set -v   #verbose execution for debugging
@@ -22,35 +24,6 @@ VTK_VER_STR="8.2"
 ITK_VER="v5.0.1"
 ITK_VER_STR="5.0"
 QT_MIN_VER="5.9.8"  # NOTE: 5.x is required, but this restriction is a clever way to ensure the anaconda version of Qt (5.9.6 or 5.9.7) isn't used since it won't work on most systems.
-
-usage()
-{
-  echo "usage: ./superbuild.sh [[-n=<num-procs>] [-i=<install_path>] [-b=<build_path>] [--clean] [--no-gui] [--vxl-dir=<vxl_path>] [--vtk-dir=<vtk_path>] [--itk-dir=<itk_path>] | [-h | --help]]"
-  echo ""
-  echo "If using Anaconda to install prerequisites please first run:"
-  echo "source ./conda_installs.sh"
-  echo ""
-  echo "For GUI applications, please make sure at least version $QT_MIN_VER of Qt5 is installed and that its qmake is in the path."
-  echo "Download Qt5 from: https://download.qt.io/archive/qt/"
-  echo ""
-  echo "Arguments:"
-  echo "  -h,--help               : Show this screen."
-  echo "  --clean                 : Remove all build directories and clone implicit dependencies."
-  echo "  --no-gui                : Do not build the ShapeWorks gui applicaitons, which require Qt."
-  echo "                          : The GUI is built by default if qmake >= $QT_MIN_VER is found in the path."
-  echo "  --with-studio           : Build ShapeWorksStudio"
-  echo "  -b,--build-dir=<path>   : Build directory for ShapeWorks and its implicit dependencieas (VXL, VTK, and ITK)."
-  echo "                          : By default uses a subdirectory of the current directory called 'build'."
-  echo "  -i,--install-dir=<path> : Install directory for ShapeWorks and its implicit dependencieas (VXL, VTK, and ITK)."
-  echo "                          : By default uses a subdirectory of the current directory called 'install'."
-  echo "  -n,--num-procs=<num>    : Number of processors to use for parallel builds (default is 8)."
-  echo "  --vxl-dir=<path>        : Path to existing VXL installation (version >= ${VXL_VER})."
-  echo "  --vtk-dir=<path>        : Path to existing VTK installation (version >= ${VTK_VER})."
-  echo "  --itk-dir=<path>        : Path to existing ITK installation (version >= ${ITK_VER})."
-  echo ""
-  echo "Example: ./superbuild.sh --num-procs=8 --install-dir=/path/to/desired/installation"
-  echo "Build results are saved in ${BUILD_LOG}."
-}
 
 parse_command_line()
 {
