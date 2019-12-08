a5df818ed4df6693a5565e3b0af8b4bdfbc4a526
Alan Morris <akenmorris@gmail.com>
2019-12-08 09:50:56 -0700

    Working on windows build.

--- .github/workflows/build_dependencies_windows.sh
+++ .github/workflows/build_dependencies_windows.sh
@@ -84,12 +84,14 @@ build_vxl()
   cd ${BUILD_DIR}
   git clone https://github.com/vxl/vxl.git
   cd vxl
-  git checkout -f tags/${VXL_VER}
+  # They fixed the VS compilation problem the day after the v2.0.2 release.
+  # There hasn't been a release since
+  git checkout -f c3fd27959f51e0469a7a6075e975f245ac306f3d
 
   if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_CORE_VIDEO:BOOL=OFF -DBUILD_BRL:BOOL=OFF -DBUILD_CONTRIB:BOOL=OFF -DVNL_CONFIG_LEGACY_METHODS=ON -DVCL_STATIC_CONST_INIT_FLOAT=0 -DVXL_FORCE_V3P_GEOTIFF:BOOL=ON -DVXL_USE_GEOTIFF:BOOL=OFF -DVXL_USE_DCMTK:BOOL=OFF -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
-  make -j${NUM_PROCS} install || exit 1
+  cmake --build . || exit 1
 
   VXL_DIR=${INSTALL_DIR}/share/vxl/cmake
 }
@@ -105,7 +107,7 @@ build_vtk()
   if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DVTK_Group_Qt:BOOL=${BUILD_GUI} -DVTK_QT_VERSION=5 -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release -DVTK_PYTHON_VERSION=3 -Wno-dev ..
-  make -j${NUM_PROCS} install || exit 1
+  cmake --build . || exit 1
 
   # TODO: this could be lib (not lib64) on the Windows linux subsystem (or even other linuxes), so need to verify
   if [ "$(uname)" == "Darwin" ]; then
@@ -127,33 +129,9 @@ build_itk()
   if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DModule_ITKVtkGlue:BOOL=ON -DITK_USE_SYSTEM_VXL=on -DVXL_DIR=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
-  make -j${NUM_PROCS} install || exit 1
+  cmake --build . || exit 1
 
   ITK_DIR=${INSTALL_DIR}/lib/cmake/ITK-${ITK_VER_STR}
-}
-
-build_shapeworks()
-{
-  echo "## Building ShapeWorks..."
-  OPENMP_FLAG="-DUSE_OPENMP=ON"
-  if [ "$(uname)" == "Darwin" ]; then
-    OPENMP_FLAG="-DUSE_OPENMP=OFF"
-  fi
-
-  cd ${BUILD_DIR}
-  if [[ $BUILD_CLEAN = 1 ]]; then rm -rf shapeworks-build; fi
-  mkdir -p shapeworks-build && cd shapeworks-build
-  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DITK_DIR=${ITK_DIR} -DVXL_DIR=${VXL_DIR} -DVTK_DIR=${VTK_DIR} -DBuild_Post:BOOL=${BUILD_POST} -DBuild_View2:BOOL=${BUILD_GUI} -DBuild_Studio:BOOL=${BUILD_STUDIO} ${OPENMP_FLAG} -Wno-dev -Wno-deprecated -DCMAKE_BUILD_TYPE=Release ${SRC}
-  make -j${NUM_PROCS} install || exit 1
-
-  # Inform users of ShapeWorks install path:
-  # FIXME/TODO: get rid of LD_LIBRARY_PATH requirement (https://github.com/SCIInstitute/ShapeWorks/issues/125)
-  echo "## -----------------------------------------"
-  echo "## ShapeWorks has successfully been installed in ${INSTALL_DIR}."
-  echo "## Set LD_LIBRARY_PATH for shared libraries to be found:"
-  echo "##   export LD_LIBRARY_PATH=${INSTALL_DIR}/lib:${INSTALL_DIR}/lib64:\$LD_LIBRARY_PATH"
-  echo "## -----------------------------------------"
-
 }
 
 # determine if we can build using specified or discovered version of Qt
@@ -205,6 +183,7 @@ build_all()
   if [[ -z $ITK_DIR ]]; then
     build_itk
   fi
+
 
 #  build_shapeworks
 }
