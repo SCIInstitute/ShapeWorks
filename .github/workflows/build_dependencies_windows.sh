4bb7f2001f2be95e586f708570ea9d4e995bcfbf
Alan Morris <akenmorris@gmail.com>
2019-12-08 12:21:17 -0700

    Updates

--- .github/workflows/build_dependencies_windows.sh
+++ .github/workflows/build_dependencies_windows.sh
@@ -1,14 +1,9 @@
 #!/bin/bash -x
 
-INSTALL_DIR=$HOME/install
-BUILD_DIR=$HOME/build
+INSTALL_DIR=$1
 
-if [ -d "$HOME/install" ]; then
-    echo "Dependencies already built"
-    exit 0
-else
-    echo “No cached dependencies, rebuilding”
-fi
+BUILD_DIR=`pwd`/build
+
 
 #set -v   #verbose execution for debugging
 
@@ -25,58 +20,6 @@ ITK_VER="v5.0.1"
 ITK_VER_STR="5.0"
 QT_MIN_VER="5.9.8"  # NOTE: 5.x is required, but this restriction is a clever way to ensure the anaconda version of Qt (5.9.6 or 5.9.7) isn't used since it won't work on most systems.
 
-parse_command_line()
-{
-  while [ "$1" != "" ]; do
-    case $1 in
-      -i=*|--install-dir=*)   INSTALL_DIR="${1#*=}"
-                              mkdir -p $INSTALL_DIR
-                              ;;
-      # FIXME/TODO: https://github.com/SCIInstitute/ShapeWorks/issues/77
-      -b=*|--build-dir=*)     BUILD_DIR="${1#*=}"
-                              mkdir -p $BUILD_DIR
-                              ;;
-      -n=*|--num-procs=*)     NUM_PROCS="${1#*=}"
-                              ;;
-      --no-gui )              BUILD_GUI=0
-                              ;;
-      --with-studio )         BUILD_STUDIO=1
-                              ;;
-      --vxl-dir=*)            VXL_DIR="${1#*=}"
-                              ;;
-      --vtk-dir=*)            VTK_DIR="${1#*=}"
-                              ;;
-      --itk-dir=*)            ITK_DIR="${1#*=}"
-                              ;;
-      --clean )               BUILD_CLEAN=1
-                              ;;
-      -h | --help )           usage
-                              exit
-                              ;;
-      * )                     usage
-                              exit 1
-    esac
-    shift
-  done
-}
-# test for required version of something (versions with dots are okay):
-# usage:
-#   at_least_required_version "name" $curr $required
-#   if [[ $? -eq 1 ]]; then echo "version sufficient"; fi
-at_least_required_version()
-{
-  packagename=$1
-  current=$2
-  required=$3
-  echo "## verifying ${packagename}..."
-  if [ "$(printf '%s\n' "$required" "$current" | sort -V | head -n1)" = "$required" ]; then 
-    echo "## SUCCESS: $current is greater than or equal to $required"
-    return 1
-  else
-    echo "## FAILURE: ${packagename} version must be at least $required"
-    return 0
-  fi
-}
 
 build_vxl()
 {
@@ -90,8 +33,10 @@ build_vxl()
 
   if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
-  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_CORE_VIDEO:BOOL=OFF -DBUILD_BRL:BOOL=OFF -DBUILD_CONTRIB:BOOL=OFF -DVNL_CONFIG_LEGACY_METHODS=ON -DVCL_STATIC_CONST_INIT_FLOAT=0 -DVXL_FORCE_V3P_GEOTIFF:BOOL=ON -DVXL_USE_GEOTIFF:BOOL=OFF -DVXL_USE_DCMTK:BOOL=OFF -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
-  cmake --build . || exit 1
+#  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_CORE_VIDEO:BOOL=OFF -DBUILD_BRL:BOOL=OFF -DBUILD_CONTRIB:BOOL=OFF -DVNL_CONFIG_LEGACY_METHODS=ON -DVCL_STATIC_CONST_INIT_FLOAT=0 -DVXL_FORCE_V3P_GEOTIFF:BOOL=ON -DVXL_USE_GEOTIFF:BOOL=OFF -DVXL_USE_DCMTK:BOOL=OFF -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
+  cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
+  cmake --build . --config Release || exit 1
+  cmake --build . --config Release --target install
 
   VXL_DIR=${INSTALL_DIR}/share/vxl/cmake
 }
@@ -107,7 +52,8 @@ build_vtk()
   if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DVTK_Group_Qt:BOOL=${BUILD_GUI} -DVTK_QT_VERSION=5 -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_BUILD_TYPE=Release -DVTK_PYTHON_VERSION=3 -Wno-dev ..
-  cmake --build . || exit 1
+  cmake --build . --config Release || exit 1
+  cmake --build . --config Release --target install
 
   # TODO: this could be lib (not lib64) on the Windows linux subsystem (or even other linuxes), so need to verify
   if [ "$(uname)" == "Darwin" ]; then
@@ -129,39 +75,12 @@ build_itk()
   if [[ $BUILD_CLEAN = 1 ]]; then rm -rf build; fi
   mkdir -p build && cd build
   cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DBUILD_SHARED_LIBS:BOOL=ON -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DModule_ITKVtkGlue:BOOL=ON -DITK_USE_SYSTEM_VXL=on -DVXL_DIR=${INSTALL_DIR} -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
-  cmake --build . || exit 1
+  cmake --build . --config Release || exit 1
+  cmake --build . --config Release --target install
 
   ITK_DIR=${INSTALL_DIR}/lib/cmake/ITK-${ITK_VER_STR}
 }
 
-# determine if we can build using specified or discovered version of Qt
-verify_qt()
-{
-  # If BUILD_GUI is true, verify sufficient version of Qt is installed using the qmake that's in the path.
-  # If not, inform the user of where to go to download and install Qt.
-
-  if [[ $BUILD_GUI = 1 ]]; then
-    if [[ `which qmake` ]]; then
-      QMAKE=`which qmake`
-    fi
-
-    # verify version
-    if [[ -f ${QMAKE} ]]; then
-      QT_VERSION=`${QMAKE} -v | grep "Using Qt version" | cut -d" " -f4`
-      at_least_required_version "Qt" "${QT_VERSION}" "${QT_MIN_VER}"
-      QT_VERSION_OK=$?
-    else
-      QT_VERSION_OK=0
-    fi
-
-    # inform user
-    if [[ $QT_VERSION_OK = 0 ]]; then
-      echo '## For GUI applications, please make sure at least version $QT_MIN_VER of Qt5 is installed and that its qmake is in the path.' >&2
-      echo '## Download Qt5 from: https://download.qt.io/archive/qt/' >&2
-      exit 1
-    fi
-  fi
-}
 
 build_all()
 {
@@ -198,8 +117,6 @@ echo "## ShapeWorks Superbuild"
 echo "##---------------------"
 
 SRC=`pwd`
-parse_command_line $*
-verify_qt
 
 echo "INSTALL_DIR: ${INSTALL_DIR}"
 echo "BUILD_DIR: ${BUILD_DIR}"
