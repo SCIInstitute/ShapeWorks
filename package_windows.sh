994a3c7a4f95a972a5d44d1cd0b8e94ab87e9a7d
Alan Morris <akenmorris@gmail.com>
2019-12-18 10:33:51 -0700

    Adding windows package script.

---
+++ package_windows.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+export SW_VERSION=$1
+ROOT=`pwd`
+
+cp -r build/bin/Release bin
+windeployqt "bin/ShapeWorksStudio.exe"
+windeployqt "bin/ShapeWorksView2.exe"
+$HOME/nsis.exe shapeworks.nsi
+
+
