5d31cc58437d5d62cc9ec3bb1cff99bd8ee2b77c
Alan Morris <akenmorris@gmail.com>
2020-01-10 22:46:56 -0700

    Adding ImageTests base.

---
+++ Testing/ImageTests/ImageTests.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <Libs/Image/Image.h>
+
+#include "TestConfiguration.h"
+
+using namespace Shapeworks;
+
+//---------------------------------------------------------------------------
+TEST(ImageTests, resample_test) {
+
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample");
+
+  Image image(test_location + "/1x2x2.nrrd");
+
+  image.resample(true, 1.0f);
+
+  Image ground_truth(test_location + "/resample_baseline.nrrd");
+
+  ASSERT_TRUE(image.compare_equal(ground_truth));
+}
+
+//TEST(ImageTests, blah_test) {
+
+// ...
+
+// }
