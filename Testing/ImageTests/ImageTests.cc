ac48ef3f63b2e7a46edeeb1dfeb97c2b199dfeed
Alan Morris <akenmorris@gmail.com>
2020-01-10 23:11:31 -0700

    Simplify path.

--- Testing/ImageTests/ImageTests.cc
+++ Testing/ImageTests/ImageTests.cc
@@ -9,13 +9,13 @@ using namespace Shapeworks;
 //---------------------------------------------------------------------------
 TEST(ImageTests, resample_test) {
 
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample");
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample/");
 
-  Image image(test_location + "/1x2x2.nrrd");
+  Image image(test_location + "1x2x2.nrrd");
 
   image.resample(true, 1.0f);
 
-  Image ground_truth(test_location + "/resample_baseline.nrrd");
+  Image ground_truth(test_location + "resample_baseline.nrrd");
 
   ASSERT_TRUE(image.compare_equal(ground_truth));
 }
