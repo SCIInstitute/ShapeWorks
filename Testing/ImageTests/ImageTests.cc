56dc6e3babe03d02c33188578183921807e05a4f
Alan Morris <akenmorris@gmail.com>
2020-01-10 23:12:59 -0700

    Formatting

--- Testing/ImageTests/ImageTests.cc
+++ Testing/ImageTests/ImageTests.cc
@@ -12,9 +12,7 @@ TEST(ImageTests, resample_test) {
   std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample/");
 
   Image image(test_location + "1x2x2.nrrd");
-
   image.resample(true, 1.0f);
-
   Image ground_truth(test_location + "resample_baseline.nrrd");
 
   ASSERT_TRUE(image.compare_equal(ground_truth));
