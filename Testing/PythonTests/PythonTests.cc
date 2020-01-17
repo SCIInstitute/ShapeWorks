8016928d02a60bc854936f3d79538dbf26230a07
Alan Morris <akenmorris@gmail.com>
2020-01-17 11:29:13 -0700

    Added tiny test of python ellipsoid example.

--- Testing/PythonTests/PythonTests.cc
+++ Testing/PythonTests/PythonTests.cc
@@ -1,26 +1,48 @@
 #include <gtest/gtest.h>
 
-#include <Libs/Image/Image.h>
+#include <iostream>
+#include <fstream>
+#include <cstdio>
 
 #include "TestConfiguration.h"
 
-using namespace shapeworks;
+#ifdef _WIN32
+   #include <io.h>
+   #define access    _access_s
+#else
+   #include <unistd.h>
+#endif
+
+//using namespace shapeworks;
+
+//---------------------------------------------------------------------------
+static bool file_exists(const std::string &filename)
+{
+  return access(filename.c_str(), 0) == 0;
+}
 
 //---------------------------------------------------------------------------
 TEST(PythonTests, tiny_test) {
 
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample/");
+  // locations
+  std::string python_examples_location = std::string(TEST_DATA_DIR) + std::string(
+    "/../../Examples/Python");
+  std::string bin_dir = std::string(BUILD_DIR) + "/bin";
+  std::cerr << "Location: " << python_examples_location << "\n";
 
-  Image image(test_location + "1x2x2.nrrd");
-  image.resample(1.0f, true /*isbinary*/);
-  image.binarize();
-  Image ground_truth(test_location + "resample_baseline.nrrd");
+  // check that one of the resulting files exists
+  std::string check_file = python_examples_location +
+                           "/TestEllipsoids/PointFiles/32/seg.ellipsoid_0.isores.pad.com.aligned.cropped.tpSmoothDT_local.particles";
 
-  ASSERT_TRUE(image.compare_equal(ground_truth));
+  // change to the python examples directory
+  chdir(python_examples_location.c_str());
+
+  // delete the file to make sure it's remade
+  std::remove(check_file.c_str());
+
+  // run python
+  std::string command = "python ellipsoidMain.py --tiny_test " + bin_dir;
+  system(command.c_str());
+
+  ASSERT_TRUE(file_exists(check_file));
 }
-
-//TEST(ImageTests, blah_test) {
-
-// ...
-
-// }
