cfe30c3566980d644d98512224f881c9241b7afa
Karthik Karanth <karanth.karthik@gmail.com>
2020-02-02 18:16:49 -0700

    use ASSERT_DOUBLE_EQ

--- Testing/ParticlesTests/ParticlesTests.cpp
+++ Testing/ParticlesTests/ParticlesTests.cpp
@@ -10,21 +10,21 @@ using namespace shapeworks;
 
 const std::string test_dir = std::string(TEST_DATA_DIR) + "/ellipsoid_particles/";
 const std::vector<std::string> filenames = {
-        test_dir + "seg.ellipsoid_0.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_1.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_10.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_11.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_12.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_13.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_14.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_15.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_16.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_17.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_18.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_19.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_2.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_20.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
-        test_dir + "seg.ellipsoid_21.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles"
+    test_dir + "seg.ellipsoid_0.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_1.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_10.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_11.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_12.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_13.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_14.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_15.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_16.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_17.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_18.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_19.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_2.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_20.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles",
+    test_dir + "seg.ellipsoid_21.isores.pad.com.aligned.cropped.tpSmoothDT_world.particles"
 };
 
 //---------------------------------------------------------------------------
@@ -33,7 +33,7 @@ TEST(ParticlesTests, compactness_test) {
     ASSERT_TRUE(particleSystem.LoadParticles(filenames));
 
     const double compactness = ShapeEvaluation::ComputeCompactness(particleSystem, 1);
-    ASSERT_TRUE(abs(compactness - 0.99178682878009183) < 1e-8f);
+    ASSERT_DOUBLE_EQ(compactness, 0.99178682878009183);
 }
 
 //---------------------------------------------------------------------------
@@ -42,7 +42,7 @@ TEST(ParticlesTests, generalization_test) {
     ASSERT_TRUE(particleSystem.LoadParticles(filenames));
 
     const double generalization = ShapeEvaluation::ComputeGeneralization(particleSystem, 1);
-    ASSERT_TRUE(abs(generalization - 25.36334900863832) < 1e-8f);
+    ASSERT_DOUBLE_EQ(generalization, 25.36334900863832);
 }
 
 //---------------------------------------------------------------------------
