ecdcac936252f08a47bff9e5e3d1f3adc9c2fa7d
archanasri <archanasri@sci.utah.edu>
2020-02-07 14:55:39 -0700

    Modified notes, changed resample to isoresample for testing

--- Testing/ImageTests/ImageTests.cpp
+++ Testing/ImageTests/ImageTests.cpp
@@ -7,71 +7,95 @@
 using namespace shapeworks;
 
 //---------------------------------------------------------------------------
-TEST(ImageTests, resample_binary_isotropic_test) {
+TEST(ImageTests, isoresample_binary_isotropic_test) {
 
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample/");
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/isoresample/");
 
   Image image(test_location + "binary-isotropic-input.nrrd");
   image.antialias();
   image.isoresample();
   image.binarize();
   image.recenter();
-  Image ground_truth(test_location + "binary-isotropic-resampled.nrrd");
+  Image ground_truth(test_location + "binary-isotropic-isoresampled.nrrd");
 
   ASSERT_TRUE(image.compare_equal(ground_truth));
 }
 
-TEST(ImageTests, resample_binary_anisotropic_test) {
+TEST(ImageTests, isoresample_binary_anisotropic_test) {
 
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample/");
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/isoresample/");
 
   Image image(test_location + "binary-anisotropic-input.nrrd");
   image.antialias();
   image.isoresample();
   image.binarize();
   image.recenter();
-  Image ground_truth(test_location + "binary-anisotropic-resampled.nrrd");
+  Image ground_truth(test_location + "binary-anisotropic-isoresampled.nrrd");
 
   ASSERT_TRUE(image.compare_equal(ground_truth));
 }
 
-TEST(ImageTests, resample_smooth_isotropic_test) {
+TEST(ImageTests, isoresample_smooth_isotropic_test) {
 
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample/");
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/isoresample/");
 
   Image image(test_location + "smooth-isotropic-input.nrrd");
   image.isoresample();
   image.recenter();
-  Image ground_truth(test_location + "smooth-isotropic-resampled.nrrd");
+  Image ground_truth(test_location + "smooth-isotropic-isoresampled.nrrd");
 
   ASSERT_TRUE(image.compare_equal(ground_truth));
 }
 
-TEST(ImageTests, resample_smooth_anisotropic_test) {
+TEST(ImageTests, isoresample_smooth_anisotropic_test) {
 
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/resample/");
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/isoresample/");
 
   Image image(test_location + "smooth-anisotropic-input.nrrd");
   image.isoresample();
   image.recenter();
-  Image ground_truth(test_location + "smooth-anisotropic-resampled.nrrd");
+  Image ground_truth(test_location + "smooth-anisotropic-isoresampled.nrrd");
 
   ASSERT_TRUE(image.compare_equal(ground_truth));
 }
 
-TEST(ImageTests, padimage_test) {
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/padimage/");
+TEST(ImageTests, isoresample_image_anisotropic_test) {
+
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/isoresample/");
+
+  Image image(test_location + "image-anisotropic-input.nrrd");
+  image.isoresample();
+  image.recenter();
+  Image ground_truth(test_location + "image-anisotropic-isoresampled.nrrd");
+
+  ASSERT_TRUE(image.compare_equal(ground_truth));
+}
+
+TEST(ImageTests, isoresample_mesh_anisotropic_test) {
+
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/isoresample/");
+
+  Image image(test_location + "mesh-anisotropic-input.nrrd");
+  image.isoresample();
+  image.recenter();
+  Image ground_truth(test_location + "mesh-anisotropic-isoresampled.nrrd");
+
+  ASSERT_TRUE(image.compare_equal(ground_truth));
+}
+
+TEST(ImageTests, pad_test) {
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/pad/");
 
   Image image(test_location + "1x2x2.nrrd");
   image.pad(30, 0.0);
  
-  Image ground_truth(test_location + "padimage_baseline.nrrd");
+  Image ground_truth(test_location + "pad_baseline.nrrd");
 
   ASSERT_TRUE(image.compare_equal(ground_truth));
 }
 
-TEST(ImageTests, padimage_identity_test) {
-  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/padimage/");
+TEST(ImageTests, pad_identity_test) {
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/pad/");
 
   Image image(test_location + "1x2x2.nrrd");
   image.pad(0, 0.0);
