23e783524ce1d4c248a2fac82d2220962c7130f7
Oleks Korshak <alexokorshak@gmail.com>
2020-03-02 13:49:07 -0700

    Remove some itk boilerplate and make ParticleDomain abstract

--- Libs/Optimize/ParticleSystem/itkParticleDomain.h
+++ Libs/Optimize/ParticleSystem/itkParticleDomain.h
@@ -1,9 +1,5 @@
 /*=========================================================================
-  Program:   ShapeWorks: Particle-based Shape Correspondence & Visualization
-  Module:    $RCSfile: itkParticleDomain.h,v $
-  Date:      $Date: 2011/03/24 01:17:33 $
-  Version:   $Revision: 1.2 $
-  Author:    $Author: wmartin $
+  Program:   ShapeWorks
 
   Copyright (c) 2009 Scientific Computing and Imaging Institute.
   See ShapeWorksLicense.txt for details.
@@ -17,32 +13,15 @@
 
 #include "itkDataObject.h"
 #include "itkPoint.h"
-#include "itkWeakPointer.h"
 
 namespace itk
 {
-/** \class ParticleDomain
- *  \brief 
- */
 template <unsigned int VDimension=3>
-class ITK_EXPORT ParticleDomain : public DataObject
+class ParticleDomain : public DataObject
 {
 public:
   /** Standard class typedefs */
-  typedef ParticleDomain Self;
-  typedef DataObject Superclass;
-  typedef SmartPointer<Self>  Pointer;
-  typedef SmartPointer<const Self> ConstPointer;
-  typedef WeakPointer<const Self>  ConstWeakPointer;
-
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
-
-  /** Run-time type information (and related methods). */
-  itkTypeMacro(ParticleDomain, DataObject);
-
-  /** Dimensionality of the domain of the particle system. */
-  itkStaticConstMacro(Dimension, unsigned int, VDimension);
+  typedef SmartPointer<ParticleDomain>  Pointer;
 
   /** Point type used to store particle locations. */
   typedef Point<double, VDimension> PointType;
@@ -51,8 +30,7 @@ public:
       example, implement boundary conditions or restrict points to lie on a
       surface.  Default behavior does nothing.  Returns true if the value of
       the point was modified and false otherwise. */
-  virtual bool ApplyConstraints(PointType &p) const
-  {  return false; }
+  virtual bool ApplyConstraints(PointType& p) const = 0;
 
   /** A Domain may define a distance calculation.  This is useful in cases
       such as geodesic distance, where distance depends on some information
@@ -70,14 +48,8 @@ public:
       gives the upper-left-hand corner of the domain.  The upper bound method
       gives the lower-right-hand-corner of the domain.  If a domain does not
       define boundaries, these methods will throw an exceptions  */
-  virtual const PointType &GetLowerBound() const
-  {
-    itkExceptionMacro("This Domain does not define a lower bound.");
-  }
-  virtual const PointType &GetUpperBound() const
-  {
-    itkExceptionMacro("This Domain does not define an upper bound.");
-  }
+  virtual const PointType& GetLowerBound() const = 0;
+  virtual const PointType& GetUpperBound() const = 0;
 
  /** Enable/Disable constraints on particle positions imposed by a domain. */
   void DisableConstraints()  { m_ConstraintsEnabled = false; }
@@ -85,31 +57,22 @@ public:
   bool GetConstraintsEnabled() const  { return m_ConstraintsEnabled; }
   void SetConstraintsEnabled( bool g )  { m_ConstraintsEnabled = g; }
 
-  
 protected:
-  ParticleDomain();
-  void PrintSelf(std::ostream& os, Indent indent) const;
+  ParticleDomain() : m_ConstraintsEnabled(true) {}
   virtual ~ParticleDomain() {}
+  void PrintSelf(std::ostream& os, Indent indent) const
+  {
+    Superclass::PrintSelf(os, indent);
+  }
 
   bool m_ConstraintsEnabled;
   
 private:
-  ParticleDomain(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
+  ParticleDomain(const ParticleDomain&); //purposely not implemented
+  void operator=(const ParticleDomain&); //purposely not implemented
 
 };
 
 } // end namespace itk
-
-
-#if ITK_TEMPLATE_EXPLICIT
-#include "Templates/itkParticleDomain+-.h"
-#endif
-
-#if ITK_TEMPLATE_TXX
-#include "itkParticleDomain.txx"
-#endif
-
-#include "itkParticleDomain.txx"
 
 #endif
