d7e58d3854510056383b0c5ab1db703ad8b375af
Alan Morris <akenmorris@gmail.com>
2020-04-04 12:48:20 -0600

    Fill in memory size

--- Studio/src/Application/Data/MeshWorker.cpp
+++ Studio/src/Application/Data/MeshWorker.cpp
@@ -20,6 +20,13 @@ void MeshWorker::process()
   std::cerr << "mesh worker here, working on item\n";
   // build the mesh using our MeshGenerator
   vtkSmartPointer<vtkPolyData> mesh = this->mesh_generator_.build_mesh(this->item_);
+
+  // compute the memory size of this shape
+  size_t shape_size = this->item_.shape.size() * sizeof(double);
+  size_t mesh_size = mesh->GetActualMemorySize() * 1024; // given in kb
+  size_t combined_size = (shape_size * 2) + mesh_size;
+  this->item_.memory_size = combined_size;
+
   this->queue_->remove(this->item_);
   this->cache_->insert_mesh(this->item_, mesh);
   emit result_ready();
