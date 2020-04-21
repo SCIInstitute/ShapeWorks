7ed574f13031585275114648ac7dac2fbaa6178c
Karthik Karanth <karanth.karthik@gmail.com>
2020-04-20 18:32:17 -0600

    disable memory usage logging

--- Libs/Optimize/ParticleSystem/MemoryUsage.h
+++ Libs/Optimize/ParticleSystem/MemoryUsage.h
@@ -2,7 +2,7 @@
 
 //TODO: Figure out memory logging in Windows or get rid of this
 // Disabled by default because this code is *nix specific.
-#define LOG_MEMORY_USAGE
+// #define LOG_MEMORY_USAGE
 
 #ifdef LOG_MEMORY_USAGE
 void process_mem_usage(double& vm_usage, double& resident_set);
