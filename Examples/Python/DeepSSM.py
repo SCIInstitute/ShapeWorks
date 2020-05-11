34d098ddfdb429a4141e52074711dd87dc262e7d
jadie1 <jadieraeadams@gmail.com>
2020-05-11 14:57:21 -0600

    Dataloaders complete

--- Examples/Python/DeepSSM.py
+++ Examples/Python/DeepSSM.py
@@ -25,6 +25,11 @@ from OptimizeUtils import *
 from AnalyzeUtils import *
 from DeepSSMUtils import *
 
+# set parent dir
+parentDir="TestDeepSSM/"
+if not os.path.exists(parentDir):
+	os.makedirs(parentDir)
+
 print("\nStep 1. Download Data\n")
 
 datasetName = "deepssm_femur"
@@ -36,20 +41,30 @@ datasetName = "deepssm_femur"
 # 	import DatasetUtils
 # 	DatasetUtils.downloadDataset(datasetName)
 
-# set parent dir
-parentDir="DeepSSMFemur/"
-if not os.path.exists(parentDir):
-	os.makedirs(parentDir)
-
 # # extract the zipfile  
 # print("Extracting data from " + filename + "...")
 # with ZipFile(filename, 'r') as zipObj:
 # 	zipObj.extractall(path=parentDir)
 
+print("\nStep 2. Reformat Data for Pytorch\n")
+
 img_dir = parentDir + datasetName + '/images/'
 model_dir = parentDir + datasetName + '/model/'
-numpy_dir = parentDir + '/numpy/'
-numpyData = getNumpyData(img_dir, model_dir, numpy_dir)
+pca_path = parentDir + datasetName + '/PCA_scores.csv'
+
+train_loader, val_loader, test_loader = getTorchDataLoaders(img_dir, model_dir, pca_path, parentDir)
+
+# print("\nStep 3. Train DeepSSM\n") 
+# deep_model = train(train_loader, val_loader)
+
+# print("\nStep 4. Test DeepSSM\n") 
+
+# # Test DeepSSM
+# test_predictions = test(test_loader)
+
+# error = getError(test_labels, test_predictions)
+
+
 
 
 
--- Examples/Python/DeepSSMUtils.py
+++ Examples/Python/DeepSSMUtils.py
@@ -3,57 +3,160 @@ import numpy as np
 import nrrd #@TODO not in conda installs (python -m pip install pynrrd)
 import random
 import torch
+from torch.utils.data import DataLoader
 
-# @TODO
-# reads .particles file and returns numpy array
-def readModel(filename):
-	return np.zeros((100,3))
+######################## Data loading functions ####################################
 
-# gets numpy data from image dir and model dir
-def getNumpyData(image_dir, model_dir, output_dir):
-	print(torch.rand(2,3).cuda())
-	print("Getting data in numpy format...")
-	if not os.path.exists(output_dir):
-		os.mkdir(output_dir)
-	# get lists of file paths
+'''
+Class for DeepSSM datasets that works with Pytorch DataLoader
+'''
+class DeepSSMdataset():
+	def __init__(self, img, pca_target, mdl_target):
+		self.img = torch.FloatTensor(img)
+		self.pca_target = torch.FloatTensor(pca_target)
+		self.mdl_target = torch.FloatTensor(mdl_target)
+	def __getitem__(self, index):
+		x = self.img[index]
+		y1 = self.pca_target[index]
+		y2 = self.mdl_target[index]
+		return x, y1, y2
+	def __len__(self):
+		return len(self.img)
+
+'''
+getTorchData helper
+returns sample prefix from path string
+'''
+def getPrefix(path):
+	file_name = os.path.basename(path)
+	prefix = '_'.join(file_name.split('_')[0:3])
+	prefix.replace('.particles','')
+	return prefix
+
+'''
+getTorchData helper
+returns a dictionary of PCA scores from csv
+{prefix:[pca scores]}
+'''
+def getPCAdictionary(pca_file):
+	PCAdict = {}
+	f = open(pca_file, "r")
+	for line in f.readlines():
+		line_array = line.replace('\n','').split(',')
+		prefix = getPrefix(line_array[0])
+		scores = [float(i) for i in line_array[1:]]
+		PCAdict[prefix] = scores
+	return PCAdict
+
+'''
+getTorchData helper
+get list from .particles format
+'''
+def getParticles(model_path):
+	f = open(model_path, "r")
+	data = []
+	for line in f.readlines():
+		points = line.replace(' \n','').split(" ")
+		points = [float(i) for i in points]
+		data.append(points)
+	return(data)
+
+'''
+Gets pytorch data from image dir, model dir, and pca scores
+Returns dir where train, validation, and test data loader are saved
+'''
+def getTorchDataLoaders(image_dir, model_dir, pca_file, parent_dir):
+	print("Reading in data... \nPercent complete:")
+	# get lists of file paths for images and models
 	imglist = []
 	for file in sorted(os.listdir(image_dir)):
 		imglist.append(image_dir + file)
 	modelList = []
 	for file in sorted(os.listdir(model_dir)):
 		modelList.append(model_dir + file)
-	# get all data and targets
-	data = []
-	targets = []
-	if len(imglist) != len(modelList):
-		print("Error: number of images does not equal number of targets")
+	# get dictionary of PCA scores
+	PCAdict = getPCAdictionary(pca_file)
+	# data error check
+	if len(imglist) != len(modelList) or len(imglist) != len(PCAdict.keys()):
+		print("Error: inconstent number of images, models, and PCA scores.")
 		exit()
-	for index in range(len(imglist)):
-		# print(str((index/len(imglist))*100)+'%')
-		[img,f] = nrrd.read(imglist[index])
-		print(np.array([img]).shape)
-		data.append([img])
-		model = readModel(modelList[index])
-		targets.append(model)
+	# get all data and targets
+	images = []
+	scores = []
+	models = []
+	# for index in range(len(imglist)):
+	for index in range(10):
+		if index%200 == 0:
+			print(str((index/len(imglist))*100)+'%')
+		image_path = imglist[index]
+		prefix = getPrefix(image_path)
+		model_path = ''
+		for modelPath in modelList:
+			if prefix in modelPath:
+				model_path = modelPath
+		# data error check
+		if prefix not in getPrefix(model_path):
+			print("Error: Images and models mismatched")
+			print(prefix)
+			exit()
+		# add image
+		[img,f] = nrrd.read(image_path)
+		images.append(img)
+		# add score
+		score = PCAdict[prefix]
+		scores.append(score)
+		# add model
+		mdl = getParticles(model_path)
+		models.append(mdl)
+
+	print("Shuffling and turning to tensors...")
 	# shuffle
-	c = list(zip(data, targets))
+	c = list(zip(images, scores, models))
 	random.shuffle(c)
-	data, targets = zip(*c)
-	data = np.array(data)
-	targets = np.array(targets)
-	# split into train, validation, and test
-	num_train = int(len(imglist)*.8) # 80%
-	num_val = int(len(imglist)*1) # 10%
-	train_data = data[:num_train]
-	np.save(output_dir + 'train_data.npy', train_data)
-	val_data = data[num_train:num_train+num_val]
-	np.save(output_dir + 'val_data.npy', val_data)
-	test_data = data[num_train+num_val:]
-	np.save(output_dir + 'test_data.npy', test_data)
-	train_targets = targets[:num_train]
-	np.save(output_dir + 'train_targets.npy', train_targets)
-	val_targets = targets[num_train:num_train+num_val]
-	np.save(output_dir + 'val_targets.npy', val_targets)
-	test_targets = targets[num_train+num_val:]
-	np.save(output_dir + 'test_targets.npy', test_targets)
-	print("Numpy data saved.")
+	images, scores, models = zip(*c)
+	# split into train (80%), validation(10%), and test(10%) datsets
+	cut1 = int(len(images)*.8) 
+	cut2 = cut1 + int(len(images)*.1)
+	train_data = DeepSSMdataset(images[:cut1], scores[:cut1], models[:cut1])
+	val_data = DeepSSMdataset(images[cut1:cut2], scores[cut1:cut2], models[cut1:cut2])
+	test_data = DeepSSMdataset(images[cut2:], scores[cut2:], models[cut2:])
+
+	print("Creating and saving dataloaders...")
+	loader_dir = parent_dir + "TorchDataLoaders/"
+	if not os.path.exists(loader_dir):
+		os.makedirs(loader_dir)
+	trainloader = DataLoader(
+			train_data,
+			batch_size=4,
+			shuffle=True,
+			num_workers=2,
+			pin_memory=torch.cuda.is_available()
+		)
+	train_path = loader_dir + 'train'
+	torch.save(trainloader, train_path)
+	validationloader = DataLoader(
+			val_data,
+			batch_size=1,
+			shuffle=True,
+			num_workers=2,
+			pin_memory=torch.cuda.is_available()
+		)
+	val_path = loader_dir + 'validation'
+	torch.save(trainloader, val_path)
+	testloader = DataLoader(
+			test_data,
+			batch_size=1,
+			shuffle=True,
+			num_workers=2,
+			pin_memory=torch.cuda.is_available()
+		)
+	test_path = loader_dir + 'test'
+	torch.save(trainloader, test_path)
+	print("Done.")
+	return train_path, val_path, test_path
+
+##################### #################################
+	def train(train_loader, val_loader):
+		pass
+
+	
