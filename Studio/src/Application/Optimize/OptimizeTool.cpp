a4dd851845f39c88f84b76d583be0072c7b81c22
Alan Morris <akenmorris@gmail.com>
2020-05-21 15:10:40 -0600

    Updated OptimizeTool for new Settings

--- Studio/src/Application/Optimize/OptimizeTool.cpp
+++ Studio/src/Application/Optimize/OptimizeTool.cpp
@@ -18,7 +18,7 @@
 #include <ui_OptimizeTool.h>
 
 //---------------------------------------------------------------------------
-OptimizeTool::OptimizeTool(Preferences& prefs) : preferences_(prefs)
+OptimizeTool::OptimizeTool()
 {
   this->ui_ = new Ui_OptimizeTool;
   this->ui_->setupUi(this);
@@ -85,15 +85,15 @@ void OptimizeTool::on_run_optimize_button_clicked()
   if (this->optimization_is_running_) {
     this->shutdown_threads();
     this->optimization_is_running_ = false;
-    this->enableActions();
+    this->enable_actions();
     return;
   }
   this->optimization_is_running_ = true;
   emit optimize_start();
 
-  this->enableActions();
+  this->enable_actions();
 
-  this->update_preferences();
+  this->store_settings();
   emit message("Please wait: running optimize step...");
   emit progress(1);
   //this->ui_->run_optimize_button->setEnabled(false);
@@ -113,7 +113,6 @@ void OptimizeTool::on_run_optimize_button_clicked()
   numbers_of_particles.push_back(this->ui_->number_of_particles->value());
   this->optimize_->SetFileOutputEnabled(false);
   this->optimize_->SetImages(imgs);
-  this->optimize_->SetCutPlanes(this->cutPlanes_);
   this->optimize_->SetStartingRegularization(this->ui_->starting_regularization->value());
   this->optimize_->SetEndingRegularization(this->ui_->ending_regularization->value());
   this->optimize_->SetIterationsPerSplit(this->ui_->iterations->value());
@@ -142,217 +141,58 @@ void OptimizeTool::on_run_optimize_button_clicked()
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::meshDecimation_valueChanged(double v)
-{
-  this->preferences_.set_preference("optimize_decimation", v);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::numClusters_valueChanged(int v)
-{
-  this->preferences_.set_preference("optimize_clusters", v);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::on_weight_valueChanged(double v)
-{
-  this->preferences_.set_preference("optimize_weight", v);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::maxAngle_valueChanged(double v)
-{
-  this->preferences_.set_preference("optimize_maxAngle", v);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::on_starting_regularization_valueChanged(double v)
-{
-  this->preferences_.set_preference("optimize_start_reg", v);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::on_ending_regularization_valueChanged(double v)
-{
-  this->preferences_.set_preference("optimize_end_reg", v);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::on_iterations_valueChanged(int v)
-{
-  this->preferences_.set_preference("optimize_iters", v);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::decay_span_valueChanged(int v)
-{
-  this->preferences_.set_preference("optimize_decay_span",
-                                    static_cast<double>(v));
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::on_procrustes_interval_valueChanged(int v)
-{
-  this->preferences_.set_preference("optimize_procrustes_interval",
-                                    static_cast<double>(v));
-}
-
-//---------------------------------------------------------------------------
 void OptimizeTool::handle_message(std::string s)
 {
   emit message(s);
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::loadCutPlanesFile(std::string file)
-{
-  std::ifstream planes(file.c_str());
-  if (!planes.good()) {
-    emit error_message("Cannot open file: " + file);
-    this->ui_->cutPlanesFile->setText("None Selected");
-    this->cutPlanes_.clear();
-    return;
-  }
-  std::vector<std::array<itk::Point<double>, 3 >> cutPlanes;
-  while (planes.good()) {
-    float x1, y1, z1, x2, y2, z2, x3, y3, z3;
-    planes >> x1 >> y1 >> z1 >> x2 >> y2 >> z2 >> x3 >> y3 >> z3;
-    if (!planes.fail()) {
-      itk::Point<double> p1, p2, p3;
-      p1[0] = x1; p1[1] = y1; p1[2] = z1;
-      p2[0] = x2; p2[1] = y2; p2[2] = z2;
-      p3[0] = x3; p3[1] = y3; p3[2] = z3;
-      cutPlanes.push_back({ { p1, p2, p3 } });
-    }
-  }
-  planes.close();
-  if (cutPlanes.empty() || (cutPlanes.size() > 1 &&
-                            cutPlanes.size() != this->session_->get_shapes().size())) {
-    emit error_message(std::string("Error reading cutting plane file. Must have 1 ") +
-                       "set of 3 points, or X set of 3 points, where X is number of samples.");
-    this->ui_->cutPlanesFile->setText("None Selected");
-    this->cutPlanes_.clear();
-    return;
-  }
-  emit message("Successfully read in cut planes file: " + file);
-  this->cutPlanes_ = cutPlanes;
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::on_cutPlanesFile_editingFinished()
-{
-  //get the cutting planes from file if possible
-  auto file = this->ui_->cutPlanesFile->text().toStdString();
-  if (file.empty() || file == "None Selected") {
-    this->ui_->cutPlanesFile->setText("None Selected");
-    this->cutPlanes_.clear();
-    return;
-  }
-  this->loadCutPlanesFile(file);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::on_cutPlanesFileButton_clicked()
-{
-  std::string file;
-  QString filename;
-  filename = QFileDialog::getOpenFileName(this,
-                                          tr("Import Clipping Planes File..."),
-                                          this->preferences_.get_preference("Main/last_directory",
-                                                                            QString()),
-                                          tr("Text files (*.txt)"));
-  if (filename.isEmpty()) {
-    return;
-  }
-  this->preferences_.set_preference("Main/last_directory",
-                                    QDir().absoluteFilePath(filename));
-  file = filename.toStdString();
-  this->ui_->cutPlanesFile->setText(QString::fromStdString(file));
-  this->loadCutPlanesFile(file);
-}
-
-//---------------------------------------------------------------------------
 void OptimizeTool::on_restoreDefaults_clicked()
 {
-  this->preferences_.delete_entry("optimize_clusters");
-  this->preferences_.delete_entry("optimize_decimation");
-  this->preferences_.delete_entry("optimize_particles");
-  this->preferences_.delete_entry("optimize_start_reg");
-  this->preferences_.delete_entry("optimize_end_reg");
-  this->preferences_.delete_entry("optimize_iters");
-  this->preferences_.delete_entry("optimize_weight");
-  this->preferences_.delete_entry("optimize_maxAngle");
-  this->preferences_.delete_entry("optimize_decay_span");
-  this->preferences_.delete_entry("optimize_procrustes_interval");
-  this->preferences_.delete_entry("optimize_iters_optimization");
-
-  this->preferences_.restore_defaults();
-  this->set_preferences();
-  auto optimize_particles = this->preferences_.get_preference("optimize_particles", 128);
-  this->ui_->number_of_particles->setValue(optimize_particles);
-  qApp->processEvents();
-  this->preferences_.set_saved(false);
+  // store a set of blank settings
+  Settings settings;
+  this->session_->get_project()->set_settings(Settings::OPTIMIZE_SETTINGS, settings);
+  // now load those settings
+  this->load_settings();
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::on_number_of_particles_valueChanged(int val)
+void OptimizeTool::set_session(QSharedPointer<Session> session)
 {
-  this->preferences_.set_preference("optimize_particles", val);
+  this->session_ = session;
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::set_project(QSharedPointer<Session> project)
+void OptimizeTool::load_settings()
 {
-  this->session_ = project;
+  Settings settings = this->session_->get_project()->get_settings(Settings::OPTIMIZE_SETTINGS);
+
+  this->ui_->weight->setValue(settings.get("relative_weighting", 1.0));
+  this->ui_->number_of_particles->setValue(settings.get("number_of_particles", 128));
+  this->ui_->starting_regularization->setValue(settings.get("starting_regularization", 10.0));
+  this->ui_->ending_regularization->setValue(settings.get("ending_regularization", 1.0));
+  this->ui_->iterations->setValue(settings.get("iterations_per_split", 1000));
+  this->ui_->optimization_iterations->setValue(settings.get("optimization_iterations", 1000));
+  this->ui_->procrustes_interval->setValue(settings.get("procrustes_interval", 0));
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::set_preferences(bool setScales)
+void OptimizeTool::store_settings()
 {
-  this->ui_->weight->setValue(
-    this->preferences_.get_preference("optimize_weight",
-                                      this->ui_->weight->value()));
-  this->ui_->number_of_particles->setValue(
-    this->preferences_.get_preference("optimize_particles",
-                                      this->ui_->number_of_particles->value()));
-  this->ui_->starting_regularization->setValue(
-    this->preferences_.get_preference("optimize_start_reg",
-                                      this->ui_->starting_regularization->value()));
-  this->ui_->ending_regularization->setValue(
-    this->preferences_.get_preference("optimize_end_reg",
-                                      this->ui_->ending_regularization->value()));
-  this->ui_->iterations->setValue(
-    this->preferences_.get_preference("optimize_iters",
-                                      this->ui_->iterations->value()));
-  this->ui_->optimization_iterations->setValue(
-    this->preferences_.get_preference("optimize_iters_optimization",
-                                      this->ui_->optimization_iterations->value()));
-  this->ui_->procrustes_interval->setValue(
-    this->preferences_.get_preference("optimize_procrustes_interval",
-                                      this->ui_->procrustes_interval->value()));
+  Settings settings = this->session_->get_project()->get_settings(Settings::OPTIMIZE_SETTINGS);
+  settings.set("relative_weighting", this->ui_->weight->value());
+  settings.set("number_of_particles", this->ui_->number_of_particles->value());
+  settings.set("starting_regularization", this->ui_->starting_regularization->value());
+  settings.set("ending_regularization", this->ui_->ending_regularization->value());
+  settings.set("iterations_per_split", this->ui_->iterations->value());
+  settings.set("optimization_iterations", this->ui_->optimization_iterations->value());
+  settings.set("procrustes_interval", this->ui_->procrustes_interval->value());
+
+  this->session_->get_project()->set_settings(Settings::OPTIMIZE_SETTINGS, settings);
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::update_preferences()
-{
-  this->preferences_.set_preference("optimize_weight",
-                                    this->ui_->weight->value());
-  this->preferences_.set_preference("optimize_particles",
-                                    this->ui_->number_of_particles->value());
-  this->preferences_.set_preference("optimize_start_reg",
-                                    this->ui_->starting_regularization->value());
-  this->preferences_.set_preference("optimize_end_reg",
-                                    this->ui_->ending_regularization->value());
-  this->preferences_.set_preference("optimize_iters",
-                                    this->ui_->iterations->value());
-  this->preferences_.set_preference("optimize_iters_optimization",
-                                    this->ui_->optimization_iterations->value());
-  this->preferences_.set_preference("optimize_procrustes_interval",
-                                    this->ui_->procrustes_interval->value());
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::enableActions()
+void OptimizeTool::enable_actions()
 {
   //this->ui_->run_optimize_button->setEnabled(true);
   if (this->optimization_is_running_) {
@@ -364,26 +204,9 @@ void OptimizeTool::enableActions()
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::disableActions()
+void OptimizeTool::disable_actions()
 {
   //this->ui_->run_optimize_button->setEnabled(false);
-}
-
-//---------------------------------------------------------------------------
-void OptimizeTool::setCutPlanesFile(std::string file)
-{
-  this->ui_->cutPlanesFile->setText(QString::fromStdString(file));
-  this->loadCutPlanesFile(file);
-}
-
-//---------------------------------------------------------------------------
-std::string OptimizeTool::getCutPlanesFile()
-{
-  auto name = this->ui_->cutPlanesFile->text().toStdString();
-  if (name == "None Selected") {
-    return "";
-  }
-  return name;
 }
 
 //---------------------------------------------------------------------------
--- Studio/src/Application/Optimize/OptimizeTool.h
+++ Studio/src/Application/Optimize/OptimizeTool.h
@@ -17,19 +17,17 @@ class OptimizeTool : public QWidget
   Q_OBJECT;
 public:
 
-  OptimizeTool(Preferences& prefs);
+  OptimizeTool();
   ~OptimizeTool();
 
   /// set the pointer to the project
-  void set_project(QSharedPointer<Session> project);
+  void set_session(QSharedPointer<Session> session);
 
-  void set_preferences(bool setScales = false);
+  void load_settings();
+  void store_settings();
 
-  void update_preferences();
-  void enableActions();
-  void disableActions();
-  void setCutPlanesFile(std::string file);
-  std::string getCutPlanesFile();
+  void enable_actions();
+  void disable_actions();
 
   void shutdown_threads();
 
@@ -37,27 +35,12 @@ public Q_SLOTS:
 
   /// Run optimize tool
   void on_run_optimize_button_clicked();
-  void on_cutPlanesFile_editingFinished();
-  void on_cutPlanesFileButton_clicked();
   void on_restoreDefaults_clicked();
-  void on_number_of_particles_valueChanged(int val);
   void handle_optimize_complete();
   void handle_progress(int val);
   void handle_error(std::string);
   void handle_warning(std::string);
-  void meshDecimation_valueChanged(double v);
-  void numClusters_valueChanged(int v);
-  void on_weight_valueChanged(double v);
-  void maxAngle_valueChanged(double v);
-  void on_starting_regularization_valueChanged(double v);
-  void on_ending_regularization_valueChanged(double v);
-  void on_iterations_valueChanged(int v);
-  void decay_span_valueChanged(int v);
-  void on_procrustes_interval_valueChanged(int v);
   void handle_message(std::string);
-
-private:
-  void loadCutPlanesFile(std::string file);
 
 signals:
   void optimize_start();
@@ -72,8 +55,6 @@ private:
   QList<QThread*> threads_;
   bool optimization_is_running_ = false;
   QOptimize* optimize_ = nullptr;
-  Preferences& preferences_;
   Ui_OptimizeTool* ui_;
   QSharedPointer<Session> session_;
-  std::vector<std::array<itk::Point<double>, 3 >> cutPlanes_;
 };
