eccbaa93430f35e9a4721a9cffe644c0eb54a2db
Oleks Korshak <alexokorshak@gmail.com>
2020-06-15 17:10:18 -0700

    use hardcoded ones as attribute scales for MeshDomain

--- Libs/Optimize/OptimizeParameterFile.cpp
+++ Libs/Optimize/OptimizeParameterFile.cpp
@@ -478,18 +478,9 @@ bool OptimizeParameterFile::read_mesh_inputs(TiXmlHandle *docHandle, Optimize *o
     }
   }
   std::vector<double> attr_scales;
-  elem = docHandle->FirstChild("attribute_scales").Element();
-  if (elem) {
-    inputsBuffer.str(elem->GetText());
-    double value;
-    while (inputsBuffer >> value) {
-      attr_scales.push_back(value);
-    }
-    inputsBuffer.clear();
-    inputsBuffer.str("");
-  }
-  inputsBuffer.clear();
-  inputsBuffer.str("");
+  attr_scales.push_back(1);
+  attr_scales.push_back(1);
+  attr_scales.push_back(1);
   if (attr_scales.size() != 3) {
     std::cerr << "not enough attribute scale values!!!" << std::endl;
     return false;
