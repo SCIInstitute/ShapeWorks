39ff86bc0a4ea8a30c225a1635e5317b1852aecc
Alan Morris <akenmorris@gmail.com>
2020-06-19 16:24:20 -0600

    Add fixed domain test case.  Resolves issue #556.

--- Testing/OptimizeTests/OptimizeTests.cpp
+++ Testing/OptimizeTests/OptimizeTests.cpp
@@ -86,3 +86,44 @@ TEST(OptimizeTests, sample_test) {
   double value = values[values.size() - 1];
   ASSERT_LT(value, 100);
 }
+
+//---------------------------------------------------------------------------
+TEST(OptimizeTests, fixed_domain_test) {
+
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/fixed_domain");
+  chdir(test_location.c_str());
+
+  // prep/groom
+  prep_distance_transform("sphere10.nrrd", "sphere10_DT.nrrd");
+  prep_distance_transform("sphere20.nrrd", "sphere20_DT.nrrd");
+  prep_distance_transform("sphere30.nrrd", "sphere30_DT.nrrd");
+  prep_distance_transform("sphere40.nrrd", "sphere40_DT.nrrd");
+
+  // make sure we clean out the output file of interest
+  std::remove("output/sphere40_DT_world.particles");
+
+  // run with parameter file
+  std::string paramfile = std::string("fixed_domain.xml");
+  Optimize app;
+  OptimizeParameterFile param;
+  ASSERT_TRUE(param.load_parameter_file(paramfile.c_str(), &app));
+  app.Run();
+
+  // compute stats
+  ParticleShapeStatistics<3> stats;
+  stats.ReadPointFiles("analyze.xml");
+  stats.ComputeModes();
+  stats.PrincipalComponentProjections();
+
+  // print out eigenvalues (for debugging)
+  auto values = stats.Eigenvalues();
+  for (int i = 0; i < values.size(); i++) {
+    std::cerr << "Eigenvalue " << i << " : " << values[i] << "\n";
+  }
+
+  // check the first mode of variation.
+  // Procrustes scaling is off for this fixed domain case, so it should be quite large. (>5000)
+  // If the new non-fixed domain doesn't optimize, the value will be about 2800
+  double value = values[values.size() - 1];
+  ASSERT_GT(value, 5000);
+}
