afcad225f397596ee2373c92d008e5ba11a4a483
Oleks Korshak <alexokorshak@gmail.com>
2020-06-28 17:32:50 -0700

    anatomyPairsToSingles throws exception instead of printing error and continueing to try to run

--- Examples/Python/GroomUtils.py
+++ Examples/Python/GroomUtils.py
@@ -518,6 +518,14 @@ def applyDistanceTransforms(parentDir, inDataList,antialiasIterations=20, smooth
 
 # Refelcts images and meshes to reference side
 def anatomyPairsToSingles(outDir, seg_list, img_list, reference_side):
+    if reference_side == 'right':
+        ref = 'R'
+        flip = 'L'
+    elif reference_side == 'left':
+        ref = 'L'
+        flip = 'R'
+    else:
+        raise Exception("reference_side must be 'left' or 'right'")
     if not os.path.exists(outDir):
         os.makedirs(outDir)
     outSegDir = os.path.join(outDir, "segmentations")
@@ -531,14 +539,6 @@ def anatomyPairsToSingles(outDir, seg_list, img_list, reference_side):
     for img in img_list:
         img_name = os.path.basename(img)
         prefix = img_name.split("_")[0]
-        if reference_side == 'right':
-            ref = 'R'
-            flip = 'L'
-        elif reference_side == 'left':
-            ref = 'L'
-            flip = 'R'
-        else:
-            print("Error: reference side must be 'left' or 'right'.")
         # check if ref exists
         ref_prefix = prefix + "_" + ref
         flip_prefix = prefix + "_" + flip
