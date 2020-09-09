9f5ddadacb87e7f2855a8559d35418d4a8df1d3f
Alan Morris <akenmorris@gmail.com>
2020-09-09 11:32:04 -0600

    Parallelize studio groom with TBB

--- Studio/src/Application/Groom/QGroom.cpp
+++ Studio/src/Application/Groom/QGroom.cpp
@@ -1,6 +1,6 @@
 #include "QGroom.h"
 
-//#include <tbb/parallel_for.h>
+#include <tbb/parallel_for.h>
 
 QGroom::QGroom(QObject* parent,
                std::vector<ImageType::Pointer> inputs,
@@ -9,52 +9,92 @@ QGroom::QGroom(QObject* parent,
                size_t iterations, bool verbose)
   : QObject(parent), ShapeWorksGroom(inputs,
                                      background, foreground, sigma,
-                                     padding, iterations, verbose) {}
+                                     padding, iterations, verbose)
+{}
 
 void QGroom::run()
 {
   this->seed_.Fill(0);
   size_t ran = 0, total = this->runTools_.size() * this->images_.size();
   if (this->runTools_.count("center")) {
-    for (size_t i = 0; i < this->images_.size(); i++) {
-      this->center(static_cast<int>(i));
-      emit progress(++ran * 100 / total);
-    }
+
+    tbb::parallel_for(
+      tbb::blocked_range<size_t>{0, this->images_.size()},
+      [&](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+          this->center(static_cast<int>(i));
+        }
+      });
+
+    ran += this->images_.size();
+    emit progress(ran * 100 / total);
   }
   if (this->runTools_.count("isolate")) {
-    for (size_t i = 0; i < this->images_.size(); i++) {
-      this->isolate(static_cast<int>(i));
-      emit progress(++ran * 100 / total);
-    }
+
+    tbb::parallel_for(
+      tbb::blocked_range<size_t>{0, this->images_.size()},
+      [&](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+          this->isolate(static_cast<int>(i));
+        }
+      });
+    ran += this->images_.size();
+    emit progress(ran * 100 / total);
   }
+
   if (this->runTools_.count("hole_fill")) {
-    for (size_t i = 0; i < this->images_.size(); i++) {
-      this->hole_fill(static_cast<int>(i));
-      emit progress(++ran * 100 / total);
-    }
+    tbb::parallel_for(
+      tbb::blocked_range<size_t>{0, this->images_.size()},
+      [&](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+          this->hole_fill(static_cast<int>(i));
+        }
+      });
+    ran += this->images_.size();
+    emit progress(ran * 100 / total);
   }
   if (this->runTools_.count("auto_pad")) {
-    for (size_t i = 0; i < this->images_.size(); i++) {
-      this->auto_pad(static_cast<int>(i));
-      emit progress(++ran * 100 / total);
-    }
+    tbb::parallel_for(
+      tbb::blocked_range<size_t>{0, this->images_.size()},
+      [&](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+          this->auto_pad(static_cast<int>(i));
+        }
+      });
+    ran += this->images_.size();
+    emit progress(ran * 100 / total);
   }
   if (this->runTools_.count("antialias")) {
-    for (size_t i = 0; i < this->images_.size(); i++) {
-      this->antialias(static_cast<int>(i));
-      emit progress(++ran * 100 / total);
-    }
+    tbb::parallel_for(
+      tbb::blocked_range<size_t>{0, this->images_.size()},
+      [&](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+          this->antialias(static_cast<int>(i));
+        }
+      });
+    ran += this->images_.size();
+    emit progress(ran * 100 / total);
   }
   if (this->runTools_.count("fastmarching")) {
-    for (size_t i = 0; i < this->images_.size(); i++) {
-      this->fastmarching(static_cast<int>(i));
-      emit progress(++ran * 100 / total);
-    }
+    tbb::parallel_for(
+      tbb::blocked_range<size_t>{0, this->images_.size()},
+      [&](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+          this->fastmarching(static_cast<int>(i));
+        }
+      });
+    ran += this->images_.size();
+    emit progress(ran * 100 / total);
   }
   if (this->runTools_.count("blur")) {
-    for (size_t i = 0; i < this->images_.size(); i++) {
-      this->blur(static_cast<int>(i));
-      emit progress(++ran * 100 / total);
-    }
+    tbb::parallel_for(
+      tbb::blocked_range<size_t>{0, this->images_.size()},
+      [&](const tbb::blocked_range<size_t>& r) {
+        for (size_t i = r.begin(); i < r.end(); ++i) {
+          this->blur(static_cast<int>(i));
+        }
+      });
+    ran += this->images_.size();
+    emit progress(ran * 100 / total);
   }
 }
