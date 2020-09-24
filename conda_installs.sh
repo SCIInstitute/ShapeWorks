6d79549113f0f1ead3feba82b94378bb05cc29c1
Cameron Christensen <cam@sci.utah.edu>
2020-09-24 16:20:30 -0600

    activate base at start to ensure not overwriting current env; disable conda install vtk as firat step to build and install vtk python bindings directly

--- conda_installs.sh
+++ conda_installs.sh
@@ -33,7 +33,7 @@ function install_conda() {
   fi
 
   # add default channels
-  conda deactivate
+  conda activate base
   conda config --add channels anaconda
   conda config --add channels conda-forge
   
@@ -66,7 +66,7 @@ function install_conda() {
   if ! conda install --yes pybind11=2.5.0; then return 1; fi
   if ! conda install --yes requests=2.24.0; then return 1; fi
   if ! conda install --yes scikit-learn=0.23.2; then return 1; fi
-  if ! conda install --yes vtk=9.0.1; then return 1; fi # VTK Python bindings (TODO: see issues #300, #302)
+  #if ! conda install --yes vtk=9.0.1; then return 1; fi # VTK Python bindings (TODO: see issues #300, #302)
 
   # shapeworks deps, platform-specific (e.g., for different versions)
   if [[ "$(uname)" == "Linux" ]]; then                                  # Linux
@@ -89,9 +89,9 @@ function install_conda() {
     if ! conda install --yes tbb-devel=2019.9; then return 1; fi
 
   else                                                                  # Windows
-    #if ! conda install --yes graphviz=2.38.0; then return 1; fi
-    if ! conda install --yes tbb=2020.1; then return 1; fi
-    if ! conda install --yes tbb-devel=2020.1; then return 1; fi
+    if ! conda install --yes graphviz=2.38.0; then return 1; fi
+    if ! conda install --yes tbb=2019.9; then return 1; fi
+    if ! conda install --yes tbb-devel=2019.9; then return 1; fi
   fi
 
   # pip installs, all platforms
