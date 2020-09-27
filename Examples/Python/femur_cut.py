23d75508d44901891506df6032f48836fe45165b
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2020-09-26 18:48:43 -0600

    Merged from femur_no_images. Now checking it out to test if it runs on that.

--- Examples/Python/femur_cut.py
+++ Examples/Python/femur_cut.py
@@ -28,7 +28,7 @@ def Run_Pipeline(args):
     if int(args.interactive) != 0:
         input("Press Enter to continue")
 
-    datasetName = "femur"
+    datasetName = "femur-v0"
     filename = datasetName + ".zip"
     # Check if the data is in the right place
     if not os.path.exists(filename):
