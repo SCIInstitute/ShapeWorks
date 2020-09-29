0a3ceed01e8e57d9becc35056e100023334a4267
Cameron Christensen <cam@sci.utah.edu>
2020-09-28 21:07:25 -0600

    don't try to add missing env vars to new ones (str + null = ka-boom); fix treating macro as string and cleanup comments and code

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -2,29 +2,34 @@
 
 #include "Testing.h"
 
-//---------------------------------------------------------------------------
-void shapeworksEnvSetup() // fixme: use googletest's setup/teardown: https://github.com/google/googletest/blob/master/googletest/docs/advanced.md
+/// set necessary env vars for running shapeworks executable and using python api
+// TODO: use googletest's setup/teardown: https://github.com/google/googletest/blob/master/googletest/docs/advanced.md
+void shapeworksEnvSetup()
 {
-  // set PATH to shapeworks executable called by shell scripts
+  const char* curr_path(std::getenv("PATH"));
+  const char* curr_pythonpath(std::getenv("PYTHONPATH"));
+  const char* curr_ld_library_path(std::getenv("LD_LIBRARY_PATH"));
+
 #ifdef _WIN32
-  auto path(std::string(BUILD_DIR) + "\\bin\\Release" + ";" + std::string(DEPS_DIR) + "\\bin" + ";" + std::getenv("PATH"));
+  auto path(std::string(BUILD_DIR) + "\\bin\\Release" + ";" + std::string(DEPS_DIR) + "\\bin" + ";" + (curr_path ? curr_path : ""));
   std::cout << "path: " << path << std::endl;
   _putenv_s("PATH", path.c_str());
-  auto pythonpath(std::string(DEPS_DIR) + "\\bin\\lib\\site-packages" + ";" + std::getenv("PYTHONPATH"));
+  auto pythonpath(std::string(DEPS_DIR) + "\\bin\\lib\\site-packages" + ";" + (curr_pythonpath ? curr_pythonpath : ""));
   std::cout << "pythonpath: " << pythonpath << std::endl;
   _putenv_s("PYTHONPATH", pythonpath.c_str());
 #elif __linux__
-  auto path(std::string(BUILD_DIR) + "/bin" + ":" + std::getenv("PATH")); // fixme: could be /bin/Debug or /bin/Release for systems such as Xcode
+  auto path(std::string(BUILD_DIR) + "/bin" + ":" + (curr_path ? curr_path : ""));
   std::cout << "path: " << path << std::endl;
   setenv("PATH", path.c_str(), true);
-  auto pythonpath(std::string(DEPS_DIR) + "/lib/python3.7/site-packages" + ":" + std::getenv("PYTHONPATH"));
+  auto pythonpath(std::string(DEPS_DIR) + "/lib/python3.7/site-packages" + ":" + (curr_pythonpath ? curr_pythonpath : ""));
   std::cout << "pythonpath: " << pythonpath << std::endl;
   setenv("PYTHONPATH", pythonpath.c_str(), true);
-  auto ld_library_path(std::string(DEPS_DIR) + "/lib" + ":" + std::string("QT_LIBDIR") + ":" + std::getenv("LD_LIBRARY_PATH"));
+  auto ld_library_path(std::string(DEPS_DIR) + "/lib" + ":" + std::string(QT_LIBDIR) + ":" + (curr_ld_library_path ? curr_ld_library_path : ""));
   std::cout << "ld_library_path: " << ld_library_path << std::endl;
   setenv("LD_LIBRARY_PATH", ld_library_path.c_str(), true);
 #else
-  auto path(std::string(BUILD_DIR) + "/bin" + ":" + std::getenv("PATH")); // fixme: could be /bin/Debug or /bin/Release for systems such as Xcode
+  auto path(std::string(BUILD_DIR) + "/bin" + ":" + (curr_path ? curr_path : "")); // fixme: could be /bin/Debug or /bin/Release for systems such as Xcode
+  std::cout << "path: " << path << std::endl;
   setenv("PATH", path.c_str(), true);
 #endif
 
@@ -39,7 +44,6 @@ void shapeworksEnvSetup() // fixme: use googletest's setup/teardown: https://git
 
   // change to the shapeworksTest directory
   auto shapeworksTestsDir(std::string(TEST_DATA_DIR) + "/../shapeworksTests");
-  std::cout << "changing directory to " << shapeworksTestsDir << std::endl;
   chdir(shapeworksTestsDir.c_str());
 
   std::cout << "test environment setup complete!" << std::endl;
