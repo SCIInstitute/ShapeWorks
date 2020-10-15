fb3a5a58cf1d7bbe70babc69674104a090afb258
Alan Morris <akenmorris@gmail.com>
2020-10-15 14:55:02 -0600

    Really try without the --force this time

--- Support/deploy_docs.sh
+++ Support/deploy_docs.sh
@@ -8,13 +8,10 @@ remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITO
 git remote rm origin
 git remote add origin "${remote_repo}"
 
-
 git branch -D gh-pages
 git checkout gh-pages
 git pull --rebase
 git checkout master
-mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/mkdocs.yml" --force
-
-#mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/mkdocs.yml" --force
+mkdocs gh-deploy --config-file "${GITHUB_WORKSPACE}/mkdocs.yml"
 
       
