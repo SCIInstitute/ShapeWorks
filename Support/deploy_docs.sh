b9f65ce22145b1d5c727906777499260e0d3e1ff
Alan Morris <akenmorris@gmail.com>
2020-10-16 12:03:23 -0600

    Fix install path, reset repo status prior to doc deploy stuff

--- Support/deploy_docs.sh
+++ Support/deploy_docs.sh
@@ -22,6 +22,7 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
+git reset --hard HEAD
 git remote rm origin
 git remote add origin "${remote_repo}"
 
