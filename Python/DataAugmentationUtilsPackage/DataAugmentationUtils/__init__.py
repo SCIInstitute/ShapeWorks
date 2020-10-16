fe197b89d9e173da8da72bd972b68ddd95f91cb9
jadie1 <jadieraeadams@gmail.com>
2020-10-16 13:43:28 -0600

    Addressing issue 759

--- Python/DataAugmentationUtilsPackage/DataAugmentationUtils/__init__.py
+++ Python/DataAugmentationUtilsPackage/DataAugmentationUtils/__init__.py
@@ -11,7 +11,7 @@ Runs data augmentation and takes the following arguements:
 - aug_type = type of augmentaion to run, 1 is for aug based only on particles, 2 bases aug on images and particles
 - sampler_type = type of distribution to represent embedded data with for sampling
 '''
-def runDataAugmentation(out_dir, img_list, point_list, num_samples, num_dim=0, sampler_type="KDE", mixture_num=0):
+def runDataAugmentation(out_dir, img_list, point_list, num_samples, num_dim=0, sampler_type="kde", mixture_num=0):
     print("Running point based data augmentation.")
     DataAugmentation.point_based_aug(out_dir, img_list, point_list, num_samples, num_dim, sampler_type, mixture_num)
     print("Done.")
