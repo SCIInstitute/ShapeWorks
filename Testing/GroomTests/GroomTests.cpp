9573113b57ade8f1b553a0dc99b49d195f1629d1
Alan Morris <akenmorris@gmail.com>
2020-11-12 11:05:32 -0700

    Adding basic groom test

---
+++ Testing/GroomTests/GroomTests.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+#include <itkApproximateSignedDistanceMapImageFilter.h>
+
+#include "Testing.h"
+
+#include <Libs/Groom/Groom.h>
+#include <Libs/Project/Project.h>
+#include "ParticleShapeStatistics.h"
+
+using namespace shapeworks;
+
+//---------------------------------------------------------------------------
+TEST(GroomTests, sample_test)
+{
+
+  std::string test_location = std::string(TEST_DATA_DIR) + std::string("/sphere");
+  chdir(test_location.c_str());
+
+  // make sure we clean out at least one necessary file to make sure we re-run
+  std::remove("sphere10_DT.nrrd");
+
+  ProjectHandle project = std::make_shared<Project>();
+  project->load("sphere.xlsx");
+  Groom app(project);
+  bool success = app.run();
+  ASSERT_TRUE(success);
+
+  Image image("sphere10_DT.nrrd");
+  Image ground_truth("sphere10_DT_baseline.nrrd");
+
+  ASSERT_TRUE(image == ground_truth);
+
+}
