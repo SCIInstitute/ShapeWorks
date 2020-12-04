ab8aa206d323b9eb36170f90de5d37a459465f46
Krithika Iyer <iyerkrithika21@gmail.com>
2020-12-04 08:53:28 -0700

    added function to generate segmentation images from meshes

--- Python/ShapeCohortGenPackage/ShapeCohortGen/CohortGenUtils.py
+++ Python/ShapeCohortGenPackage/ShapeCohortGen/CohortGenUtils.py
@@ -2,7 +2,10 @@ import os
 import itk
 import numpy as np
 import subprocess
-
+import shutil
+import sys
+sys.path.append("../../../Examples/Python/")
+import GroomUtils
 '''
 Make folder
 '''
@@ -21,6 +24,28 @@ def get_files(folder):
 		file_list.append(file_path)
 	file_list = sorted(file_list)
 	return file_list
+
+
+'''
+Get files with specific extensions
+'''
+def get_file_with_ext(file_list,extension):
+
+	extList =[]
+	for file in file_list:
+		ext = file.split(".")[1]
+		if(ext==extension):
+			extList.append(file)
+	return extList
+
+'''
+Delete files with specific extensions
+'''
+def clean_dir(folder,extension):
+	for filename in os.listdir(folder):
+		
+		if(extension in filename):
+			os.remove(folder+"/"+filename)
 
 '''
 Generates segmentation from meshes by rasterizing
@@ -73,3 +98,135 @@ def apply_noise(img, foreground_mean, foreground_var, background_mean, backgroun
 	background_noise[foreground_indices] = 0
 	noisy_img = img + foreground_noise + background_noise
 	return noisy_img
+
+
+def getMeshInfo(outDir, meshList, spacing):
+    # get meshes in vtk format
+    if(len(meshList)>1):
+    	meshListStr = ''
+    	name = "_"
+    	for mesh in meshList:
+    		meshListStr += mesh + '\n'
+    elif(len(meshList) ==1):
+    	meshListStr = meshList[0]
+    	name = meshList[0].split("/")[1].split(".")[0]
+
+    # Write XML
+    xmlfilename = outDir + "MeshInfo.xml"
+    out_origin = outDir + "origin.txt"
+    out_size = outDir + "size.txt"
+    xml = open(xmlfilename, "a")
+    xml.write("<?xml version=\"1.0\" ?>\n")
+    xml.write("<mesh>\n")
+    xml.write(meshListStr+"\n")
+    xml.write("</mesh>\n")
+    xml.write("<spacing_x>\n" + str(spacing[0]) + "\n</spacing_x>\n")
+    xml.write("<spacing_y>\n" + str(spacing[1]) + "\n</spacing_y>\n")
+    xml.write("<spacing_z>\n" + str(spacing[2]) + "\n</spacing_z>\n")
+    xml.write("<out_origin_filename>\n" + out_origin + "\n</out_origin_filename>\n")
+    xml.write("<out_size_filename>\n" + out_size + "\n</out_size_filename>\n")
+    xml.close()
+    execCommand = ["ComputeRasterizationVolumeOriginAndSize", xmlfilename]
+    
+    subprocess.check_call(execCommand)
+    os.remove(xmlfilename)
+    origin_file = open(out_origin, 'r')
+    origin = np.array(origin_file.read().split()).astype(int)
+    size_file = open(out_size, 'r')
+    size = np.array(size_file.read().split()).astype(int)
+    os.remove(out_origin)
+    os.remove(out_size)
+    return origin, size
+
+
+def generate_segmentations(meshes,out_dir):
+	segDir = out_dir+"/segmentations"
+	meshDir = out_dir+"/meshes"
+
+	#Get Mesh lists
+	PLYmeshList = get_file_with_ext(meshes,'ply')
+	VTKMeshList = get_file_with_ext(meshes,'vtk')
+	
+	#Spacing should this be user defned ? 
+	spacing = [1,1,2]
+
+	#get the origin and size based on all the meshes in the VTKMeshList
+	origin, size = getMeshInfo(meshDir, VTKMeshList, spacing)
+	all_mesh_data = {}
+	all_mesh_data["origin"] = origin
+	all_mesh_data["size"] = size
+	all_mesh_data["spacing"] = spacing
+
+	segList= []
+	meshIndex=0
+
+	numMeshes = len(VTKMeshList)
+	meshIndexArray = np.array(list(range(numMeshes)))
+	#Randomly select 20% meshes for boundary touching samples
+	subSampleSize = int(0.2*numMeshes)
+	randomSamples = np.random.choice(meshIndexArray,subSampleSize,replace=False)
+
+	#Use the PLY format of meshes to generate Segmentation images
+	for mesh in PLYmeshList:
+		mesh_name = os.path.basename(mesh)
+		prefix = mesh_name.split(".")[0] 
+		print("prefix is " , prefix)
+
+		# write xml file
+		infoPrefix = os.path.join(segDir, prefix)
+		xmlfilename = infoPrefix + "_GenerateBinaryAndDT.xml"
+		if os.path.exists(xmlfilename):
+			os.remove(xmlfilename)
+		xml = open(xmlfilename, "a")
+		xml.write("<?xml version=\"1.0\" ?>\n")
+		xml.write("<mesh>\n")
+		xml.write(mesh+"\n")
+		xml.write("</mesh>\n")
+
+		#If the meshIndex is in the randomly selected samples, get the origin and size 
+		# of that mesh so that the segmentation image touch the boundary
+		if(meshIndex in randomSamples):
+			m_origin, m_size = getMeshInfo(meshDir, [VTKMeshList[meshIndex]], spacing)
+			m_data = {}
+			m_data["origin"] = m_origin
+			m_data["size"] = m_size 
+			m_data["spacing"] = spacing
+			data = m_data
+		#Else use the origin and size calculated using the entire cohort of meshes
+		else:
+			data = all_mesh_data
+
+		meshIndex+=1
+
+		#List of image axes
+		axes = np.array(["x","y","z"])
+		#Randomly select two axes which will be padded
+		selectedAxes = np.random.choice(axes,2,replace = False)
+
+		# Write origin, size, and spacing data
+		for key,value in data.items():
+			index = 0
+			for dim in ["x","y","z"]:
+				#If the dim is in the random selection of axes, add padding
+				if dim in selectedAxes and key=="size":
+					#Random padding
+					e = np.random.randint(20,30,size=1)
+					val = str(value[index] + e[0] )
+				else:
+					val = str(value[index])
+				xml.write("<" + key + "_" + dim + ">" + str(val) + "</" + key + "_" + dim + ">\n")
+				index += 1
+		xml.close()
+		print("########### Turning Mesh To Volume ##############")
+		segFile = GroomUtils.rename(mesh, segDir, "", ".nrrd")
+		# call generate binary and DT
+		execCommand = ["GenerateBinaryAndDTImagesFromMeshes", xmlfilename]
+		subprocess.check_call(execCommand)
+		os.remove(xmlfilename)
+		# save output volume
+		output_volume = mesh.replace(".ply", ".rasterized_sp" + str(spacing[0]) + ".nrrd")
+		shutil.move(output_volume, segFile)
+		segList.append(segFile)
+	# Clean the mesh directory of all the unwanted .nrrd files
+	clean_dir(meshDir,'nrrd')
+	return segList
