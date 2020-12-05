8a171282e24447660f4fe50ed2ec761d5347ba92
Alan Morris <akenmorris@gmail.com>
2020-12-05 11:24:31 -0700

    Try using GITHUB_REF

--- Support/package_windows.sh
+++ Support/package_windows.sh
@@ -9,7 +9,8 @@ VERSION=$1
 PLATFORM="windows"
 
 if [[ "$VERSION" == "tag" ]]; then
-    VERSION="ShapeWorks-$(git describe --tags)-${PLATFORM}"
+    #    VERSION="ShapeWorks-$(git describe --tags)-${PLATFORM}"
+    VERSION="ShapeWorks-${GITHUB_REF#refs/heads/}-${PLATFORM}"
 fi
 
 # Special case for when we are on the master branch (dev releases)
