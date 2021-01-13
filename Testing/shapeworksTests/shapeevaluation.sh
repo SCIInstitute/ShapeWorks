743bfbffc448577fb91561417549b28c59609bc9
Karthik Karanth <karanth.karthik@gmail.com>
2021-01-12 20:38:38 -0700

    use python for floating point comparison

--- Testing/shapeworksTests/shapeevaluation.sh
+++ Testing/shapeworksTests/shapeevaluation.sh
@@ -1,3 +1,16 @@
-#! /bin/bash
+#!/bin/bash
 
-shapeworks readparticlesystem --names $DATA/ellipsoid_particles/* -- compactness generalization specificity | diff - $DATA/shapeevaluation.txt
+truth=`cut -d' ' -f4 $DATA/shapeevaluation.txt | tr '\n' ' '`
+
+if ! result=`shapeworks readparticlesystem --names $DATA/ellipsoid_particles/* -- compactness generalization specificity | cut -d' ' -f4 | tr '\n' ' '`; then
+    exit 1
+fi
+
+script="import sys
+lhs = [float(x) for x in \"$truth\".split()]
+rhs = [float(x) for x in \"$result\".split()]
+for a,b in zip(lhs, rhs):
+    if abs(a-b) > 1e-1:
+        sys.exit(1)
+"
+python -c "$script"
