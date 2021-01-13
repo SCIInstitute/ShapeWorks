2210d4d5e638242881f3dca0f15c8e5c4437ddfc
Karthik Karanth <karanth.karthik@gmail.com>
2021-01-12 22:53:20 -0700

    try fixing tests on windows: pass numbers as args instead

--- Testing/shapeworksTests/shapeevaluation.sh
+++ Testing/shapeworksTests/shapeevaluation.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-truth=`cut -d' ' -f4 $DATA/shapeevaluation.txt | tr '\n' ' '`
+truth=`cut -d' ' -f4 $DATA/shapeevaluation.txt`
 
-if ! result=`shapeworks readparticlesystem --names $DATA/ellipsoid_particles/* -- compactness generalization specificity | cut -d' ' -f4 | tr '\n' ' '`; then
+if ! result=`shapeworks readparticlesystem --names $DATA/ellipsoid_particles/* -- compactness generalization specificity | cut -d' ' -f4`; then
     exit 1
 fi
 
 script="import sys
-lhs = [float(x) for x in \"$truth\".split()]
-rhs = [float(x) for x in \"$result\".split()]
+lhs = [float(x) for x in sys.argv[1].split()]
+rhs = [float(x) for x in sys.argv[2].split()]
 for a,b in zip(lhs, rhs):
     if abs(a-b) > 1e-1:
         sys.exit(1)
 "
-python -c "$script"
+python -c "$script" "$truth" "$result"
