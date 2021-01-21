890ec024a3f2bb33d6123a3a76fe80c294955b4c
Alan Morris <akenmorris@gmail.com>
2021-01-21 09:46:06 -0700

    Commenting out debug output.

--- Libs/Mesh/MeshUtils.cpp
+++ Libs/Mesh/MeshUtils.cpp
@@ -84,9 +84,9 @@ Eigen::MatrixXd MeshUtils::generateWarpMatrix(Eigen::MatrixXd TV , Eigen::Matrix
 		Eigen::VectorXi J = Eigen::VectorXi::LinSpaced(TV.rows(),0,TV.rows()-1);
 		Eigen::VectorXd sqrD;
 		Eigen::MatrixXd _2;
-		std::cout<<"Finding closest points..."<<std::endl;
+    //std::cout<<"Finding closest points..."<<std::endl;
 		igl::point_mesh_squared_distance(Vref,TV,J,sqrD,b,_2);
-		std::cout << "sqrd " <<sqrD.minCoeff() <<std::endl;
+    //std::cout << "sqrd " <<sqrD.minCoeff() <<std::endl;
 	}
   // force perfect positioning, rather have popping in low-res than high-res.
   // The correct/elaborate thing to do is express original low.V in terms of
@@ -95,13 +95,13 @@ Eigen::MatrixXd MeshUtils::generateWarpMatrix(Eigen::MatrixXd TV , Eigen::Matrix
   // list of points --> list of singleton lists
   std::vector<std::vector<int> > S;
   igl::matrix_to_list(b,S);
-  std::cout<<"Computing weights for "<<b.size()<<
-    " handles at "<<TV.rows()<<" vertices..."<<std::endl;
+  //std::cout<<"Computing weights for "<<b.size()<<
+    //" handles at "<<TV.rows()<<" vertices..."<<std::endl;
   // Technically k should equal 3 for smooth interpolation in 3d, but 2 is
   // faster and looks OK
   const int k = 2;
   igl::biharmonic_coordinates(TV,TF,S,k,W);
-  std::cout<<"Reindexing..."<< std::endl;
+  //std::cout<<"Reindexing..."<< std::endl;
   std::cout << W.rows() << " " << W.cols() << std::endl;
   // Throw away interior tet-vertices, keep weights and indices of boundary
   Eigen::VectorXi I,J;
@@ -110,7 +110,7 @@ Eigen::MatrixXd MeshUtils::generateWarpMatrix(Eigen::MatrixXd TV , Eigen::Matrix
   std::for_each(b.data(),b.data()+b.size(),[&I](int & a){a=I(a);});
   igl::slice(Eigen::MatrixXd(TV),J,1,TV);
   igl::slice(Eigen::MatrixXd(W),J,1,W);
-  std::cout << "It's done!!" << std::endl;
+  //std::cout << "It's done!!" << std::endl;
   return W;
 }
 
