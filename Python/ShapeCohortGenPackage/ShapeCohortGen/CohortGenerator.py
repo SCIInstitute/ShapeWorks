ba6d14ae4b81551bebec1e01c498ae8de5f01604
jadie1 <jadieraeadams@gmail.com>
2021-02-10 22:43:51 -0700

    Making generate_image arguements accessible

--- Python/ShapeCohortGenPackage/ShapeCohortGen/CohortGenerator.py
+++ Python/ShapeCohortGenPackage/ShapeCohortGen/CohortGenerator.py
@@ -12,11 +12,11 @@ class CohortGenerator():
 			return
 		self.segs = CohortGenUtils.generate_segmentations(self.meshes, self.out_dir, randomize_size, spacing, allow_on_boundary)
 		return self.segs
-	def generate_images(self):
+	def generate_images(self, blur_factor=1, foreground_mean=180, foreground_var=30, background_mean=80, background_var=30):
 		if not self.segs:
 			print("Error: No segmentations have been generated to get images from.\n Call 'generate_segmentations' first.")
 			return
-		self.images = CohortGenUtils.generate_images(self.segs, self.out_dir, blur_factor=1, foreground_mean=180, foreground_var=30, background_mean=80, background_var=30)
+		self.images = CohortGenUtils.generate_images(self.segs, self.out_dir, blur_factor, foreground_mean, foreground_var, background_mean, background_var)
 		return self.images
 
 class EllipsoidCohortGenerator(CohortGenerator):
