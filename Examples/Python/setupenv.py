37e4e7a73371e489fd71a10af45782379efee96e
Alan Morris <akenmorris@gmail.com>
2021-02-20 11:52:13 -0700

    Remove more OSX studio bundle stuff

--- Examples/Python/setupenv.py
+++ Examples/Python/setupenv.py
@@ -32,17 +32,13 @@ def setup_shapeworks_env(shapeworks_bin_dir = None,   # path to the binary direc
         else: # Linux
             shapeworks_bin_dir = "../../../../bin"
         
-    # add shapeworks (and studio on mac) directory to python path 
+    # add shapeworks directory to python path 
     sys.path.append(shapeworks_bin_dir)
-    if platform.system() == "Darwin": # MacOS
-        sys.path.append(shapeworks_bin_dir + "/ShapeWorksStudio.app/Contents/MacOS")
     
-    # add shapeworks and studio to the system path
+    # add shapeworks to the system path
     os.environ["PATH"] = shapeworks_bin_dir + os.pathsep + os.environ["PATH"]
     if dependencies_bin_dir is not None:
         os.environ["PATH"] = dependencies_bin_dir + os.pathsep + os.environ["PATH"]
-    if platform.system() == "Darwin": # MacOS
-        os.environ["PATH"] = shapeworks_bin_dir + "/ShapeWorksStudio.app/Contents/MacOS" + os.pathsep + os.environ["PATH"]
     
     if verbose:
         print_python_path()
