f8bdfbf483a49b9a84eafd3580175477ea16ae74
Karthik Karanth <karanth.karthik@gmail.com>
2021-02-28 00:44:52 -0700

    Try robin_hood unordered_map: not much difference in performance

--- Libs/Optimize/ParticleSystem/FixedSizeCache.h
+++ Libs/Optimize/ParticleSystem/FixedSizeCache.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <unordered_map>
-#include <unordered_set>
+#include <robin_hood.h>
 
 template<class Key, class Value>
 struct FixedSizeCache {
@@ -14,7 +13,7 @@ struct FixedSizeCache {
    * This class is pretty bare-bones because a lot of operations like "resizing entries to correct size" are application-specific
    */
 
-  std::unordered_map<Key, int> key2entry;
+  robin_hood::unordered_map<Key, int> key2entry;
   std::vector<Value> entries;
 
   // clear everything in the cache except what is required by the active_keys. What is "active" is determined by the
@@ -22,8 +21,8 @@ struct FixedSizeCache {
   void clear_except_active(const std::vector<Key>& active_keys) {
 
     // figure out which (unique) active_keys we need to keep
-    std::unordered_map<Key, int> to_keep;
-    std::unordered_set<int> taken; // keep track of the corresponding entry locations
+    robin_hood::unordered_map<Key, int> to_keep;
+    robin_hood::unordered_set<int> taken; // keep track of the corresponding entry locations
     for(const Key& key : active_keys) {
       const auto entry = key2entry.find(key);
       if(entry != key2entry.end()) {
@@ -50,7 +49,7 @@ struct FixedSizeCache {
         continue;
       }
 
-      // is this spot already taken by a triangle we wish to keep?
+      // is this spot already taken by a key we wish to keep?
       while(taken.find(next_idx) != taken.end()) {
         next_idx++;
       }
