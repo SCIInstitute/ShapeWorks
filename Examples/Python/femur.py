69868ec0b7f9b2a5701639efca98c75ea282274f
jadie1 <jadieraeadams@gmail.com>
2021-03-04 15:53:17 -0700

    Moving interactive step to very beginning of femur and femur_cut use cases

--- Examples/Python/femur.py
+++ Examples/Python/femur.py
@@ -32,8 +32,22 @@ def Run_Pipeline(args):
     outputDirectory = "Output/femur/"
     if not os.path.exists(outputDirectory):
         os.makedirs(outputDirectory)
-    
 
+    if not args.groom_images:
+        # set spacing
+        spacing = [1.0, 1.0, 1.0]
+        answer = input("Use ispotropic spacing for mesh rasterization? y/n \n")
+        if answer == 'n':
+            done = False
+            while not done:
+                spacing = []
+                spacing.append(float(input("Enter x spacing:\n")))
+                spacing.append(float(input("Enter y spacing:\n")))
+                spacing.append(float(input("Enter z spacing:\n")))
+                answer2 = input('Is spacing = ' + str(spacing) + ' okay? y/n\n')
+                if answer2 == 'y':
+                    done = True
+    
     if args.tiny_test:
         args.use_single_scale = True
         args.interactive = False
@@ -223,19 +237,6 @@ def Run_Pipeline(args):
             MeshesToVolumes - Shapeworks requires volumes so we need to convert 
             mesh segementaions to binary segmentations.
             """
-            # set spacing
-            spacing = [1.0, 1.0, 1.0]
-            answer = input("Use ispotropic spacing for mesh rasterization? y/n \n")
-            if answer == 'n':
-                done = False
-                while not done:
-                    spacing = []
-                    spacing.append(float(input("Enter x spacing:\n")))
-                    spacing.append(float(input("Enter y spacing:\n")))
-                    spacing.append(float(input("Enter z spacing:\n")))
-                    answer2 = input('Is spacing = ' + str(spacing) + ' okay? y/n\n')
-                    if answer2 == 'y':
-                        done = True
 
             fileList_seg = MeshesToVolumes(groomDir + "volumes", reflectedFiles_mesh, spacing)
 
