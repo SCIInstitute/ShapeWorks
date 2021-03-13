8dc0264234cd62b8c328fcec930f05e01c32cad7
Karthik Karanth <karanth.karthik@gmail.com>
2021-03-13 10:28:10 -0700

    try explicitly adding pkg-config

--- conda_installs.sh
+++ conda_installs.sh
@@ -98,7 +98,8 @@ function install_conda() {
     openexr=2.5.3 \
     pybind11=2.5.0 \
     notebook=6.1.5 \
-    nbformat=4.4.0
+    nbformat=4.4.0 \
+    pkg-config=0.29.2
   then return 1; fi
 
   # linux and mac (only) deps
