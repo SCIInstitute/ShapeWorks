0766ad304a45522a67805f6d1907ba31b97b63fa
Cameron Christensen <cam@sci.utah.edu>
2021-03-31 13:29:07 -0600

    missing changes pr #1180

--- Examples/Python/CommonUtils.py
+++ Examples/Python/CommonUtils.py
@@ -78,7 +78,7 @@ def download_subset(use_case,datasetName,outputDirectory):
 		if(generate_download_flag(outputDirectory,"shape_models/femur/mean/")):
 			meanFilesList = sorted([files for files in fileList if re.search("^shape_models(?:/|\\\)femur(?:/|\\\)mean(?:/|\\\).*",files)])
 			DatasetUtils.downloadDataset(datasetName,destinationPath=outputDirectory,fileList = meanFilesList)
-	
+
 def download_and_unzip_dataset(datasetName, outputDirectory):
 	# Check if the unzipped data is present and number of files are more than 3 for full use case
 	if generate_download_flag(outputDirectory,datasetName):
