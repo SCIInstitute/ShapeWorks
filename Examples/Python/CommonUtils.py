21e783938cb265a858d5fd60cfddd384fb315083
jessdtate <jess@sci.utah.edu>
2021-04-04 01:28:46 -0600

    missed a merge marker

--- Examples/Python/CommonUtils.py
+++ Examples/Python/CommonUtils.py
@@ -80,10 +80,6 @@ def download_subset(use_case,datasetName,outputDirectory):
 			meanFilesList = sorted([files for files in fileList if re.search("^shape_models(?:/|\\\)femur(?:/|\\\)mean(?:/|\\\).*",files)])
 			DatasetUtils.downloadDataset(datasetName,destinationPath=outputDirectory,fileList = meanFilesList)
 
-<<<<<<< HEAD
-
-=======
->>>>>>> f453277fb18fb102ae24ca09ff2fcbe07fe61425
 def download_and_unzip_dataset(datasetName, outputDirectory):
 	# Check if the unzipped data is present and number of files are more than 3 for full use case
 	if generate_download_flag(outputDirectory,datasetName):
