e36dfbe07f1e8f689a491ffcd4780a62344dfe28
Karthik Karanth <karanth.karthik@gmail.com>
2021-05-14 16:46:11 -0600

    add openblas

--- Support/package.sh
+++ Support/package.sh
@@ -106,7 +106,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     cd ..
 else
     # Copy libraries from anaconda
-    conda_libs="libcholmod libboost_system libboost_iostreams libboost_filesystem libbz2 liblzma liblz4 libtbb libHalf libpython libzstd"
+    conda_libs="libopenblas libcholmod libboost_system libboost_iostreams libboost_filesystem libbz2 liblzma liblz4 libtbb libHalf libpython libzstd"
     for clib in $conda_libs; do
         cp ${CONDA_PREFIX}/lib/${clib}* lib
     done
