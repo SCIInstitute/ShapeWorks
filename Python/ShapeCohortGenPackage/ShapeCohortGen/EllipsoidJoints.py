cebe9beca4ea6af94ae8e9b8d7e356ab41258bf1
Krithika Iyer <iyerkrithika21@gmail.com>
2021-05-24 16:35:03 -0600

    added comments

--- Python/ShapeCohortGenPackage/ShapeCohortGen/EllipsoidJoints.py
+++ Python/ShapeCohortGenPackage/ShapeCohortGen/EllipsoidJoints.py
@@ -8,7 +8,7 @@ from ShapeCohortGen.Ellipsoids import *
 
 def generate_ellipsoid_joint(num_samples, meshDir, randomize_center, randomize_x_radius, randomize_y_radius, randomize_z_radius,mode_size,mode_rotation,separation=0):
 
-
+	# randomize radii
 	if randomize_x_radius:
 		x_radius = np.random.randint(low =15,high=25,size =1)[0]
 	else:
@@ -24,7 +24,7 @@ def generate_ellipsoid_joint(num_samples, meshDir, randomize_center, randomize_x
 	radii = [x_radius,y_radius,z_radius]
 
 
-
+	# if the mode of variation is rotation, then the second ellipsoid will be rotated uniformly
 	if(mode_rotation==True):
 		apply_rotation_flag = 1
 	else:
@@ -51,19 +51,17 @@ def generate_ellipsoid_joint(num_samples, meshDir, randomize_center, randomize_x
 		else:
 			center_loc_1 = [0,0,0]
 
-
+		# if mode of variation is size, then the radii of the second ellipsoid are randomized
 		if(mode_size == True):
 			radii_2 = [np.random.randint(low =15,high=25,size =1)[0],\
 					   np.random.randint(low =5,high=15,size =1)[0],\
 					   np.random.randint(low =5,high=15,size =1)[0]]
+		# if size is not the selected mode of variation, both ellipsoids will have same radii
 		else:
 			radii_2 = radii_1
 
 
-
-
-
-		
+		#ellipsoid 1 
 		ellipsoid_joint1 = addEllipsoid(center_loc_1,radii_1,0,rotation_axis='Y')
 		#Write to .ply file
 		ply_writer = vtk.vtkPLYWriter()
@@ -76,17 +74,21 @@ def generate_ellipsoid_joint(num_samples, meshDir, randomize_center, randomize_x
 		vtk_writer.SetFileName(vtkFileName1)
 		vtk_writer.Update()
 
+		# if mode rotation is not selected, this will be zero
 		rotation = i*increments*apply_rotation_flag
+		# change the center of ellipsoid 2 such that it is above ellipsoid 1 
+		# distance between the two surfaces = separation
 		location_offset = [0,radii_1[1]*2 + separation,0]
 		center_loc_2 = list( map(add, center_loc_1, location_offset) )
 
+		#ellipsoid 2
 		ellipsoid_joint2 = addEllipsoid(center_loc_2,radii_2,rotation,rotation_axis='Y')
-
+		#write to ply
 		ply_writer = vtk.vtkPLYWriter()
 		ply_writer.SetInputData(ellipsoid_joint2.GetOutput())
 		ply_writer.SetFileName(plyFileName2)
 		ply_writer.Update()
-
+		#write to vtk
 		vtk_writer = vtk.vtkPolyDataWriter()
 		vtk_writer.SetInputData(ellipsoid_joint2.GetOutput())
 		vtk_writer.SetFileName(vtkFileName2)
