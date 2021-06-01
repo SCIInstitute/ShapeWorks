c8a0a9c745b8a10bcd7aba307a9e19e92516c0c4
archanasri <archanasri@sci.utah.edu>
2021-06-01 13:19:36 -0600

    Adding more decimate tests

--- Testing/PythonTests/decimate.py
+++ Testing/PythonTests/decimate.py
@@ -27,3 +27,42 @@ val = decimateTest2()
 
 if val is False:
   sys.exit(1)
+
+def decimateTest3():
+  mesh = Mesh(os.environ["DATA"] + "/femur.vtk")
+  mesh.decimate()
+
+  compareMesh = Mesh(os.environ["DATA"] + "/decimate3.vtk")
+
+  return mesh == compareMesh
+
+val = decimateTest3()
+
+if val is False:
+  sys.exit(1)
+
+def decimateTest4():
+  mesh = Mesh(os.environ["DATA"] + "/femur.vtk")
+  mesh.decimate(0.9, 25.5, False)
+
+  compareMesh = Mesh(os.environ["DATA"] + "/decimate4.vtk")
+
+  return mesh == compareMesh
+
+val = decimateTest4()
+
+if val is False:
+  sys.exit(1)
+
+def decimateTest5():
+  mesh = Mesh(os.environ["DATA"] + "/femur.vtk")
+  mesh.decimate(0.9, 25.5)
+
+  compareMesh = Mesh(os.environ["DATA"] + "/decimate5.vtk")
+
+  return mesh == compareMesh
+
+val = decimateTest5()
+
+if val is False:
+  sys.exit(1)
--- Testing/shapeworksTests/decimate.sh
+++ Testing/shapeworksTests/decimate.sh
@@ -3,3 +3,9 @@
 shapeworks readmesh --name $DATA/femur.vtk decimate --reduction 0.0 --angle 0.0 --preservetopology 0 comparemesh --name $DATA/decimate1.vtk
 if [[ $? != 0 ]]; then exit -1; fi
 shapeworks readmesh --name $DATA/femur.vtk decimate --reduction 0.0 --angle 0.0 comparemesh --name $DATA/decimate2.vtk
+if [[ $? != 0 ]]; then exit -1; fi
+shapeworks readmesh --name $DATA/femur.vtk decimate comparemesh --name $DATA/decimate3.vtk
+if [[ $? != 0 ]]; then exit -1; fi
+shapeworks readmesh --name $DATA/femur.vtk decimate --reduction 0.9 --angle 25.5 comparemesh --name $DATA/decimate4.vtk
+if [[ $? != 0 ]]; then exit -1; fi
+shapeworks readmesh --name $DATA/femur.vtk decimate --reduction 0.9 --angle 25.5 --preservetopology 0 comparemesh --name $DATA/decimate5.vtk
