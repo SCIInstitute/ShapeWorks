bbebd05a004bf2b450e823628832d5e9f6a9c9ce
Cameron Christensen <cam@sci.utah.edu>
2021-06-18 17:26:05 -0600

    quotes in the command line are fine, but not in python, and they aren't necessary

--- conda_env_setup.bat
+++ conda_env_setup.bat
@@ -10,7 +10,7 @@ set BIN_PATH=/%BIN_PATH::=%
 for %%F in (activate deactivate) DO (
     if not exist "%CONDA_PREFIX%\etc\conda\%%F.d" mkdir "%CONDA_PREFIX%\etc\conda\%%F.d"
     copy shapeworks-%%F.bat "%CONDA_PREFIX%\etc\conda\%%F.d\shapeworks-%%F.bat"
-    call python .\replace_strings.py "%CONDA_PREFIX%\etc\conda\%%F.d\shapeworks-%%F.bat" shapeworks_placeholder_string "\"%cd%\bin\""
+    call python .\replace_strings.py "%CONDA_PREFIX%\etc\conda\%%F.d\shapeworks-%%F.bat" shapeworks_placeholder_string "%cd%\bin"
 
     :: Copy unix shell activation scripts, needed by Windows Bash users
     copy shapeworks-%%F.sh "%CONDA_PREFIX%\etc\conda\%%F.d\shapeworks-%%F.sh"
