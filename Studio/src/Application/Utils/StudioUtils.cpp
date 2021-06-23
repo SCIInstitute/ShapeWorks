39007fe82c7ec301784bd9363e23bb6ed15e4e46
Alan Morris <akenmorris@gmail.com>
2021-06-23 09:26:33 -0600

    Only ask about multiple domains when num domains is > 1, not > 0 (oops)

--- Studio/src/Application/Utils/StudioUtils.cpp
+++ Studio/src/Application/Utils/StudioUtils.cpp
@@ -7,7 +7,7 @@ namespace shapeworks {
 bool StudioUtils::ask_multiple_domains_as_single(QWidget* parent, std::shared_ptr<Project> project)
 {
   bool single = true;
-  if (project->get_number_of_domains_per_subject() > 0) {
+  if (project->get_number_of_domains_per_subject() > 1) {
     QMessageBox::StandardButton reply;
     reply = QMessageBox::question(parent, "Multiple Domains",
                                   "This export contains multiple domains.\n\n"
