d768dee93609a3e88194115ddbb820867d389520
Alan Morris <akenmorris@gmail.com>
2021-07-06 07:56:28 -0600

    Remove non-verbose output

--- Libs/Optimize/ParticleSystem/Sampler.h
+++ Libs/Optimize/ParticleSystem/Sampler.h
@@ -128,7 +128,6 @@ public:
   void AddImage(ImageType::Pointer image, double narrow_band, std::string name = "");
 
   void ApplyConstraintsToZeroCrossing(){
-      std::cout << "ApplyConstraintsToZeroCrossing " << m_DomainList.size() << std::endl;
       for(size_t i = 0; i < m_DomainList.size(); i++){
           this->m_DomainList[i]->UpdateZeroCrossingPoint();
       }
