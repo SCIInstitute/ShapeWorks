6024e7755e73b7761f79dfa4033442734bc2d06a
archanasri <archanasri@sci.utah.edu>
2021-09-07 11:04:30 -0600

    Fixing typo for mesh test

--- Testing/shapeworksTests/meshcurvature.sh
+++ Testing/shapeworksTests/meshcurvature.sh
@@ -4,4 +4,4 @@ shapeworks read-mesh --name $DATA/m03.vtk mesh-curvature --type mean set-field -
 if [[ $? != 0 ]]; then exit -1; fi
 shapeworks read-mesh --name $DATA/m03.vtk mesh-curvature set-field --name PrincipalCurvature compare-mesh --name $DATA/principalcurvature.vtk
 if [[ $? != 0 ]]; then exit -1; fi
-shapeworks read-mesh --name $DATA/m03.vtk mesh-curvature --type gaussian mean set-field --name GaussianCurvature compare-mesh --name $DATA/gaussiancurvature.vtk
+shapeworks read-mesh --name $DATA/m03.vtk mesh-curvature --type gaussian set-field --name GaussianCurvature compare-mesh --name $DATA/gaussiancurvature.vtk
