c54fe50e272da34869ce7cad94b9989349831002
Alan Morris <akenmorris@gmail.com>
2021-09-13 00:30:26 -0600

    Remove image write

--- Testing/MeshTests/MeshTests.cpp
+++ Testing/MeshTests/MeshTests.cpp
@@ -653,8 +653,6 @@ TEST(MeshTests, warpTest2)
   ASSERT_TRUE(warper.get_warp_available());
 
   Mesh output = warper.build_mesh(movingPoints);
-  output.write("/tmp/check.vtk");
-
   ASSERT_TRUE(output == ellipsoid_warped);
 }
 
