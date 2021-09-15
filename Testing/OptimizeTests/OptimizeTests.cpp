781eebf402e0fad7b65e3b0918712b794268b525
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2021-09-15 11:05:27 -0600

    Reduced iterations in unit tests to speed things up.

--- Testing/OptimizeTests/OptimizeTests.cpp
+++ Testing/OptimizeTests/OptimizeTests.cpp
@@ -399,7 +399,7 @@ TEST(OptimizeTests, sphereConstraint)
       auto violation_report_data = app.GetSampler()->GetParticleSystem()->GetDomain(
         domain)->GetConstraints()->ViolationReportData(p);
 
-      double slack = 6.5e-1;
+      double slack = 3.0e-1;
 
       for (int j = 0; j < 3; j++) {
         for (int k = 0; k < violation_report_data[j].size(); k++) {
@@ -478,7 +478,7 @@ TEST(OptimizeTests, sphereCuttingPlaneConstraint)
       auto violation_report_data = app.GetSampler()->GetParticleSystem()->GetDomain(
         domain)->GetConstraints()->ViolationReportData(p);
 
-      double slack = 1.5e-1;
+      double slack = 3.0e-1;
 
       for (int j = 0; j < 3; j++) {
         for (int k = 0; k < violation_report_data[j].size(); k++) {
@@ -641,7 +641,7 @@ TEST(OptimizeTests, MultiDomainConstraint)
       auto violation_report_data = app.GetSampler()->GetParticleSystem()->GetDomain(
         domain)->GetConstraints()->ViolationReportData(p);
 
-      double slack = 2.5e-1;
+      double slack = 7.5e-1;
 
       for (int j = 0; j < 3; j++) {
         for (int k = 0; k < violation_report_data[j].size(); k++) {
