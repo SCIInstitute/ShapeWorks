5e65a737e3375580e56d87c856dd3c88b1277a0a
Alan Morris <akenmorris@gmail.com>
2021-11-11 10:56:39 -0700

    GHA doesn't have wget

--- Support/build_docs.sh
+++ Support/build_docs.sh
@@ -28,10 +28,10 @@ fi
 
 if [ ! -f $INSTALL_DIR/bin/doxybook2 ] ; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
-	wget "https://github.com/matusnovak/doxybook2/releases/download/v1.4.0/doxybook2-osx-amd64-v1.4.0.zip"
+	curl -o doxybook2-osx-amd64-v1.4.0.zip "https://github.com/matusnovak/doxybook2/releases/download/v1.4.0/doxybook2-osx-amd64-v1.4.0.zip"
 	unzip doxybook2-osx-amd64-v1.4.0.zip bin/doxybook2 -d $INSTALL_DIR
     else
-	wget "https://github.com/matusnovak/doxybook2/releases/download/v1.4.0/doxybook2-linux-amd64-v1.4.0.zip"
+	curl -o doxybook2-linux-amd64-v1.4.0.zip "https://github.com/matusnovak/doxybook2/releases/download/v1.4.0/doxybook2-linux-amd64-v1.4.0.zip"
 	unzip doxybook2-linux-amd64-v1.4.0.zip bin/doxybook2 -d $INSTALL_DIR
     fi
     rm -f doxybook*.zip
