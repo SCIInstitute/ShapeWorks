709fb05d979eb853d7ad5e84609ac37dd91ed016
Alan Morris <akenmorris@gmail.com>
2021-11-12 09:13:45 -0700

    trying scripted cache

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -7,9 +7,12 @@ file=conda-linux-${hash}.tar.gz
 cd /
 scp runner@${CACHE_HOST}:github/${file} .
 if [ -f ${file} ] ; then
+    echo "Cache file was found"
     tar --use-compress-program=pigz -xf $file
 else
+    echo "Cache file was not found"
     cd $RUNNER_WORKSPACE
+    ls
     source ./install_shapeworks.sh
 fi
 
