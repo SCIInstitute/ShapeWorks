a6347a5b2d563f9a27dc0c9ed4f522c241e75465
Alan Morris <akenmorris@gmail.com>
2021-11-19 21:02:52 -0700

    Fix paths

--- .github/workflows/restore_caches.sh
+++ .github/workflows/restore_caches.sh
@@ -12,7 +12,7 @@ cd /
 if [[ "$USE_CCACHE" == "ON" ]]; then
     # Restore ccache
     scp runner@${CACHE_HOST}:github/${CCACHE_FILE} /tmp
-    if [ -f ${CCACHE_FILE} ] ; then
+    if [ -f /tmp/${CCACHE_FILE} ] ; then
 	echo "ccache file was found"
 	tar --use-compress-program=pigz -xf /tmp/${CCACHE_FILE}
 	rm /tmp/$CCACHE_FILE
@@ -21,7 +21,7 @@ fi
 
 # Restore conda installs
 scp runner@${CACHE_HOST}:github/${CONDA_FILE} /tmp
-if [ -f ${CONDA_FILE} ] ; then
+if [ -f /tmp/${CONDA_FILE} ] ; then
     echo "Conda Cache file was found"
     tar --use-compress-program=pigz -xf /tmp/$CONDA_FILE
     rm /tmp/$CONDA_FILE
@@ -29,7 +29,7 @@ fi
 
 # Restore dependencies
 scp runner@${CACHE_HOST}:github/${DEP_FILE} /tmp
-if [ -f ${DEP_FILE} ] ; then
+if [ -f /tmp/${DEP_FILE} ] ; then
     echo "Dependency Cache file was found"
     tar --use-compress-program=pigz -xf /tmp/$DEP_FILE
     rm /tmp/$DEP_FILE
