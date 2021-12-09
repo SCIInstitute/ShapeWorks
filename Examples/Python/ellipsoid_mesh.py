800393cccd837965deafc5d78f74116a15bf1024
jadie1 <jadieraeadams@gmail.com>
2021-12-09 11:25:54 -0700

    Adding alignment to ellipsoid_mesh

--- Examples/Python/ellipsoid_mesh.py
+++ Examples/Python/ellipsoid_mesh.py
@@ -3,7 +3,6 @@
 ====================================================================
 Example ShapeWorks Pipeline for mesh optimization
 ====================================================================
-
 In this example we provide a full pipeline with an example dataset of axis 
 aligned ellipsoid meshes.
 """
@@ -12,17 +11,17 @@ import glob
 import shapeworks as sw
 import OptimizeUtils
 import AnalyzeUtils
+import numpy as np
 
 
 def Run_Pipeline(args):
     print("\nStep 1. Extract Data\n")
     """
     Step 1: EXTRACT DATA
-
     We define dataset_name which determines which dataset to download from 
     the portal and the directory to save output from the use case in. 
     """
-    dataset_name = "ellipsoid_1mode_aligned"
+    dataset_name = "ellipsoid_1mode"
     output_directory = "Output/ellipsoid_mesh/"
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
@@ -45,12 +44,84 @@ def Run_Pipeline(args):
             sample_idx = sw.data.sample_meshes(inputMeshes, int(args.num_subsample))
             mesh_files = [mesh_files[i] for i in sample_idx]
 
-    # This dataset is prealigned and does not require any grooming steps.
+    # If skipping grooming, use the pregroomed meshes from the portal
+    if args.skip_grooming:
+        print("Skipping grooming.")
+        out_directory = output_directory + dataset_name + '/groomed/meshes/'
+        indices = []
+        if args.tiny_test:
+            indices = [0, 1, 2]
+        elif args.use_subsample:
+            indices = sample_idx
+        mesh_files = sw.data.get_file_list(
+            out_directory, ending=".vtk", indices=indices)
 
-    print("\nStep 2. Optimize - Particle Based Optimization\n")
+    # Else groom the meshes for optimization
+    else:
+        print("\nStep 2. Groom - Data Pre-processing\n")
+        """
+        Step 2: GROOMING 
+        The required grooming steps are: 
+        1. Reference selection
+        2. Rigid Alignment
+        For more information on grooming see docs/workflow/groom.md
+        http://sciinstitute.github.io/ShapeWorks/workflow/groom.html
+        """
+
+        # Create a directory for groomed output
+        groom_dir = output_directory + 'groomed/'
+        if not os.path.exists(groom_dir):
+            os.makedirs(groom_dir)
+
+        """
+        First, we need to loop over the mesh files and load them
+        """
+        mesh_list = []
+        mesh_names = []
+        for mesh_file in mesh_files:
+            print('Loading: ' + mesh_file)
+            # get current shape name
+            mesh_names.append(mesh_file.split('/')
+                               [-1].replace('.nrrd', ''))
+            # load mesh
+            mesh = sw.Mesh(mesh_file)
+            # append to the shape list
+            mesh_list.append(mesh)
+
+        """
+        Grooming Step 1: Select a reference
+        This step requires loading all of the meshes at once so the shape
+        closest to the mean can be found and selected as the reference. 
+        """
+        ref_index = sw.find_reference_mesh_index(mesh_list)
+        # Make a copy of the reference segmentation
+        ref_mesh = mesh_list[ref_index].write(groom_dir + 'reference.vtk')
+        ref_name = mesh_names[ref_index]
+        print("Reference found: " + ref_name)
+
+        """
+        Grooming Step 2: Rigid alignment
+        This step rigidly aligns each shape to the selected reference. 
+        """
+        for mesh, name in zip(mesh_list, mesh_names):
+            print('Aligning ' + name + ' to ' + ref_name)
+            # compute rigid transformation
+            rigid_transform = mesh.createTransform(ref_mesh, sw.Mesh.AlignmentType.Rigid)
+            # apply rigid transform
+            mesh.applyTransform(rigid_transform)
+            # apply centering
+            translation = np.eye(4)
+            translation[0:3,-1] = -ref_center
+            mesh.applyTransform(translation)
+
+        # Save groomed meshes
+        mesh_files = sw.utils.save_meshes(groom_dir + 'meshes/', mesh_list,
+                        mesh_names, extension='vtk', compressed=False, verbose=True)
+
+
+    print("\nStep 3. Optimize - Particle Based Optimization\n")
     """
-    Step 2: OPTIMIZE - Particle Based Optimization
-
+    Step 3: OPTIMIZE - Particle Based Optimization
     Now we can run optimization directly on the meshes.
     For more details on the plethora of parameters for shapeworks please refer 
     to docs/workflow/optimze.md
@@ -108,7 +179,6 @@ def Run_Pipeline(args):
     print("\nStep 3. Analysis - Launch ShapeWorksStudio - sparse correspondence model.\n")
     """
     Step 3: ANALYZE - Shape Analysis and Visualization
-
     Now we launch studio to analyze the resulting shape model.
     For more information about the analysis step, see docs/workflow/analyze.md
     http://sciinstitute.github.io/ShapeWorks/workflow/analyze.html
