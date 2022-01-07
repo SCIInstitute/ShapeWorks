a8259ce5a09bdf5d070052ff0b6f569481fa6736
Alan Morris <akenmorris@gmail.com>
2022-01-07 00:36:22 -0700

    Switch to dir instead of files

--- .github/workflows/copy_artifact.sh
+++ .github/workflows/copy_artifact.sh
@@ -7,7 +7,7 @@ echo "#############################"
 . $GITHUB_WORKSPACE/.github/workflows/common.sh
 
 TARGET="/tmp/${1}.${SUFFIX}"
-compress_file $TARGET "${@:2}"
+compress_file $TARGET "${2}"
 
 scp ${TARGET} runner@${CACHE_HOST}:github/artifacts
 rm ${TARGET}
