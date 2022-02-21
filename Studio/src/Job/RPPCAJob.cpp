f33574a84414ed349a46890a1555d32da8951a89
Krithika Iyer <iyerkrithika21@gmail.com>
2022-02-20 18:45:11 -0700

    fixed job, removed reduntant code

--- Studio/src/Job/RPPCAJob.cpp
+++ Studio/src/Job/RPPCAJob.cpp
@@ -1,7 +1,6 @@
 #include <pybind11/embed.h>
 #include <pybind11/stl.h>
 #include <pybind11/eigen.h>
-
 namespace py = pybind11;
 using namespace pybind11::literals; // to bring in the `_a` literal
 
@@ -14,27 +13,53 @@ RPPCAJob::RPPCAJob(ParticleShapeStatistics stats) : stats_(stats)
 
 //---------------------------------------------------------------------------
 void RPPCAJob::run()
-{
+{ 
+  std::cout << "Inside the job" << std::endl;
 
+  auto data = this->stats_.ShapeMatrix();
+  int numSamples = data.cols();
+  int numDimensions = data.rows();
+  auto num_modes = this->stats_.SampleSize();
+  // std::cout << this->stats_->ShapeMatrix().cols() << std::endl;
   py::module sw = py::module::import("shapeworks");
   
   py::object geteigenVecs = sw.attr("stats").attr("get_rrpcaEigenVector");
   py::object geteigenVals = sw.attr("stats").attr("get_rppcaEigenValues");
   py::object getexpVar = sw.attr("stats").attr("get_expVar");
   py::object getmean = sw.attr("stats").attr("get_rppcamean");
-  py::object getXminusMean = sw.attr("stats").attr("get_X_minsMean");
   
-  Eigen::MatrixXd rppcaEigenVectors = geteigenVecs(data,num_modes);
-  Eigen::VectorXd rppcaEigenValues = geteigenVecs(data,num_modes);
-  Eigen::VectorXd rppcaExpVals = getexpVar(data,num_modes);
-  Eigen::VectorXd rppcaMean = getmean(data,num_modes);
-  Eigen::MatrixXd rppcaXminMean = getXminusMean(data,num_modes);
-  this.stats_->set_rppcaEigenVecs(rppcaEigenValues);
-  this.stats_->set_rppcaEigenVals(rppcaEigenValues);
-  this.stats_->set_rppcaExpVar(rppcaExpVals);
-  this.stats_->set_rppcaMean(rppcaMean);
-  this.stats_->set_rppcaXminMean(rppcaXminMean);
-  this.stats_->m_rppca_job_done = true;
+  
+  Eigen::MatrixXd rppcaEigenVectors = geteigenVecs(data,num_modes).cast<Eigen::MatrixXd>();
+  Eigen::VectorXd rppcaEigenValues = geteigenVals(data,num_modes).cast<Eigen::VectorXd>();
+  Eigen::VectorXd rppcaExpVals = getexpVar(data,num_modes).cast<Eigen::VectorXd>();
+  Eigen::VectorXd rppcaMean = getmean(data,num_modes).cast<Eigen::VectorXd>();
+  
+  // for (unsigned int i = 0; i < numSamples; i++) {
+  //   double total = 0.0f;
+  //   for (unsigned int j = 0; j < numDimensions; j++) {
+  //     total += rppcaEigenVectors(j, i) * rppcaEigenVectors(j, i);
+  //   }
+  //   total = sqrt(total);
+
+  //   for (unsigned int j = 0; j < numDimensions; j++) {
+  //     rppcaEigenVectors(j, i) = rppcaEigenVectors(j, i) / (total + 1.0e-15);
+  //   }
+
+  // }
+  
+  
+
+  this->m_rppcaeigenvalues.resize(rppcaEigenValues.size());
+    for(int i = 0; i<rppcaEigenValues.size(); i++){
+      this->m_rppcaeigenvalues[i] = rppcaEigenValues[i];
+    }
+
+  this->m_rppcapercentVarByMode.resize(rppcaExpVals.size());
+    for(int i = 0; i<rppcaExpVals.size(); i++){
+      this->m_rppcapercentVarByMode[i] = rppcaExpVals[i];
+    }
+  this->m_rppcaeigenvectors = rppcaEigenVectors;
+  this->m_rppcamean = rppcaMean;
 }
 
 //---------------------------------------------------------------------------
@@ -43,7 +68,6 @@ QString RPPCAJob::name()
   return "RPPCA Job";
 }
 //---------------------------------------------------------------------------
-
 
 
 }
--- Studio/src/Job/RPPCAJob.h
+++ Studio/src/Job/RPPCAJob.h
@@ -1,23 +1,30 @@
 #pragma once
 #include <Job/Job.h>
 #include <ParticleShapeStatistics.h>
-#include <Eigen/Eigen>
+
 namespace shapeworks {
 
-class RRPCAJob : public Job {
+class RPPCAJob : public Job {
   Q_OBJECT
 public:
 
   RPPCAJob(ParticleShapeStatistics stats);
   void run() override;
   QString name() override;
+  Eigen::MatrixXd RPPCAEigenvectors() { return m_rppcaeigenvectors; }
+  std::vector<double> RPPCAEigenvalues() { return m_rppcaeigenvalues; }
+  Eigen::VectorXd RPPCAMean() { return m_rppcamean; }
+  std::vector<double> RPPCAExpVar() { return m_rppcapercentVarByMode; }
 
  
 
 private:
 
   ParticleShapeStatistics stats_;
-  
+  Eigen::MatrixXd m_rppcaeigenvectors;
+  std::vector<double> m_rppcaeigenvalues;
+  Eigen::VectorXd m_rppcamean;
+  std::vector<double> m_rppcapercentVarByMode;
 
 };
 }
