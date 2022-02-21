4dcecb57db98b2502e3c13817b7dadf7b41a4114
Krithika Iyer <iyerkrithika21@gmail.com>
2022-02-20 18:43:11 -0700

    cleaned up shape statistics class and removed the rppca functions and variables

--- Libs/Particles/ParticleShapeStatistics.h
+++ Libs/Particles/ParticleShapeStatistics.h
@@ -81,15 +81,12 @@ public:
   const Eigen::MatrixXd &Eigenvectors() { return m_eigenvectors; }
   const std::vector<double> &Eigenvalues() { return m_eigenvalues; }
 
-  const Eigen::MatrixXd &RPPCAEigenvectors() { return m_rppcaeigenvectors; }
-  const std::vector<double> &RPPCAEigenvalues() { return m_rppcaeigenvalues; }
-  const Eigen::VectorXd &RPPCAMean() { return m_rppcamean; }
-  const std::vector<double> &RPPCAExpVar() { return m_rppcapercentVarByMode; }
-  const Eigen::MatrixXd &RPPCAPointsMinMean() { return m_rppcapointsMinusMean; }
+  
   /** Returns the mean shape. */
   const Eigen::VectorXd &Mean() { return m_mean; }
   const Eigen::VectorXd &Group1Mean() { return m_mean1; }
   const Eigen::VectorXd &Group2Mean() { return m_mean2; }
+  void SetRPPCAJobDone(bool val){m_rppca_job_done = val;}
 
   // Returns group2 - group1 mean
   const Eigen::VectorXd &NormalizedGroupDifference() { return m_groupdiffnorm; }
@@ -135,42 +132,9 @@ public:
   Eigen::MatrixXd get_group1_matrix();
   Eigen::MatrixXd get_group2_matrix();
 
-  Eigen::MatrixXd getRPPCAMode();
+  
 
-  void set_rppcaEigenVecs(Eigen::MatrixXd X)
-  { 
-    
-    this->m_rppcaeigenvectors = X;
-  }
 
-   void set_rppcaEigenVals(Eigen::VectorXd X)
-  {
-    this->m_rppcaeigenvalues.resize(X.size());
-    for(int i = 0; i<X.size(); i++){
-      this->m_rppcaeigenvalues[i] = X[i];
-    }
-    
-  }
-
-  void set_rppcaMean(Eigen::VectorXd X)
-  {
-    
-    this->m_rppcamean = X;
-    
-  }
-   void set_rppcaExpVar(Eigen::VectorXd X)
-  {
-    
-    this->m_rppcapercentVarByMode.resize(X.size());
-    for(int i = 0; i<X.size(); i++){
-      this->m_rppcapercentVarByMode[i] = X[i];
-    }
-  }
-  void set_rppcaXminMean(Eigen::MatrixXd X)
-  {
-    
-    this->m_rppcapointsMinusMean = X;
-  }
 private:
 
   unsigned int m_numSamples1;
@@ -195,11 +159,6 @@ private:
   std::vector<double> m_percentVarByMode;
   Eigen::VectorXd m_fishersLD;
   Eigen::MatrixXd m_principals;
-  Eigen::MatrixXd m_rppcaeigenvectors;
-  std::vector<double> m_rppcaeigenvalues;
-  Eigen::VectorXd m_rppcamean;
-  std::vector<double> m_rppcapercentVarByMode;
-  Eigen::MatrixXd m_rppcapointsMinusMean;
 
   Eigen::VectorXd m_groupdiff;
   Eigen::VectorXd m_groupdiffnorm;
