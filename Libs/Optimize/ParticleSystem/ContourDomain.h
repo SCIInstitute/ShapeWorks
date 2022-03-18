b1b0a715987bcc7e59082628c67baf3001e3c358
Alan Morris <akenmorris@gmail.com>
2022-03-18 15:00:56 -0600

    Consistent formatting.

--- Libs/Optimize/ParticleSystem/ContourDomain.h
+++ Libs/Optimize/ParticleSystem/ContourDomain.h
@@ -8,52 +8,46 @@
 =========================================================================*/
 #pragma once
 
-#include "itkParticleDomain.h"
-#include <Eigen/Dense>
-#include <vtkPolyData.h>
-#include <vtkLine.h>
-#include <vtkGenericCell.h>
-#include <vtkCellLocator.h>
 #include <itkObjectFactory.h>
+#include <vtkCellLocator.h>
+#include <vtkGenericCell.h>
+#include <vtkLine.h>
+#include <vtkPolyData.h>
 
-namespace itk
-{
-class ContourDomain : public ParticleDomain
-{
-public:
+#include <Eigen/Dense>
+
+#include "itkParticleDomain.h"
+
+namespace itk {
+class ContourDomain : public ParticleDomain {
+ public:
   /** Standard class typedefs */
-  typedef SmartPointer<ContourDomain>  Pointer;
+  typedef SmartPointer<ContourDomain> Pointer;
   itkSimpleNewMacro(ContourDomain);
-
-  /** Point type used to store particle locations. */
-  using PointType = ParticleDomain::PointType;
 
   explicit ContourDomain() {}
   virtual ~ContourDomain() {}
 
   void SetPolyLine(vtkSmartPointer<vtkPolyData> poly_data);
 
-  shapeworks::DomainType GetDomainType() const override {
-    return shapeworks::DomainType::Contour;
-  }
+  shapeworks::DomainType GetDomainType() const override { return shapeworks::DomainType::Contour; }
 
   virtual bool ApplyConstraints(PointType& p, int idx, bool dbg = false) const override;
 
-  virtual PointType UpdateParticlePosition(const PointType &point,
-                                           int idx, VectorDoubleType &update) const override;
+  virtual PointType UpdateParticlePosition(const PointType& point, int idx, VectorDoubleType& update) const override;
 
-  virtual VectorDoubleType ProjectVectorToSurfaceTangent(VectorDoubleType& gradE,
-                                                                            const PointType &pos, int idx) const override;
+  virtual VectorDoubleType ProjectVectorToSurfaceTangent(VectorDoubleType& gradE, const PointType& pos,
+                                                         int idx) const override;
 
-  virtual VectorFloatType SampleNormalAtPoint(const PointType &point, int idx) const override {
+  virtual VectorFloatType SampleNormalAtPoint(const PointType& point, int idx) const override {
     throw std::runtime_error("Contours do not have normals");
   }
 
-  virtual VectorFloatType SampleGradientAtPoint(const PointType &point, int idx) const override {
+  virtual VectorFloatType SampleGradientAtPoint(const PointType& point, int idx) const override {
     throw std::runtime_error("Contours do not have gradients");
   }
 
-  virtual GradNType SampleGradNAtPoint(const PointType &p, int idx) const override {
+  virtual GradNType SampleGradNAtPoint(const PointType& p, int idx) const override {
     throw std::runtime_error("Contours do not have gradient of normals");
   }
 
@@ -63,16 +57,14 @@ public:
   }
 
   virtual double GetMaxDiameter() const override {
-    //todo copied from MeshDomain: should this not be the length of the bounding box diagonal?
+    // todo copied from MeshDomain: should this not be the length of the bounding box diagonal?
     const PointType bb = upper_bound_ - lower_bound_;
     return std::max({bb[0], bb[1], bb[2]});
   }
 
-  virtual void UpdateZeroCrossingPoint() override { }
+  virtual void UpdateZeroCrossingPoint() override {}
 
-  double GetCurvature(const PointType &p, int idx) const override {
-    return GetSurfaceMeanCurvature();
-  }
+  double GetCurvature(const PointType& p, int idx) const override { return GetSurfaceMeanCurvature(); }
 
   virtual double GetSurfaceMeanCurvature() const override {
     // This function is used by MeanCurvatureAttribute which is used for good/bad assessment
@@ -86,18 +78,14 @@ public:
     return 0.02;
   }
 
-  double Distance(const PointType &a, int idx_a, const PointType &b, int idx_b,
-                         VectorDoubleType* out_grad=nullptr) const override;
+  double Distance(const PointType& a, int idx_a, const PointType& b, int idx_b,
+                  VectorDoubleType* out_grad = nullptr) const override;
 
-  double SquaredDistance(const PointType &a, int idx_a, const PointType &b, int idx_b) const override;
+  double SquaredDistance(const PointType& a, int idx_a, const PointType& b, int idx_b) const override;
 
-  const PointType& GetLowerBound() const override {
-    return lower_bound_;
-  }
+  const PointType& GetLowerBound() const override { return lower_bound_; }
 
-  const PointType& GetUpperBound() const override {
-    return upper_bound_;
-  }
+  const PointType& GetUpperBound() const override { return upper_bound_; }
 
   PointType GetZeroCrossingPoint() const override {
     PointType out;
@@ -106,9 +94,7 @@ public:
     return out;
   }
 
-  double GetSurfaceArea() const override {
-    throw std::runtime_error("Contours do not have area");
-  }
+  double GetSurfaceArea() const override { throw std::runtime_error("Contours do not have area"); }
 
   void DeleteImages() override {
     // TODO what?
@@ -119,33 +105,16 @@ public:
 
   void InvalidateParticlePosition(int idx) const override;
 
-  PointType GetPositionAfterSplit(const PointType& pt,
-                                          const VectorDoubleType& local_direction, const VectorDoubleType& global_direction, double epsilon) const override;
+  PointType GetPositionAfterSplit(const PointType& pt, const VectorDoubleType& local_direction,
+                                  const VectorDoubleType& global_direction, double epsilon) const override;
 
-protected:
-  void PrintSelf(std::ostream& os, Indent indent) const override
-  {
+ protected:
+  void PrintSelf(std::ostream& os, Indent indent) const override {
     DataObject::Superclass::PrintSelf(os, indent);
     os << indent << "ContourDomain\n";
   }
 
-private:
-  PointType lower_bound_, upper_bound_;
-
-  vtkSmartPointer<vtkPolyData> poly_data_;
-  vtkSmartPointer<vtkCellLocator> cell_locator_;
-  std::vector<vtkSmartPointer<vtkLine>> lines_;
-
-  // Geodesics between all point pairs. Assumes the number of points is very small
-  Eigen::MatrixXd geodesics_;
-
-  // cache which line a particle is on
-  mutable std::vector<int> particle_lines_;
-  // store some information about the last geodesic query. The next one will most likely reuse this
-  mutable int geo_lq_idx_ = -1;
-  mutable int geo_lq_line_ = -1;
-  mutable double geo_lq_dist_ = -1;
-
+ private:
   void ComputeBounds();
   void ComputeGeodesics(vtkSmartPointer<vtkPolyData> poly_data);
 
@@ -161,6 +130,22 @@ private:
   int NumberOfPoints() const;
 
   Eigen::Vector3d GetPoint(int id) const;
+
+  PointType lower_bound_, upper_bound_;
+
+  vtkSmartPointer<vtkPolyData> poly_data_;
+  vtkSmartPointer<vtkCellLocator> cell_locator_;
+  std::vector<vtkSmartPointer<vtkLine>> lines_;
+
+  // Geodesics between all point pairs. Assumes the number of points is very small
+  Eigen::MatrixXd geodesics_;
+
+  // cache which line a particle is on
+  mutable std::vector<int> particle_lines_;
+  // store some information about the last geodesic query. The next one will most likely reuse this
+  mutable int geo_lq_idx_ = -1;
+  mutable int geo_lq_line_ = -1;
+  mutable double geo_lq_dist_ = -1;
 };
 
-} // end namespace itk
+}  // end namespace itk
