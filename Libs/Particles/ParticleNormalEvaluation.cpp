042a11894e826bf54401e22227f285b2a236c2e4
Alan Morris <akenmorris@gmail.com>
2022-03-23 19:24:42 -0600

    Update signature.

--- Libs/Particles/ParticleNormalEvaluation.cpp
+++ Libs/Particles/ParticleNormalEvaluation.cpp
@@ -2,5 +2,11 @@
 
 namespace shapeworks {
 
+std::vector<bool> ParticleNormalEvaluation::evalaute_particle_normals(
+    ParticleSystem local_particles, std::vector<std::shared_ptr<VtkMeshWrapper>> meshes, double max_angle) {
+
+
 
 }
+
+}  // namespace shapeworks
