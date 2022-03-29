a38ce80d9649b80c5eda6710d22ebab4ae66f163
Cameron Christensen <cam@sci.utah.edu>
2022-03-29 17:13:40 -0600

    deploy docs to origin/gh-pages

--- Support/deploy_docs.sh
+++ Support/deploy_docs.sh
@@ -49,5 +49,5 @@ git pull --rebase
 git checkout master
 python Python/RunShapeWorksAutoDoc.py --md_filename docs/tools/ShapeWorksCommands.md
 doxybook2 -i ${INSTALL_DIR}/Documentation/Doxygen/xml -o docs/api -c docs/doxygen/doxybook2.config.json
-cd "${GITHUB_WORKSPACE}" && mike deploy --title "6.3.0 (dev)" 6.3 dev --branch origin/gh-pages
-mike set-default 6.2
+cd "${GITHUB_WORKSPACE}" && mike deploy --title "6.3.0 (dev)" 6.3 dev --branch origin/gh-pages --update-aliases
+mike set-default 6.3
