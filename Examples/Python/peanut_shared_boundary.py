1484886d3ba6e5018e42cef5132da46a5d6c361f
Krithika Iyer <iyerkrithika21@gmail.com>
2022-04-01 00:31:01 -0600

    refactor the use case to use modified shared boundary commands and project API

--- Examples/Python/peanut_shared_boundary.py
+++ Examples/Python/peanut_shared_boundary.py
@@ -9,179 +9,221 @@ shared boundary mesh is extracted, and the contour. Shape statistics for each of
 pieces are then jointly optimized
 """
 import os
-import shutil
-import OptimizeUtils 
-import AnalyzeUtils
 import shapeworks as sw
 import glob
+import numpy as np
 import subprocess
 
 def Run_Pipeline(args):
-    datasetName = "peanut-v0"
-    outputDirectory = "Output/peanut_shared_boundary/"
-    groomDir = outputDirectory + datasetName + "/groomed/"
-    if not os.path.exists(outputDirectory):
-        os.makedirs(outputDirectory)
-    if not os.path.exists(groomDir):
-        os.makedirs(groomDir)
-
-    sw.data.download_and_unzip_dataset(datasetName, outputDirectory)
-    meshFiles = sorted(glob.glob(outputDirectory + datasetName + "/meshes/*.stl"))
-
-    # 01. Remesh
-    # TODO This has to be done using mmgtools, but since this is not yet available
-    # it is already done using MeshLab and stored in the appropriate folder. This
-    # code is commented out because pymeshlab's Qt conflicts with ShapeWorks' Qt
-    remeshDir = groomDir + "01_remesh/"
-    if not os.path.exists(remeshDir):
-        os.makedirs(remeshDir)
     """
-    for fname in meshFiles:
-        ms = pymeshlab.MeshSet()
-        ms.load_new_mesh(fname)
-        targetlen = 0.4 # Data specific
-        ms.remeshing_isotropic_explicit_remeshing(targetlen=targetlen)
-        new_name = remeshDir + fname.split('/')[-1]
-        ms.save_current_mesh(new_name)
+    Download supershapes data. Refer to `generate_supershapes` in this file to see the generation
+    process
     """
+    print("\nStep 1. Extract Data\n")
+    dataset_name = "peanut-v0"
+    output_directory = "Output/peanut_shared_boundary/"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
 
-    # 02. Extract shared boundaries
-    # TODO Uses the shared_boundary_extractor script that has to be consolidated into shapework.Mesh
-    extractedDir = groomDir + "02_extracted/"
-    if not os.path.exists(extractedDir):
-        os.makedirs(extractedDir)
-    for i in range(15):
-        fname_l = f'{remeshDir}/{i:02d}_l.ply'
-        fname_r = f'{remeshDir}/{i:02d}_r.ply'
-        tol = '1e-3' # distance threshold to consider two surfaces as "shared"
+    # If running a tiny_test, then download subset of the data
+    if args.tiny_test:
+        args.use_single_scale = 1
+        # sw.data.download_subset(args.use_case, dataset_name, output_directory)
+        mesh_files = sorted(glob.glob(output_directory +
+                            dataset_name + "/meshes/*.stl"))[:2]
+    # Else download the entire dataset
+    else:
+        sw.data.download_and_unzip_dataset(dataset_name, output_directory)
+        mesh_files = sorted(glob.glob(output_directory +
+                                     dataset_name + "/meshes/*.stl"))
 
-        fname_out_l = f'{extractedDir}/{i:02d}_l.ply'
-        fname_out_r = f'{extractedDir}/{i:02d}_r.ply'
-        fname_out_m = f'{extractedDir}/{i:02d}_m.ply'
+        if args.use_subsample:
+            inputMeshes =[sw.Mesh(filename) for filename in mesh_files]
+            sample_idx = sw.data.sample_meshes(inputMeshes, int(args.num_subsample),domains_per_shape=2)
+            mesh_files = [mesh_files[i] for i in sample_idx]
 
-        sw.MeshUtils.sharedBoundaryExtractor(sw.Mesh(fname_l),sw.Mesh(fname_r),fname_out_l,fname_out_r,fname_out_m)
-        # cmd = [
-        #     'shared_boundary_extractor',
-        #     fname_l, fname_r,
-        #     fname_out_l, fname_out_r, fname_out_m,
-        #     tol
-        # ]
-        # print(f'Extraction Command: {" ".join(cmd)}')
-        # subprocess.check_call(cmd)
-
-    # 03. Smooth
-    # TODO Use shapeworks.Mesh() https://github.com/SCIInstitute/ShapeWorks/issues/1234
-    # For now, use meshes pre-smoothed using MeshLab. These were smoothed using "Laplacian smooth"
-    # with default parameters
-    smoothDir = groomDir + "03_smooth/"
-    if not os.path.exists(smoothDir):
-        os.makedirs(smoothDir)
+    print("\nStep 2. Groom - Data Pre-processing\n")
     """
-    for fname in glob.glob(f'{extractedDir}/*.ply')
-        ms = pymeshlab.MeshSet()
-        ms.load_new_mesh(fname)
-        ms.laplacian_smooth()
-        new_name = smoothDir + fname.split('/')[-1]
-        ms.save_current_mesh(new_name)
+    Step 2: GROOMING 
+    The required grooming steps are: 
+    1. Remesh 
+    2. Extract Shared Boundary Surface
+    3. Smooth? 
+    4. Extract Boundary Loop contours
     """
 
-    # 04. Extract boundary loop
-    # TODO This uses the boundary_loop_extractor script which has to eventually be added
-    # to shapeworks.Mesh. Contour I/O has to potentially be supported in shapeworks.Mesh
-    finalDir = groomDir + "04_final/"
-    if not os.path.exists(finalDir):
-        os.makedirs(finalDir)
-    for i in range(15):
-        fname_l = f'{smoothDir}/{i:02d}_l.ply'
-        fname_r = f'{smoothDir}/{i:02d}_r.ply'
-        fname_m = f'{smoothDir}/{i:02d}_m.ply'
+    # Create a directory for groomed output
+    groom_dir = output_directory + 'groomed/'
+    if not os.path.exists(groom_dir):
+        os.makedirs(groom_dir)
 
-        fname_out_l = f'{finalDir}/{i:02d}_l.ply'
-        fname_out_r = f'{finalDir}/{i:02d}_r.ply'
-        fname_out_m = f'{finalDir}/{i:02d}_m.ply'
-        fname_out_c = f'{finalDir}/{i:02d}_c.vtp'
-
-        # Copy L, R, M as is
-        shutil.copy(fname_l, fname_out_l)
-        shutil.copy(fname_r, fname_out_r)
-        shutil.copy(fname_m, fname_out_m)
-
-        # # Extract boundary loop from M
-        # cmd = [
-        #     'boundary_loop_extractor',
-        #     fname_m,
-        #     fname_out_c,
-        # ]
-        # print(f'Boundary Extraction Command: {" ".join(cmd)}')
-        # subprocess.check_call(cmd)
-        sw.MeshUtils.boundaryLoopExtractor(fname_out_c,sw.Mesh(fname_m))
-
-    # Copy everything to shape model directory
-    shapeModelDir = outputDirectory + "shape_model/"
-    if not os.path.exists(shapeModelDir):
-        os.makedirs(shapeModelDir)
-    for fname in glob.glob(f'{finalDir}/*'):
-        new_name = shapeModelDir + fname.split('/')[-1]
-        shutil.copy(fname, new_name)
-
-    # TODO Use OptimizeUtils.py once Multidomain PR is merged https://github.com/SCIInstitute/ShapeWorks/pull/1143
-    shutil.copy(f'{outputDirectory}/{datasetName}/shape_models/peanut_shared_boundary/shared_with_contours.xlsx',
-                f'{outputDirectory}/shape_model/')
-    optimizeCmd = 'shapeworks optimize --name Output/peanut_shared_boundary/shape_model/shared_with_contours.xlsx'.split()
-    subprocess.check_call(optimizeCmd)
-
-    # TODO Use AnalyzeUtils.py once Multidomain PR is merged https://github.com/SCIInstitute/ShapeWorks/pull/1143
-    analyzeCmd = 'ShapeWorksStudio Output/peanut_shared_boundary/shape_model/shared_with_contours.xlsx'.split()
-    subprocess.check_call(analyzeCmd)
-
-
-    # mesh_files = sorted(glob.glob(f'{finalDir}/*'))
-    # #Make directory to save optimization output
-    # point_dir = outputDirectory + 'shape_models_new/'
-    # if not os.path.exists(point_dir):
-    #     os.makedirs(point_dir)
-    # # Create a dictionary for all the parameters required by optimization
-    # parameter_dictionary = {
-    #     "number_of_particles" : [32,256,32,256],
-    #     "use_normals": [0,0,0,0],
-    #     "normal_weight": [10.0,10.0,10.0,10.0],
-    #     "checkpointing_interval" : 200,
-    #     "keep_checkpoints" : 0,
-    #     "iterations_per_split" : 1000,
-    #     "optimization_iterations" : 2500,
-    #     "starting_regularization" :100,
-    #     "ending_regularization" : 0.01,
-    #     "domains_per_shape" : 4,
-    #     "relative_weighting" : 12, #10, # 1 for segmentation images
-    #     "initial_relative_weighting" : 0.01,
-    #     "procrustes_interval" : 3,
-    #     "procrustes_scaling" : 0,
-    #     "save_init_splits" : 0,
-    #     "verbosity" : 3,
-    #     "multiscale_particles" : 8,
-    #     "domain_type": "contour",
-    #     "recompute_regularization_interval" : 2
-
-    #   }
+    """
+    First, we need to loop over the mesh files and load them
+    """
+    # list of shape segmentations
+    mesh_list = []
+    # list of shape names (shape files prefixes) to be used for saving outputs
+    mesh_names = []
+    domain_ids = []
+    for mesh_file in mesh_files:
+        print('Loading: ' + mesh_file)
+        # get current shape name
+        mesh_name = mesh_file.split('/')[-1].replace('.stl', '')
+        mesh_names.append(mesh_name)
+        # get domain identifiers
+        domain_ids.append(mesh_name.split(".")[0].split("_")[-1])
+        
+        # load mesh
+        mesh = sw.Mesh(mesh_file)
+        # do initial grooming steps
+        print("Grooming: " + mesh_name)
+        mesh.remeshPercent(percentage=60, adaptivity=1.0)
+        # append to the mesh list
+        mesh_list.append(mesh)
 
     
+    #domain identifiers for all shapes
+    domain_ids = np.array(domain_ids)
+    #shape index for all shapes in domain left 
+    domain1_indx = list(np.where(domain_ids == 'l')[0])
+    #shape index for all shapes in domain right
+    domain2_indx = list(np.where(domain_ids == 'r')[0])
 
-    # # Execute the optimization function
-    # [local_point_files, world_point_files] = OptimizeUtils.runShapeWorksOptimize(
-    #     point_dir, mesh_files, parameter_dictionary)
 
-    # if args.tiny_test:
-    #     print("Done with tiny test")
-    #     exit()
+    # 02. Extract shared boundaries and 03. Smooth the meshes
+    extracted_shared_meshes = []
+    extracted_meshes = []
+    # distance threshold to consider two surfaces as "shared"
+    tol = 1e-3
+    for i in range(len(domain1_indx)):
+        mesh_l = mesh_list[domain1_indx[i]]
+        mesh_r = mesh_list[domain2_indx[i]]
+        extracted_l,extracted_r,extracted_s = sw.MeshUtils.sharedBoundaryExtractor(mesh_l,mesh_r,tol)
+        #smooth the meshes 
+        extracted_l.smooth()
+        extracted_r.smooth()
+        extracted_s.smooth()
+        # append the extracted meshes to list
+        extracted_meshes.append(extracted_l)
+        extracted_meshes.append(extracted_r)
+        # append the extracted shared surface mesh to list
+        extracted_shared_meshes.append(extracted_s)
 
-    # print("\nStep 3. Analysis - Launch ShapeWorksStudio - sparse correspondence model.\n")
-    # """
-    # Step 3: ANALYZE - Shape Analysis and Visualization
 
-    # Now we launch studio to analyze the resulting shape model.
-    # For more information about the analysis step, see docs/workflow/analyze.md
+    # 04. Extract boundary loop
+    contours = []
+    for i in range(len(extracted_shared_meshes)):
+        output_contour = sw.MeshUtils.boundaryLoopExtractor(extracted_shared_meshes[i])
+        contours.append(output_contour)
+
+    # Save groomed meshes
+    groomed_mesh_files = sw.utils.save_meshes(groom_dir + 'meshes/', extracted_meshes, mesh_names, extension='vtk')
+    
+    extracted_shared_names = [mesh_names[i].replace("l","s") for i in domain1_indx]
+    groomed_shared_mesh_files = sw.utils.save_meshes(groom_dir + 'meshes/', extracted_shared_meshes, extracted_shared_names, extension = "vtk")
+    
+    contour_names = [mesh_names[i].replace("l","c") for i in domain1_indx]
+    contour_files = sw.utils.save_meshes(groom_dir + 'contours/', contours,contour_names, extension = "vtp")
+    
+    print("\nStep 3. Optimize - Particle Based Optimization\n")
+    """
+    Step 3: OPTIMIZE - Particle Based Optimization
+
+    Now we can run optimization directly on the meshes.
+    For more details on the plethora of parameters for shapeworks please refer 
+    to docs/workflow/optimze.md
+    http://sciinstitute.github.io/ShapeWorks/workflow/optimize.html
+    """
+
+    # Create project spreadsheet
+    project_location = output_directory + "shape_models/"
+    if not os.path.exists(project_location):
+        os.makedirs(project_location)
+    domains_per_shape = 4
+    # Set subjects
+    subjects = []
+    num_init_domains = 2 # left and right
+    for i in range(len(domain1_indx)):
+        subject = sw.Subject()
+        subject.set_number_of_domains(domains_per_shape)
+        rel_mesh_files = []
+        rel_groom_files = []
+        transform = []
+        # Adding left and right spheres
+        for d in range(num_init_domains):
+            rel_mesh_files += sw.utils.get_relative_paths([os.getcwd() + '/' + mesh_files[i*num_init_domains+d]], project_location)
+            rel_groom_files += sw.utils.get_relative_paths([os.getcwd() + '/' + groomed_mesh_files[i*num_init_domains+d]], project_location)
+            transform.append(np.eye(4).flatten())
+        # Adding extracted shared surface
+        rel_mesh_files += sw.utils.get_relative_paths([os.getcwd() + '/' + groomed_shared_mesh_files[i]], project_location)
+        rel_groom_files += sw.utils.get_relative_paths([os.getcwd() + '/' + groomed_shared_mesh_files[i]], project_location)
+        transform.append(np.eye(4).flatten())
+        # Addng extracted contour
+        rel_mesh_files += sw.utils.get_relative_paths([os.getcwd() + '/' + contour_files[i]], project_location)
+        rel_groom_files += sw.utils.get_relative_paths([os.getcwd() + '/' + contour_files[i]], project_location)
+        transform.append(np.eye(4).flatten())
+
+        subject.set_groomed_transforms(transform)
+        subject.set_groomed_filenames(rel_groom_files)
+        subject.set_original_filenames(rel_mesh_files)
+        subjects.append(subject)
+    # Set project
+    project = sw.Project()
+    project.set_subjects(subjects)
+    project.set_original_domain_types([sw.DomainType.ContourDomain])
+    project.set_groomed_domain_types([sw.DomainType.ContourDomain])
+    parameters = sw.Parameters()
+
+    # Create a dictionary for all the parameters required by optimization
+    num_particles = [256,256,32,32]
+
+    parameter_dictionary = {
+        "checkpointing_interval" : 200,
+        "keep_checkpoints" : 0,
+        "iterations_per_split" : 1000,
+        "optimization_iterations" : 2500,
+        "starting_regularization" :100,
+        "ending_regularization" : 0.01,
+        "domains_per_shape" : 4,
+        "relative_weighting" : 12, 
+        "initial_relative_weighting" : 0.01,
+        "procrustes_interval" : 3,
+        "procrustes_scaling" : 0,
+        "save_init_splits" : 0,
+        "verbosity" : 3,
+        "multiscale_particles" : 8,
+        "recompute_regularization_interval" : 2
+
+      }
+
+    # If running a tiny test, reduce some parameters
+    if args.tiny_test:
+        num_particles = [32,32,8,8]
+        parameter_dictionary["optimization_iterations"] = 30
+        parameter_dictionary["iterations_per_split"] = 30
+
+
+    # Add param dictionary to spreadsheet
+    for key in parameter_dictionary:
+        parameters.set(key, sw.Variant([parameter_dictionary[key]]))
+    parameters.set("number_of_particles" ,sw.Variant(num_particles))
+    project.set_parameters("optimize", parameters)
+    
+    spreadsheet_file = output_directory + "shape_models/peanut_shared_boundary_" + args.option_set + ".xlsx"
+    project.save(spreadsheet_file)
+
+    # Run optimization
+    optimize_cmd = ('shapeworks optimize --name ' + spreadsheet_file).split()
+    subprocess.check_call(optimize_cmd)
+
+    # # If tiny test or verify, check results and exit
+    # sw.utils.check_results(args, spreadsheet_file)
+
+    print("\nStep 4. Analysis - Launch ShapeWorksStudio")
+    """
+    Step 4: ANALYZE - open in studio
+    For more information about the analysis step, see:
     # http://sciinstitute.github.io/ShapeWorks/workflow/analyze.html
-    # """
-    # domains_per_shape = 4
-    # AnalyzeUtils.launchShapeWorksStudio(
-    #     point_dir, mesh_files, local_point_files, world_point_files,domains_per_shape)
+    """
+    analyze_cmd = ('ShapeWorksStudio ' + spreadsheet_file).split()
+    subprocess.check_call(analyze_cmd)
