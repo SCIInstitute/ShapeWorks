0c51252f1e2d2660003cdfbd4dc4bf6bbe2e713d
Krithika Iyer <iyerkrithika21@gmail.com>
2022-04-01 13:23:50 -0600

    add python tests for shared boundary commands

---
+++ Testing/PythonTests/shared_boundary.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from shapeworks import *
+
+success = True
+
+def sharedBoundaryExtractor():
+	left = Mesh(os.environ["DATA"] + "/shared_boundary/00_l.vtk")
+	right = Mesh(os.environ["DATA"] + "/shared_boundary/00_r.vtk")
+	tol = 1e-3
+	out_left, out_right, out_shared = MeshUtils.sharedBoundaryExtractor(left,right,tol)
+
+	ground_truth_left = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_l.vtk")
+	ground_truth_right = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_r.vtk")
+	ground_truth_shared = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_s.vtk")
+
+	return (ground_truth_left == out_left) and (ground_truth_right == out_right) and (ground_truth_shared == out_shared)
+
+
+success &= utils.test(sharedBoundaryExtractor)
+
+def boundaryLoopExtractor():
+
+	mesh = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_s.vtk")
+	loop = MeshUtils.boundaryLoopExtractor(mesh);
+
+	ground_truth = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_c.vtp")
+
+	return ground_truth == loop
+
+success &= utils.test(sharedBoundaryExtractor)
