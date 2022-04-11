fa49d4350a8068c0f1e726476f4f55a990f5d81d
Alan Morris <akenmorris@gmail.com>
2022-04-11 11:52:09 -0600

    Reformat

--- Studio/src/Interface/StatusBarWidget.cpp
+++ Studio/src/Interface/StatusBarWidget.cpp
@@ -1,6 +1,7 @@
 #include "StatusBarWidget.h"
-#include <QStatusBar>
+
 #include <QResizeEvent>
+#include <QStatusBar>
 
 // ui
 #include <ui_StatusBarWidget.h>
@@ -8,17 +9,13 @@
 namespace shapeworks {
 
 //---------------------------------------------------------------------------
-StatusBarWidget::StatusBarWidget(QMainWindow* parent) : QWidget(parent)
-{
+StatusBarWidget::StatusBarWidget(QMainWindow* parent) : QWidget(parent) {
   this->ui_ = new Ui_StatusBarWidget;
   this->ui_->setupUi(this);
 
-  this->normal_message_icon_.addFile(
-    QString::fromUtf8(":/Studio/Images/log_normal.png"), QSize(), QIcon::Normal);
-  this->error_message_icon_.addFile(
-    QString::fromUtf8(":/Studio/Images/log_error.png"), QSize(), QIcon::Normal);
-  this->warning_message_icon_.addFile(
-    QString::fromUtf8(":/Studio/Images/log_warning.png"), QSize(), QIcon::Normal);
+  this->normal_message_icon_.addFile(QString::fromUtf8(":/Studio/Images/log_normal.png"), QSize(), QIcon::Normal);
+  this->error_message_icon_.addFile(QString::fromUtf8(":/Studio/Images/log_error.png"), QSize(), QIcon::Normal);
+  this->warning_message_icon_.addFile(QString::fromUtf8(":/Studio/Images/log_warning.png"), QSize(), QIcon::Normal);
 
   this->q_status_bar_ = parent->statusBar();
   this->q_status_bar_->setContentsMargins(0, 0, 0, 0);
@@ -31,16 +28,13 @@ StatusBarWidget::StatusBarWidget(QMainWindow* parent) : QWidget(parent)
 }
 
 //---------------------------------------------------------------------------
-void StatusBarWidget::set_message(MessageType message_type, QString message)
-{
+void StatusBarWidget::set_message(MessageType message_type, QString message) {
   message.replace('\n', ' ');
   if (message_type == MessageType::normal) {
     this->ui_->log_button->setIcon(this->normal_message_icon_);
-  }
-  else if (message_type == MessageType::warning) {
+  } else if (message_type == MessageType::warning) {
     this->ui_->log_button->setIcon(this->warning_message_icon_);
-  }
-  else {
+  } else {
     this->ui_->log_button->setIcon(this->error_message_icon_);
     message = "<font color=#CC0000>" + message + "</font>";
   }
@@ -50,8 +44,7 @@ void StatusBarWidget::set_message(MessageType message_type, QString message)
 }
 
 //---------------------------------------------------------------------------
-void StatusBarWidget::set_progress(int value)
-{
+void StatusBarWidget::set_progress(int value) {
   if (value < 0) {
     this->ui_->progress_bar->setVisible(true);
     this->ui_->progress_bar->setMinimum(0);
@@ -64,8 +57,7 @@ void StatusBarWidget::set_progress(int value)
   if (value < 100) {
     this->ui_->progress_bar->setVisible(true);
     this->ui_->progress_bar->setValue(value);
-  }
-  else {
+  } else {
     this->ui_->progress_bar->setValue(100);
     this->ui_->progress_bar->setVisible(false);
   }
@@ -74,22 +66,19 @@ void StatusBarWidget::set_progress(int value)
 }
 
 //---------------------------------------------------------------------------
-void StatusBarWidget::resizeEvent(QResizeEvent* event)
-{
+void StatusBarWidget::resizeEvent(QResizeEvent* event) {
   QWidget::resizeEvent(event);
   this->setMaximumSize(event->size());
   this->update_layout();
 }
 
 //---------------------------------------------------------------------------
-void StatusBarWidget::update_layout()
-{
+void StatusBarWidget::update_layout() {
   if (this->ui_->progress_bar->isVisible()) {
     this->ui_->message->setMaximumWidth(this->width() * 0.66);
-  }
-  else {
+  } else {
     this->ui_->message->setMaximumWidth(this->width() * 1.0);
   }
 }
 
-}
+}  // namespace shapeworks
