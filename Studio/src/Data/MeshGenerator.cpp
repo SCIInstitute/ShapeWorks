a2db696f35abea9657b1d6976333a69394cf66b6
Alan Morris <akenmorris@gmail.com>
2022-04-11 19:52:32 -0600

    Reformat

--- Studio/src/Data/MeshGenerator.cpp
+++ Studio/src/Data/MeshGenerator.cpp
@@ -1,25 +1,19 @@
-#include <limits>
-
-#include <QFileInfo>
-
-#include <itkPoint.h>
-
-#include <vtkPolyDataNormals.h>
-#include <vtkMarchingCubes.h>
-
-#include <Data/MeshGenerator.h>
 #include <Data/ItkToVtk.h>
+#include <Data/LegacyMeshGenerator.h>
+#include <Data/MeshGenerator.h>
+#include <Data/QMeshWarper.h>
 #include <Data/StudioLog.h>
-
-#include <itkVTKImageExport.h>
-#include <itkOrientImageFilter.h>
-
 #include <Libs/Mesh/Mesh.h>
 #include <Libs/Mesh/MeshUtils.h>
-
 #include <Libs/Utils/StringUtils.h>
-#include <Data/QMeshWarper.h>
-#include <Data/LegacyMeshGenerator.h>
+#include <itkOrientImageFilter.h>
+#include <itkPoint.h>
+#include <itkVTKImageExport.h>
+#include <vtkMarchingCubes.h>
+#include <vtkPolyDataNormals.h>
+
+#include <QFileInfo>
+#include <limits>
 
 namespace shapeworks {
 
@@ -28,33 +22,27 @@ const std::string MeshGenerator::RECONSTRUCTION_DISTANCE_TRANSFORM_C("distance_t
 const std::string MeshGenerator::RECONSTRUCTION_MESH_WARPER_C("mesh_warper");
 
 //---------------------------------------------------------------------------
-MeshGenerator::MeshGenerator()
-{}
+MeshGenerator::MeshGenerator() {}
 
 //---------------------------------------------------------------------------
-MeshGenerator::~MeshGenerator()
-{}
+MeshGenerator::~MeshGenerator() {}
 
 //---------------------------------------------------------------------------
-MeshHandle MeshGenerator::build_mesh(const MeshWorkItem& item)
-{
+MeshHandle MeshGenerator::build_mesh(const MeshWorkItem& item) {
   if (item.filename != "") {
     return this->build_mesh_from_file(item.filename);
-  }
-  else {
+  } else {
     return this->build_mesh_from_points(item.points, item.domain);
   }
 }
 
 //---------------------------------------------------------------------------
-MeshHandle MeshGenerator::build_mesh_from_points(const Eigen::VectorXd& shape,
-                                                 int domain)
-{
+MeshHandle MeshGenerator::build_mesh_from_points(const Eigen::VectorXd& shape, int domain) {
   MeshHandle mesh(new StudioMesh);
 
   auto& surface_reconstructors = this->reconstructors_->surface_reconstructors_;
   auto& mesh_warpers = this->reconstructors_->mesh_warpers_;
-  if(mesh_warpers.size() > domain && mesh_warpers[domain]->is_contour()) {
+  if (mesh_warpers.size() > domain && mesh_warpers[domain]->is_contour()) {
     auto poly_data = vtkSmartPointer<vtkPolyData>::New();
     mesh->set_poly_data(poly_data);
     return mesh;
@@ -67,30 +55,21 @@ MeshHandle MeshGenerator::build_mesh_from_points(const Eigen::VectorXd& shape,
     return mesh;
   }
 
-  bool distance_transform_available =
-    surface_reconstructors.size() > domain &&
-    surface_reconstructors[domain] &&
-    surface_reconstructors[domain]->get_surface_reconstruction_available();
+  bool distance_transform_available = surface_reconstructors.size() > domain && surface_reconstructors[domain] &&
+                                      surface_reconstructors[domain]->get_surface_reconstruction_available();
 
-  if (this->reconstruction_method_ == RECONSTRUCTION_DISTANCE_TRANSFORM_C &&
-      distance_transform_available) {
-    vtkSmartPointer<vtkPolyData> poly_data = surface_reconstructors[domain]->build_mesh(
-      shape);
+  if (this->reconstruction_method_ == RECONSTRUCTION_DISTANCE_TRANSFORM_C && distance_transform_available) {
+    vtkSmartPointer<vtkPolyData> poly_data = surface_reconstructors[domain]->build_mesh(shape);
 
-    vtkSmartPointer<vtkPolyDataNormals> polydata_normals =
-      vtkSmartPointer<vtkPolyDataNormals>::New();
+    vtkSmartPointer<vtkPolyDataNormals> polydata_normals = vtkSmartPointer<vtkPolyDataNormals>::New();
     polydata_normals->SetInputData(poly_data);
     polydata_normals->Update();
     poly_data = polydata_normals->GetOutput();
 
     mesh->set_poly_data(poly_data);
-  }
-  else if (this->reconstruction_method_ == RECONSTRUCTION_MESH_WARPER_C &&
-           mesh_warpers.size() > domain && mesh_warpers[domain] &&
-           mesh_warpers[domain]->get_warp_available()) {
-
-    Eigen::MatrixXd points = Eigen::Map<const Eigen::VectorXd>((double*) shape.data(),
-                                                               shape.size());
+  } else if (this->reconstruction_method_ == RECONSTRUCTION_MESH_WARPER_C && mesh_warpers.size() > domain &&
+             mesh_warpers[domain] && mesh_warpers[domain]->get_warp_available()) {
+    Eigen::MatrixXd points = Eigen::Map<const Eigen::VectorXd>((double*)shape.data(), shape.size());
     points.resize(3, shape.size() / 3);
     points.transposeInPlace();
 
@@ -102,15 +81,13 @@ MeshHandle MeshGenerator::build_mesh_from_points(const Eigen::VectorXd& shape,
       mesh->set_error_message(message);
       return mesh;
     }
-    vtkSmartPointer<vtkPolyDataNormals> polydata_normals =
-      vtkSmartPointer<vtkPolyDataNormals>::New();
+    vtkSmartPointer<vtkPolyDataNormals> polydata_normals = vtkSmartPointer<vtkPolyDataNormals>::New();
     polydata_normals->SetInputData(poly_data);
     polydata_normals->Update();
     poly_data = polydata_normals->GetOutput();
 
     mesh->set_poly_data(poly_data);
-  }
-  else {
+  } else {
     LegacyMeshGenerator legacy;
     mesh->set_poly_data(legacy.buildMesh(shape));
   }
@@ -118,8 +95,7 @@ MeshHandle MeshGenerator::build_mesh_from_points(const Eigen::VectorXd& shape,
 }
 
 //---------------------------------------------------------------------------
-MeshHandle MeshGenerator::build_mesh_from_image(ImageType::Pointer image, float iso_value)
-{
+MeshHandle MeshGenerator::build_mesh_from_image(ImageType::Pointer image, float iso_value) {
   MeshHandle mesh(new StudioMesh);
 
   try {
@@ -151,9 +127,8 @@ MeshHandle MeshGenerator::build_mesh_from_image(ImageType::Pointer image, float 
 }
 
 //---------------------------------------------------------------------------
-MeshHandle MeshGenerator::build_mesh_from_file(std::string filename, float iso_value)
-{
-  //std::cerr << "build_mesh from " << filename << "\n";
+MeshHandle MeshGenerator::build_mesh_from_file(std::string filename, float iso_value) {
+  // std::cerr << "build_mesh from " << filename << "\n";
   MeshHandle mesh(new StudioMesh);
 
   if (!QFileInfo(QString::fromStdString(filename)).exists()) {
@@ -176,8 +151,7 @@ MeshHandle MeshGenerator::build_mesh_from_file(std::string filename, float iso_v
       STUDIO_LOG_ERROR(QString::fromStdString(message));
       mesh->set_error_message(message);
     }
-  }
-  else {
+  } else {
     try {
       // read file using ITK
       using ReaderType = itk::ImageFileReader<ImageType>;
@@ -190,8 +164,7 @@ MeshHandle MeshGenerator::build_mesh_from_file(std::string filename, float iso_v
       using Orienter = itk::OrientImageFilter<ImageType, ImageType>;
       Orienter::Pointer orienter = Orienter::New();
       orienter->UseImageDirectionOn();
-      orienter->SetDesiredCoordinateOrientation(
-        itk::SpatialOrientation::ITK_COORDINATE_ORIENTATION_RAI);
+      orienter->SetDesiredCoordinateOrientation(itk::SpatialOrientation::ITK_COORDINATE_ORIENTATION_RAI);
       orienter->SetInput(image);
       orienter->Update();
       image = orienter->GetOutput();
@@ -204,25 +177,17 @@ MeshHandle MeshGenerator::build_mesh_from_file(std::string filename, float iso_v
     }
   }
   return mesh;
-
 }
 
 //---------------------------------------------------------------------------
-void MeshGenerator::set_reconstruction_method(std::string method)
-{
-  this->reconstruction_method_ = method;
-}
+void MeshGenerator::set_reconstruction_method(std::string method) { this->reconstruction_method_ = method; }
 
 //---------------------------------------------------------------------------
-std::string MeshGenerator::get_reconstruction_method()
-{
-  return this->reconstruction_method_;
-}
+std::string MeshGenerator::get_reconstruction_method() { return this->reconstruction_method_; }
 
 //---------------------------------------------------------------------------
-void MeshGenerator::set_mesh_reconstructors(std::shared_ptr<MeshReconstructors> reconstructors)
-{
+void MeshGenerator::set_mesh_reconstructors(std::shared_ptr<MeshReconstructors> reconstructors) {
   this->reconstructors_ = reconstructors;
 }
 
-}
+}  // namespace shapeworks
