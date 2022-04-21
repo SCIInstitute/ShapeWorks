831d91e0b1982e9ffe44f55a5f03dfe6c5de7c31
Alan Morris <akenmorris@gmail.com>
2022-04-21 10:27:47 -0600

    Update OptimizeTool style

--- Studio/src/Optimize/OptimizeTool.cpp
+++ Studio/src/Optimize/OptimizeTool.cpp
@@ -90,7 +90,7 @@ OptimizeTool::~OptimizeTool() {}
 //---------------------------------------------------------------------------
 void OptimizeTool::handle_error(QString msg) {
   emit error_message(msg);
-  this->update_run_button();
+  update_run_button();
 }
 
 //---------------------------------------------------------------------------
@@ -98,54 +98,54 @@ void OptimizeTool::handle_warning(QString msg) { emit warning_message(msg); }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::handle_progress(int val, QString progress_message) {
-  if (!this->optimization_is_running_) {
+  if (!optimization_is_running_) {
     return;
   }
 
   emit progress(val);
   emit status(progress_message);
 
-  auto particles = this->optimize_->GetParticles();
-  this->session_->update_particles(particles);
+  auto particles = optimize_->GetParticles();
+  session_->update_particles(particles);
 }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::handle_optimize_complete() {
-  this->optimization_is_running_ = false;
+  optimization_is_running_ = false;
 
-  auto particles = this->optimize_->GetParticles();
-  this->session_->update_particles(particles);
+  auto particles = optimize_->GetParticles();
+  session_->update_particles(particles);
 
-  auto procrustes_transforms = this->optimize_->GetProcrustesTransforms();
-  this->session_->update_procrustes_transforms(procrustes_transforms);
+  auto procrustes_transforms = optimize_->GetProcrustesTransforms();
+  session_->update_procrustes_transforms(procrustes_transforms);
 
-  this->session_->calculate_reconstructed_samples();
-  this->session_->get_project()->store_subjects();
+  session_->calculate_reconstructed_samples();
+  session_->get_project()->store_subjects();
   emit progress(100);
 
-  QString duration = QString::number(this->elapsed_timer_.elapsed() / 1000.0, 'f', 1);
+  QString duration = QString::number(elapsed_timer_.elapsed() / 1000.0, 'f', 1);
   emit message("Optimize Complete.  Duration: " + duration + " seconds");
   emit optimize_complete();
-  this->update_run_button();
+  update_run_button();
 }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::handle_optimize_failed() {
-  this->optimization_is_running_ = false;
+  optimization_is_running_ = false;
   emit progress(100);
 
-  QString duration = QString::number(this->elapsed_timer_.elapsed() / 1000.0, 'f', 1);
+  QString duration = QString::number(elapsed_timer_.elapsed() / 1000.0, 'f', 1);
   emit message("Optimize Failed.  Duration: " + duration + " seconds");
   emit optimize_complete();
-  this->update_run_button();
+  update_run_button();
 }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::on_run_optimize_button_clicked() {
-  if (this->optimization_is_running_) {
-    this->optimization_is_running_ = false;
-    this->shutdown_threads();
-    this->enable_actions();
+  if (optimization_is_running_) {
+    optimization_is_running_ = false;
+    shutdown_threads();
+    enable_actions();
     emit progress(100);
     emit message("Optimize Aborted");
     emit optimize_complete();
@@ -159,29 +159,28 @@ void OptimizeTool::on_run_optimize_button_clicked() {
     session_->save_project(session_->get_filename());
   }
 
-  this->optimization_is_running_ = true;
+  optimization_is_running_ = true;
   emit optimize_start();
 
-  this->enable_actions();
+  enable_actions();
 
-  this->store_params();
+  store_params();
   emit message("Please wait: running optimize step...");
 
-  this->elapsed_timer_.start();
-  this->optimize_ = QSharedPointer<QOptimize>::create();
+  elapsed_timer_.start();
+  optimize_ = QSharedPointer<QOptimize>::create();
 
-  this->session_->clear_particles();
+  session_->clear_particles();
 
-  this->handle_progress(1, "");
-  this->optimize_parameters_ = QSharedPointer<OptimizeParameters>::create(this->session_->get_project());
+  handle_progress(1, "");
+  optimize_parameters_ = QSharedPointer<OptimizeParameters>::create(session_->get_project());
 
-  this->optimize_parameters_->set_load_callback(
-      std::bind(&OptimizeTool::handle_load_progress, this, std::placeholders::_1));
-  this->optimize_->SetFileOutputEnabled(false);
+  optimize_parameters_->set_load_callback(std::bind(&OptimizeTool::handle_load_progress, this, std::placeholders::_1));
+  optimize_->SetFileOutputEnabled(false);
 
   ShapeworksWorker* worker =
-      new ShapeworksWorker(ShapeworksWorker::OptimizeType, NULL, this->optimize_, this->optimize_parameters_,
-                           this->session_, std::vector<std::vector<itk::Point<double>>>(),
+      new ShapeworksWorker(ShapeworksWorker::OptimizeType, NULL, optimize_, optimize_parameters_, session_,
+                           std::vector<std::vector<itk::Point<double>>>(),
                            std::vector<std::vector<itk::Point<double>>>(), std::vector<std::string>());
 
   QThread* thread = new QThread;
@@ -189,7 +188,7 @@ void OptimizeTool::on_run_optimize_button_clicked() {
   connect(thread, SIGNAL(started()), worker, SLOT(process()));
   connect(worker, SIGNAL(result_ready()), this, SLOT(handle_optimize_complete()));
   connect(worker, &ShapeworksWorker::failure, this, &OptimizeTool::handle_optimize_failed);
-  connect(this->optimize_.data(), &QOptimize::progress, this, &OptimizeTool::handle_progress);
+  connect(optimize_.data(), &QOptimize::progress, this, &OptimizeTool::handle_progress);
   connect(worker, &ShapeworksWorker::error_message, this, &OptimizeTool::handle_error);
   connect(worker, &ShapeworksWorker::message, this, &OptimizeTool::handle_message);
   connect(worker, SIGNAL(finished()), worker, SLOT(deleteLater()));
@@ -197,7 +196,7 @@ void OptimizeTool::on_run_optimize_button_clicked() {
   connect(worker, SIGNAL(finished()), thread, SLOT(quit()));
   thread->start();
 
-  this->threads_ << thread;
+  threads_ << thread;
 }
 
 //---------------------------------------------------------------------------
@@ -205,29 +204,29 @@ void OptimizeTool::handle_message(QString s) { emit message(s); }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::on_restoreDefaults_clicked() {
-  this->session_->get_project()->clear_parameters(Parameters::OPTIMIZE_PARAMS);
-  this->load_params();
+  session_->get_project()->clear_parameters(Parameters::OPTIMIZE_PARAMS);
+  load_params();
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::set_session(QSharedPointer<Session> session) { this->session_ = session; }
+void OptimizeTool::set_session(QSharedPointer<Session> session) { session_ = session; }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::load_params() {
-  this->setup_domain_boxes();
-  auto params = OptimizeParameters(this->session_->get_project());
+  setup_domain_boxes();
+  auto params = OptimizeParameters(session_->get_project());
 
   ui_->number_of_particles->setText(QString::number(params.get_number_of_particles()[0]));
 
-  auto domain_names = this->session_->get_project()->get_domain_names();
+  auto domain_names = session_->get_project()->get_domain_names();
   for (int i = 0; i < domain_names.size(); i++) {
-    if (i < this->particle_boxes_.size()) {
+    if (i < particle_boxes_.size()) {
       int particles = 128;
       if (i < params.get_number_of_particles().size()) {
         particles = params.get_number_of_particles()[i];
       }
 
-      this->particle_boxes_[i]->setText(QString::number(particles));
+      particle_boxes_[i]->setText(QString::number(particles));
     }
   }
 
@@ -252,22 +251,22 @@ void OptimizeTool::load_params() {
   ui_->use_landmarks->setChecked(params.get_use_landmarks());
   ui_->narrow_band->setText(QString::number(params.get_narrow_band()));
 
-  this->update_ui_elements();
+  update_ui_elements();
 }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::store_params() {
-  auto params = OptimizeParameters(this->session_->get_project());
+  auto params = OptimizeParameters(session_->get_project());
 
   std::vector<int> num_particles;
   num_particles.push_back(ui_->number_of_particles->text().toInt());
 
-  auto domain_names = this->session_->get_project()->get_domain_names();
+  auto domain_names = session_->get_project()->get_domain_names();
   if (domain_names.size() > 1) {
     num_particles.clear();
     for (int i = 0; i < domain_names.size(); i++) {
-      if (i < this->particle_boxes_.size()) {
-        num_particles.push_back(this->particle_boxes_[i]->text().toInt());
+      if (i < particle_boxes_.size()) {
+        num_particles.push_back(particle_boxes_[i]->text().toInt());
       }
     }
   }
@@ -294,8 +293,8 @@ void OptimizeTool::store_params() {
   params.set_use_landmarks(ui_->use_landmarks->isChecked());
 
   // always use preference value
-  params.set_geodesic_cache_multiplier(this->preferences_.get_geodesic_cache_multiplier());
-  params.set_optimize_output_prefix(this->preferences_.get_optimize_file_template().toStdString());
+  params.set_geodesic_cache_multiplier(preferences_.get_geodesic_cache_multiplier());
+  params.set_optimize_output_prefix(preferences_.get_optimize_file_template().toStdString());
 
   params.set_narrow_band(ui_->narrow_band->text().toDouble());
 
@@ -303,7 +302,7 @@ void OptimizeTool::store_params() {
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::enable_actions() { this->update_run_button(); }
+void OptimizeTool::enable_actions() { update_run_button(); }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::disable_actions() {
@@ -313,13 +312,13 @@ void OptimizeTool::disable_actions() {
 //---------------------------------------------------------------------------
 void OptimizeTool::shutdown_threads() {
   std::cerr << "Shut Down Optimization Threads\n";
-  if (!this->optimize_) {
+  if (!optimize_) {
     return;
   }
-  if (this->optimize_parameters_) {
-    this->optimize_parameters_->set_abort_load(true);
+  if (optimize_parameters_) {
+    optimize_parameters_->set_abort_load(true);
   }
-  this->optimize_->AbortOptimization();
+  optimize_->AbortOptimization();
 }
 
 //---------------------------------------------------------------------------
@@ -332,15 +331,15 @@ void OptimizeTool::update_ui_elements() {
 }
 
 //---------------------------------------------------------------------------
-void OptimizeTool::activate() { this->enable_actions(); }
+void OptimizeTool::activate() { enable_actions(); }
 
 //---------------------------------------------------------------------------
 void OptimizeTool::handle_load_progress(int count) {
-  if (!this->optimization_is_running_) {
+  if (!optimization_is_running_) {
     return;
   }
 
-  double value = static_cast<double>(count) / this->session_->get_shapes().size() * 100.0f;
+  double value = static_cast<double>(count) / session_->get_shapes().size() * 100.0f;
   if (value < 100) {  // cannot emit 100 or the main interface will think the job is done
     emit progress(static_cast<int>(value));
   }
@@ -350,9 +349,9 @@ void OptimizeTool::handle_load_progress(int count) {
 bool OptimizeTool::validate_inputs() {
   bool all_valid = true;
 
-  std::vector<QLineEdit*> combined = this->line_edits_;
+  std::vector<QLineEdit*> combined = line_edits_;
 
-  combined.insert(combined.end(), this->particle_boxes_.begin(), this->particle_boxes_.end());
+  combined.insert(combined.end(), particle_boxes_.begin(), particle_boxes_.end());
 
   for (QLineEdit* line_edit : combined) {
     QString text = line_edit->text();
@@ -369,11 +368,11 @@ bool OptimizeTool::validate_inputs() {
 
 //---------------------------------------------------------------------------
 void OptimizeTool::update_run_button() {
-  bool inputs_valid = this->validate_inputs();
+  bool inputs_valid = validate_inputs();
 
-  ui_->run_optimize_button->setEnabled(inputs_valid && this->session_->get_groomed_present());
+  ui_->run_optimize_button->setEnabled(inputs_valid && session_->get_groomed_present());
 
-  if (this->optimization_is_running_) {
+  if (optimization_is_running_) {
     ui_->run_optimize_button->setText("Abort Optimize");
   } else {
     ui_->run_optimize_button->setText("Run Optimize");
@@ -383,16 +382,16 @@ void OptimizeTool::update_run_button() {
 //---------------------------------------------------------------------------
 void OptimizeTool::setup_domain_boxes() {
   qDeleteAll(ui_->domain_widget->findChildren<QWidget*>(QString(), Qt::FindDirectChildrenOnly));
-  this->particle_boxes_.clear();
+  particle_boxes_.clear();
 
   QLineEdit* last_box = ui_->number_of_particles;
 
-  if (this->session_->get_project()->get_number_of_domains_per_subject() < 2) {
+  if (session_->get_project()->get_number_of_domains_per_subject() < 2) {
     ui_->particle_stack->setCurrentIndex(0);
     ui_->domain_widget->setMaximumSize(1, 1);
   } else {
     ui_->domain_widget->setMaximumSize(9999, 9999);
-    auto domain_names = this->session_->get_project()->get_domain_names();
+    auto domain_names = session_->get_project()->get_domain_names();
     QGridLayout* layout = new QGridLayout;
     QIntValidator* above_zero = new QIntValidator(1, std::numeric_limits<int>::max(), this);
     for (int i = 0; i < domain_names.size(); i++) {
@@ -404,7 +403,7 @@ void OptimizeTool::setup_domain_boxes() {
       box->setValidator(above_zero);
       connect(box, &QLineEdit::textChanged, this, &OptimizeTool::update_run_button);
 
-      this->particle_boxes_.push_back(box);
+      particle_boxes_.push_back(box);
       layout->addWidget(box, i, 1);
     }
 
