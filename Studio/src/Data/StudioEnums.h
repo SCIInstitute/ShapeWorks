495edb7aaeec82a1b507cdbd047b0103cb241ab1
Alan Morris <akenmorris@gmail.com>
2022-05-18 18:48:36 -0600

    Adding StudioEnums to encapsulate enums

---
+++ Studio/src/Data/StudioEnums.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+
+namespace shapeworks {
+
+//-----------------------------------------------------------------------------
+enum DisplayMode { Original = 0, Groomed = 1, Reconstructed = 2 };
+
+//-----------------------------------------------------------------------------
+std::string display_mode_to_string(DisplayMode display_mode);
+
+//-----------------------------------------------------------------------------
+DisplayMode string_to_display_mode(std::string str);
+
+}  // namespace shapeworks
