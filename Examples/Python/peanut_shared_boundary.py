d1df755ec87b89b3f9a734e5407afa6c21cdb989
Alan Morris <akenmorris@gmail.com>
2022-05-26 02:33:03 -0600

    Add method of checking results only for a matching pattern.

--- Examples/Python/peanut_shared_boundary.py
+++ Examples/Python/peanut_shared_boundary.py
@@ -223,7 +223,7 @@ def Run_Pipeline(args):
     subprocess.check_call(optimize_cmd)
 
     # If tiny test or verify, check results and exit
-    sw.utils.check_results(args, spreadsheet_file)
+    sw.utils.check_results_pattern(args, spreadsheet_file, "c_world")
 
     print("\nStep 4. Analysis - Launch ShapeWorksStudio")
     """
