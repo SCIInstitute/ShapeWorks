af64ceb7e4d837939206b73cdc16189765060959
Alan Morris <akenmorris@gmail.com>
2022-07-02 09:26:36 -0600

    Fix #1815 crash when project has blank columns
    Unfortunately, blank columns match the default "fixed columns" of ""
    and then a blank value matches and the subjects all get added as fixed
    domains.

--- Libs/Optimize/OptimizeParameters.cpp
+++ Libs/Optimize/OptimizeParameters.cpp
@@ -502,7 +502,8 @@ bool OptimizeParameters::set_up_optimize(Optimize* optimize) {
 //---------------------------------------------------------------------------
 bool OptimizeParameters::is_subject_fixed(std::shared_ptr<Subject> subject) {
   auto table = subject->get_table_values();
-  if (table.find(get_fixed_subjects_column()) != table.end()) {
+  auto fixed_subjects_column = get_fixed_subjects_column();
+  if (fixed_subjects_column != "" && table.find(fixed_subjects_column) != table.end()) {
     if (table[get_fixed_subjects_column()] == get_fixed_subjects_choice()) {
       return true;
     }
