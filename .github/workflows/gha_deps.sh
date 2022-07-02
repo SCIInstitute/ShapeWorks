dbdddfc96b9b1d0a3f8b8d38f67a51feaaedf482
Alan Morris <akenmorris@gmail.com>
2022-07-01 22:00:30 -0600

    More build fix attempts

--- .github/workflows/gha_deps.sh
+++ .github/workflows/gha_deps.sh
@@ -23,7 +23,7 @@ else
 	export FILE="${DEP_FILE}"
     fi
     export SDKROOT=$HOME/MacOSX10.13.sdk # only needed for MacOS obviously
-    ./build_dependencies.sh --build-type=$BUILD_TYPE --num-procs=1
+    ./build_dependencies.sh --build-type=$BUILD_TYPE --num-procs=3
     rm -rf $BUILD_DIR
 
     echo "Create and store cache"
