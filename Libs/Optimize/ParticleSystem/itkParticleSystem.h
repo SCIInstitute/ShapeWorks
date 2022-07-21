63d89ceff424f35a456571124d1f8383ae930fec
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2022-07-21 15:06:24 -0600

    Removed geo dist

--- Libs/Optimize/ParticleSystem/itkParticleSystem.h
+++ Libs/Optimize/ParticleSystem/itkParticleSystem.h
@@ -376,11 +376,6 @@ class ParticleSystem : public DataObject {
     m_Domains, m_Positions, and m_Transform lists. */
   void SetNumberOfDomains(unsigned int);
 
-  /** Adds a MeshWrapper for geodesic distance computation */
-  void AddMeshWrapper(std::shared_ptr<shapeworks::VtkMeshWrapper> mesh) {GeoMeshes.push_back(mesh);}
-
-  std::vector<std::vector<size_t> > Compute6NN(size_t domain);
-
  protected:
   ParticleSystem();
   void PrintSelf(std::ostream &os, Indent indent) const;
@@ -465,9 +460,6 @@ class ParticleSystem : public DataObject {
   std::vector<std::vector<bool>> m_FixedParticleFlags;
 
   std::mt19937 m_rand{42};
-
-  // Meshes to query geo distances
-  std::vector<std::shared_ptr<shapeworks::VtkMeshWrapper> > GeoMeshes;
 };
 
 }  // end namespace itk
