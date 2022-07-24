fb0b7f046602564ab170efefd24cbfd3ada0c3b1
Alan Morris <akenmorris@gmail.com>
2022-07-24 16:19:48 -0600

    File and transform output

--- Libs/Project/JsonProjectWriter.cpp
+++ Libs/Project/JsonProjectWriter.cpp
@@ -6,12 +6,74 @@ using json = nlohmann::json;
 
 namespace shapeworks {
 
-static json create_data_sheet(ProjectHandle project) {
-  json j;
-
-  return j;
+//---------------------------------------------------------------------------
+static void assign_keys(json &j, std::string prefix, std::vector<std::string> filenames,
+                        std::vector<std::string> domains) {
+  if (filenames.empty()) {
+    return;
+  }
+  if (filenames.size() != domains.size()) {
+    throw std::runtime_error(prefix + " filenames and number of domains mismatch");
+  }
+  for (int i = 0; i < domains.size(); i++) {
+    std::string key = prefix + "_" + domains[i];
+    std::string value = filenames[i];
+    j[key] = value;
+  }
 }
 
+//---------------------------------------------------------------------------
+static std::string transform_to_string(std::vector<double> transform) {
+  std::string str;
+  for (int j = 0; j < transform.size(); j++) {
+    str = str + " " + std::to_string(transform[j]);
+  }
+  return str;
+}
+
+//---------------------------------------------------------------------------
+static void assign_transforms(json &j, std::string prefix, std::vector<std::vector<double>> transforms,
+                              std::vector<std::string> domains) {
+  if (transforms.empty()) {
+    return;
+  }
+  if (transforms.size() != domains.size()) {
+    throw std::runtime_error(prefix + " filenames and number of domains mismatch");
+  }
+  for (int i = 0; i < domains.size(); i++) {
+    std::string key = prefix + "_" + domains[i];
+    std::string value = transform_to_string(transforms[i]);
+    j[key] = value;
+  }
+}
+
+//---------------------------------------------------------------------------
+static json create_data_sheet(ProjectHandle project) {
+  // json j;
+
+  auto subjects = project->get_subjects();
+
+  auto domains = project->get_domain_names();
+
+  std::vector<json> list;
+  for (int i = 0; i < subjects.size(); i++) {
+    std::shared_ptr<Subject> subject = subjects[i];
+    json j;
+    j["name"] = subject->get_display_name();
+
+    assign_keys(j, "shape", subject->get_original_filenames(), domains);
+    assign_keys(j, "landmarks_file", subject->get_landmarks_filenames(), domains);
+    assign_keys(j, "groomed", subject->get_groomed_filenames(), domains);
+    assign_keys(j, "local_particles", subject->get_local_particle_filenames(), domains);
+    assign_keys(j, "world_particles", subject->get_world_particle_filenames(), domains);
+    assign_transforms(j, "alignment", subject->get_groomed_transforms(), domains);
+    assign_transforms(j, "procrustes", subject->get_procrustes_transforms(), domains);
+  }
+
+  return list;
+}
+
+//---------------------------------------------------------------------------
 bool JsonProjectWriter::write_project(ProjectHandle project, std::string filename) {
   json j;
 
