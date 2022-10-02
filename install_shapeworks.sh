09165f311938504307bed7efbdaf532c664bbd01
Alan Morris <akenmorris@gmail.com>
2022-10-02 11:37:54 -0600

    Attempt to address #1885, restructuring conda installation

--- install_shapeworks.sh
+++ install_shapeworks.sh
@@ -3,7 +3,7 @@
 #
 echo ""
 echo "Note: this script only supports bash and zsh shells "
-echo "      It must be called using \"source ./install_shapeworks.sh [optional_env_name]\""
+echo "      It must be called using \"source ./install_shapeworks.sh [--developer] [optional_env_name]\""
 echo ""
 
 (return 0 2>/dev/null) && sourced=1 || sourced=0
@@ -13,12 +13,33 @@ if [[ "$sourced" == "0" ]]; then
   exit 1
 fi
 
+
+POSITIONAL_ARGS=()
+DEVELOPER=NO
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --developer)
+      DEVELOPER=YES
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
 CONDAENV=shapeworks
-if [[ "$#" -eq 1 ]]; then
-   CONDAENV=$1
+if [[ "${#POSITIONAL_ARGS[@]}" -eq 1 ]]; then
+   CONDAENV=${POSITIONAL_ARGS[1]}
 fi
 
-echo "Creating new conda environment for ShapeWorks called $CONDAENV..."
+echo "Creating new conda environment for ShapeWorks called \"$CONDAENV\"..."
+
 
 # PyTorch installation
 function install_pytorch() {
@@ -71,41 +92,48 @@ function install_conda() {
   conda config --add channels conda-forge
 
   CONDA_PACKAGES=(python=3.9.13 \
-    cmake=3.23.2 \
-    gtest=1.11.0 \
-    gmock=1.11.0 \
     colorama=0.4.4 \
     requests=2.27.1 \
     geotiff=1.7.1 \
     numpy=1.22.4 \
     openblas=0.3.20 \
-    doxygen=1.9.2 \
-    graphviz=4.0.0 \
     'vtk=9.1.0=qt*' \
     scikit-learn=1.1.1 \
-    tbb=2021.5.0 \
-    tbb-devel=2021.5.0 \
-    boost=1.74.0 \
-    openexr=3.1.5 \
-    ilmbase=2.5.5 \
-    pybind11=2.9.2 \
-    nlohmann_json=3.10.5 \
-    spdlog=1.10.0 \
-    pkg-config=0.29.2 \
-    openh264==2.3.0 \
-    libhwloc=2.8.0 \
     pip=22.1.2
    )		      
 
-  # linux (only) deps
-  if [[ "$(uname)" == "Linux" ]]; then
-      # required by install_python_module.sh
-      CONDA_PACKAGES+=(zlib=1.2.12 patchelf=0.14.5)
-  fi
+  if [[ "$DEVELOPER" == "YES" ]] ; then
+    echo "Developer packages enabled"
 
+    CONDA_PACKAGES+=(cmake=3.23.2 \
+		     gtest=1.11.0 \
+		     gmock=1.11.0 \
+		     doxygen=1.9.2 \
+		     graphviz=4.0.0 \
+		     tbb=2021.5.0 \
+		     tbb-devel=2021.5.0 \
+		     boost=1.74.0 \
+		     openexr=3.1.5 \
+		     ilmbase=2.5.5 \
+		     pybind11=2.9.2 \
+		     nlohmann_json=3.10.5 \
+		     spdlog=1.10.0 \
+		     pkg-config=0.29.2 \
+		     openh264==2.3.0 \
+		     libhwloc=2.8.0 \
+		    )
+    
+    # linux (only) deps
+    if [[ "$(uname)" == "Linux" ]]; then
+	# required by install_python_module.sh
+	CONDA_PACKAGES+=(zlib=1.2.12 patchelf=0.14.5)
+    fi
+  fi
 
   echo "Installing CONDA_PACKAGES = ${CONDA_PACKAGES[@]}"
 
+  return
+  
   # create and activate shapeworks env
   if ! conda create --yes --name $CONDAENV ${CONDA_PACKAGES[@]} ; then
       return 1;
