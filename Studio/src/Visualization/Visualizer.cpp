a031015cf79e4bf2ad681e92eb4959fc8083b213
Alan Morris <akenmorris@gmail.com>
2022-11-10 17:05:34 -0700

    Add new particle coloring based on:
    https://sashamaps.net/docs/resources/20-colors/

--- Studio/src/Visualization/Visualizer.cpp
+++ Studio/src/Visualization/Visualizer.cpp
@@ -301,23 +301,38 @@ void Visualizer::update_lut() {
     }
   } else {
     auto lut = vtkSmartPointer<vtkLookupTable>::New();
-    lut->SetNumberOfTableValues(12);
+    lut->SetNumberOfTableValues(21);
 
     int v = 0;
-    lut->SetTableValue(v++, 0, 1, 0);  // green
-    lut->SetTableValue(v++, 1, 0, 0);  // red
-    lut->SetTableValue(v++, 0, 0, 1);  // blue
+    // lut->SetTableValue(v++, 0, 1, 0);  // green
+    // lut->SetTableValue(v++, 1, 0, 0);  // red
+    // lut->SetTableValue(v++, 0, 0, 1);  // blue
+    // lut->SetTableValue(v++, 255 / 255.0, 72 / 255.0, 228 / 255.0);  // purple
+    // lut->SetTableValue(v++, 71 / 255.0, 207 / 255.0, 255 / 255.0);  // teal
 
-    lut->SetTableValue(v++, 255 / 255.0, 72 / 255.0, 228 / 255.0);  // purple
-    lut->SetTableValue(v++, 71 / 255.0, 207 / 255.0, 255 / 255.0);  // teal
+    auto add_color = [&](int r, int g, int b) { lut->SetTableValue(v++, r / 255.0, g / 255.0, b / 255.0); };
 
-    lut->SetTableValue(v++, 58 / 255.0, 130 / 255.0, 161 / 255.0);
-    lut->SetTableValue(v++, 239 / 255.0, 201 / 255.0, 112 / 255.0);
-    lut->SetTableValue(v++, 106 / 255.0, 80 / 255.0, 121 / 255.0);
-    lut->SetTableValue(v++, 170 / 255.0, 213 / 255.0, 116 / 255.0);
-    lut->SetTableValue(v++, 187 / 255.0, 80 / 255.0, 57 / 255.0);
-    lut->SetTableValue(v++, 242 / 255.0, 163 / 255.0, 100 / 255.0);
-    lut->SetTableValue(v++, 159 / 255.0, 219 / 255.0, 208 / 255.0);
+    add_color(230, 25, 75);
+    add_color(60, 180, 75);
+    add_color(255, 225, 25);
+    add_color(0, 130, 200);
+    add_color(245, 130, 48);
+    add_color(145, 30, 180);
+    add_color(70, 240, 240);
+    add_color(240, 50, 230);
+    add_color(210, 245, 60);
+    add_color(250, 190, 212);
+    add_color(0, 128, 128);
+    add_color(220, 190, 255);
+    add_color(170, 110, 40);
+    add_color(255, 250, 200);
+    add_color(128, 0, 0);
+    add_color(170, 255, 195);
+    add_color(128, 128, 0);
+    add_color(255, 215, 180);
+    add_color(0, 0, 128);
+    add_color(128, 128, 128);
+    add_color(255, 255, 255);
 
     // normal particle coloring mode
     glyph_lut_->ForceBuild();
