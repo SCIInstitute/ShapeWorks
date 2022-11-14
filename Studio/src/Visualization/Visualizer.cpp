4ba292600434aabea19d67b8cc6c0081598a32c6
Alan Morris <akenmorris@gmail.com>
2022-11-14 13:31:08 -0700

    Implement preference for controlling particle coloring mode.

--- Studio/src/Visualization/Visualizer.cpp
+++ Studio/src/Visualization/Visualizer.cpp
@@ -3,6 +3,7 @@
 #include <Shape.h>
 #include <Utils/StudioUtils.h>
 #include <Visualization/ColorMap.h>
+#include <Visualization/ParticleColors.h>
 #include <Visualization/Visualizer.h>
 #include <vtkAppendPolyData.h>
 #include <vtkLookupTable.h>
@@ -300,44 +301,41 @@ void Visualizer::update_lut() {
       }
     }
   } else {
-    auto lut = vtkSmartPointer<vtkLookupTable>::New();
-    lut->SetNumberOfTableValues(21);
+    if (preferences_.get_particle_colors() == ParticleColors::ParticleColorsType::Distinct) {
+      auto lut = vtkSmartPointer<vtkLookupTable>::New();
+      lut->SetNumberOfTableValues(21);
 
-    int v = 0;
-    // lut->SetTableValue(v++, 0, 1, 0);  // green
-    // lut->SetTableValue(v++, 1, 0, 0);  // red
-    // lut->SetTableValue(v++, 0, 0, 1);  // blue
-    // lut->SetTableValue(v++, 255 / 255.0, 72 / 255.0, 228 / 255.0);  // purple
-    // lut->SetTableValue(v++, 71 / 255.0, 207 / 255.0, 255 / 255.0);  // teal
+      int v = 0;
+      auto add_color = [&](int r, int g, int b) { lut->SetTableValue(v++, r / 255.0, g / 255.0, b / 255.0); };
 
-    auto add_color = [&](int r, int g, int b) { lut->SetTableValue(v++, r / 255.0, g / 255.0, b / 255.0); };
+      add_color(230, 25, 75);
+      add_color(60, 180, 75);
+      add_color(255, 225, 25);
+      add_color(0, 130, 200);
+      add_color(245, 130, 48);
+      add_color(145, 30, 180);
+      add_color(70, 240, 240);
+      add_color(240, 50, 230);
+      add_color(210, 245, 60);
+      add_color(250, 190, 212);
+      add_color(0, 128, 128);
+      add_color(220, 190, 255);
+      add_color(170, 110, 40);
+      add_color(255, 250, 200);
+      add_color(128, 0, 0);
+      add_color(170, 255, 195);
+      add_color(128, 128, 0);
+      add_color(255, 215, 180);
+      add_color(0, 0, 128);
+      add_color(128, 128, 128);
+      add_color(255, 255, 255);
 
-    add_color(230, 25, 75);
-    add_color(60, 180, 75);
-    add_color(255, 225, 25);
-    add_color(0, 130, 200);
-    add_color(245, 130, 48);
-    add_color(145, 30, 180);
-    add_color(70, 240, 240);
-    add_color(240, 50, 230);
-    add_color(210, 245, 60);
-    add_color(250, 190, 212);
-    add_color(0, 128, 128);
-    add_color(220, 190, 255);
-    add_color(170, 110, 40);
-    add_color(255, 250, 200);
-    add_color(128, 0, 0);
-    add_color(170, 255, 195);
-    add_color(128, 128, 0);
-    add_color(255, 215, 180);
-    add_color(0, 0, 128);
-    add_color(128, 128, 128);
-    add_color(255, 255, 255);
-
-    // normal particle coloring mode
-    glyph_lut_->ForceBuild();
-    for (int i = 0; i < num_points; i++) {
-      glyph_lut_->SetTableValue(i, lut->GetTableValue(i % lut->GetNumberOfTableValues()));
+      // normal particle coloring mode
+      for (int i = 0; i < num_points; i++) {
+        glyph_lut_->SetTableValue(i, lut->GetTableValue(i % lut->GetNumberOfTableValues()));
+      }
+    } else {
+      glyph_lut_->ForceBuild();
     }
   }
 
