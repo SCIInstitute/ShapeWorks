15169010e030df6cff2663569ae8759977c9c168
Alan Morris <akenmorris@gmail.com>
2022-11-14 13:55:46 -0700

    Added missing files.

---
+++ Studio/src/Visualization/ParticleColors.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <qobjectdefs.h>
+#include <vtkLookupTable.h>
+
+namespace shapeworks {
+
+class ParticleColors : public QObject {
+  Q_OBJECT;
+
+ public:
+  enum ParticleColorsType { Distinct = 0, Original = 1 };
+  Q_ENUM(ParticleColorsType);
+
+  static vtkSmartPointer<vtkLookupTable> construct(ParticleColorsType type, int num);
+};
+
+}  // namespace shapeworks
