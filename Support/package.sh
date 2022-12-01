14987affe3e12abc7d01451e1dc4c4e6c9e70342
Alan Morris <akenmorris@gmail.com>
2022-12-01 09:28:14 -0700

    No need to copy libfmt

--- Support/package.sh
+++ Support/package.sh
@@ -116,7 +116,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     cd ..
 else
     # Copy libraries from anaconda
-    conda_libs="libboost_iostreams libboost_filesystem libbz2 liblzma liblz4 libtbb libHalf libpython libz libspd libfmt"
+    conda_libs="libboost_iostreams libboost_filesystem libbz2 liblzma liblz4 libtbb libHalf libpython libz libspd"
     for clib in $conda_libs; do
         cp ${CONDA_PREFIX}/lib/${clib}* lib
     done
