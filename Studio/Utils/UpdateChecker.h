7e227bb1bd12a564f6e4a76b1d7d5a8f8a79d89e
Alan Morris <akenmorris@gmail.com>
2022-12-16 10:46:05 -0700

    Add UpdateChecker file.

---
+++ Studio/Utils/UpdateChecker.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace shapeworks {
+
+//! Update Checker
+/*!
+ * This class checks for new versions of ShapeWorks to inform the user
+ */
+class UpdateChecker {
+ public:
+ private:
+};
+
+}  // namespace shapeworks
