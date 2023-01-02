d044e2dcc1de6ef11fc7506151b00bccc69f6a61
nawazishkhan1-nk <nawazishkhan1.nk@gmail.com>
2023-01-02 13:54:49 -0700

    main

---
+++ Python/NonLinearSSM/main.py
@@ -0,0 +1,21 @@
+from model import InvertibleNetwork
+from model import *
+
+# Train Burn-In Model
+particles_dir = ''
+
+inv_net = InvertibleNetwork(init_particles_dir=particles_dir)
+inv_net.train_invertible_network()
+
+
+# Set up Shapeworks Optimizer Object
+sw_opt = sw.Optimize()
+def before_callback():
+    inv_net.norm_model.eval()
+    particles = sw_opt.GetParticleSystem()
+
+    # Compute gradients
+
+    # Set gradients
+    
+    pass
--- Python/NonLinearSSM/model.py
+++ Python/NonLinearSSM/model.py
@@ -17,6 +17,10 @@ from nflib.flows import (
     MAF, NormalizingFlowModel,
 )
 params = DictMap(json.load(open('params_file.json')))
+M = params.num_corr
+N = params.num_samples
+d = params.dimension
+
 DEVICE = torch.device(params.device if torch.cuda.is_available() else 'cpu')
 torch.cuda.empty_cache()
 
@@ -42,12 +46,9 @@ def get_flow_model(flow_model_type, d, M):
     if flow_model_type == 'MAF':
         return [MAF(dim=d*M, parity=i%2, device=DEVICE, nh=128) for i in range(10)]
 
-class NonLinearSSM:
+class InvertibleNetwork:
     def __init__(self, init_particles_dir, flow_model_type='MAF') -> None:
         self.prior_mean, self.prior_cov, self.prior = get_prior_distribution(params)
-        M = params.num_corr
-        N = params.num_samples
-        d = params.dimension
         self.flow_model_type = flow_model_type
         self.init_particles = load_shape_matrix(init_particles_dir, N, M, d)
         self.init_particles_data = DatasetSuperShapes(self.init_particles)
@@ -60,10 +61,34 @@ class NonLinearSSM:
         pass
 
     def train_invertible_network(self):
-        num_iter = params.num_initial_iterations
+        num_iter = params.num_initial_iterations # burn in iterations
+        batch_size = params.batch_size
         optimizer = optim.Adam(self.model.parameters(), 
                             lr=params.learning_rate,
                             weight_decay=params.weight_decay)
         self.norm_model.train()
+
+        for k in range(num_iter):
+            xx = data.sample(batch_size).to(DEVICE)
+            xx = xx.reshape(batch_size, -1)
+            zs, prior_logprob, log_det = self.norm_model(xx.float())
+            logprob = prior_logprob + log_det
+            loss = -torch.sum(logprob) # NLL
+            self.norm_model.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if k % 1000 == 0:
+                logging.info(f"Epoch = {k} | Loss = {loss.item()}")
+
+        
+        # Compute Jacobian and save it
+
+
+        # Save model checkpoint
+
+
+
+
 
 
