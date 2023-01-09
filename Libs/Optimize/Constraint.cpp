4311b4781efa1057dcba44a0ab31405d28e0f831
Alan Morris <akenmorris@gmail.com>
2023-01-09 00:02:15 -0700

    Remove mu term from free form constraint as it seems to be causing some problems.

--- Libs/Optimize/Constraint.cpp
+++ Libs/Optimize/Constraint.cpp
@@ -86,7 +86,9 @@ Eigen::Vector3d Constraint::lagragianGradient(const Eigen::Vector3d &pt, double 
   */
   Eigen::Vector3d constraint_grad = constraintGradient(pt);
   double eval = constraintEval(pt);
-  double maxterm = mu_ + C * eval;
+  /// Alan: removed mu_ term as it seems to be causing some problems
+  //double maxterm = mu_ + C * eval;
+  double maxterm = C * eval;
   if (maxterm < 0) {
   //if ( C * eval < 0) {
     return Eigen::Vector3d(0, 0, 0);
