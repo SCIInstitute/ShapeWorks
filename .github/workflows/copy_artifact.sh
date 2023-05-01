1ade462d1a8d82e45183faf4470ccd6f27684814
Alan Morris <akenmorris@gmail.com>
2023-05-01 12:56:58 -0600

    Try cygpath.

--- .github/workflows/copy_artifact.sh
+++ .github/workflows/copy_artifact.sh
@@ -12,6 +12,8 @@ compress_file $TARGET "${2}"
 
 if [[ "$PLATFORM" == "windows" ]]; then
     choco install rsync -y
+    TARGET=`cygpath -m {TARGET}`
+    echo "Using target ${TARGET}"
 fi
 
 # try 5 times
