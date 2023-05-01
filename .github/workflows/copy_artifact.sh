4e7cc27c91377584f90e703c211dffb1ffc93165
Alan Morris <akenmorris@gmail.com>
2023-05-01 16:39:16 -0600

    Fix TARGET, continue on failure.

--- .github/workflows/copy_artifact.sh
+++ .github/workflows/copy_artifact.sh
@@ -12,13 +12,13 @@ compress_file $TARGET "${2}"
 
 if [[ "$PLATFORM" == "windows" ]]; then
     choco install rsync -y
-    TARGET=`cygpath -m {TARGET}`
+    TARGET=`cygpath -m ${TARGET}`
     echo "Using target ${TARGET}"
 fi
 
 # try 5 times
 for run in {1..5}; do
-    rsync ${TARGET} runner@${CACHE_HOST}:github/artifacts
+    rsync ${TARGET} runner@${CACHE_HOST}:github/artifacts ||:
     sleep 1
 done
 rm ${TARGET}
