9d284597029fc7eb46fe58bcf63e05f028f670bf
Alan Morris <akenmorris@gmail.com>
2023-05-04 12:26:29 -0600

    The 2nd template argument should be the coordinate representation, which since we are using itk::Point<double, 3>, this should have been double, not the float that Image::PixelType is.  The default is double, so I am just removing it.

--- Libs/Image/Image.h
+++ Libs/Image/Image.h
@@ -32,7 +32,7 @@ class Image {
   using ImageType = itk::Image<PixelType, 3>;
   using StatsPtr = itk::StatisticsImageFilter<ImageType>::Pointer;
   using ImageIterator = itk::ImageRegionIterator<ImageType>;
-  using InterpolatorType = itk::LinearInterpolateImageFunction<ImageType, Image::PixelType>;
+  using InterpolatorType = itk::LinearInterpolateImageFunction<ImageType>;
 
   // constructors and assignment operators //
   Image(const Dims dims);
