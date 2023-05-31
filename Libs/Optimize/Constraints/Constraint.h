8bcd2dcb282ee54d623809cbe38881116c1562f5
HeavenlyBerserker <hxu.yunocreative@gmail.com>
2023-05-31 15:53:25 -0600

    Added comments all over the place

--- Libs/Optimize/Constraints/Constraint.h
+++ Libs/Optimize/Constraints/Constraint.h
@@ -10,33 +10,55 @@
 
 namespace shapeworks {
 
+/**
+ * \class Constraint
+ * \ingroup Group-Constraints
+ *
+ *  This class is the general constraint class. Each instance represents a single constraint, either cutting-plane, sphere or free-form. They all inherit from this class. This class containts all the infrastructure to handle gradients and evaluations, which is shared among all constraint types.
+ *  NOTE: Not actually using the augmented lagrangian. We are using quadratic penalty and not lagrangian because it works better.
+ *
+ */
+
 class Constraint {
  public:
+  /// Returns if pt in vnl_vector format is violated by the constraint
   bool isViolated(const vnl_vector<double> &pt) const { return isViolated(Eigen::Vector3d(pt[0], pt[1], pt[2])); }
+  /// Returns if pt in Eigen format is violated by the constraint
   virtual bool isViolated(const Eigen::Vector3d &pt) const = 0;
+  /// Prints the constraint neatly
   virtual void print() const = 0;
 
   // For augmented lagrangian
   void setZ(double inz) { z_ = inz; }
   double getZ() { return z_; }
+  /// Initializes mu
   void setMus(std::vector<double> inmu) { mus_ = inmu; }
+  /// Gets mu
   std::vector<double> getMus() { return mus_; }
+  /// Sets Lambda
   void setLambda(double inLambda) { lambda_ = inLambda; }
+  /// Gets Lambda
   double getLambda() { return lambda_; }
 
+  /// Returns the gradient of the constraint
   virtual Eigen::Vector3d constraintGradient(const Eigen::Vector3d &pt) const = 0;
+  /// Returns the evaluation on the constraint, i.e. the signed distance to the constraint boundary
   virtual double constraintEval(const Eigen::Vector3d &pt) const = 0;
 
   void updateZ(const Eigen::Vector3d &pt, double C);
 
+  /// Updates the value of mu according to the augmented lagrangian update
   void updateMu(const Eigen::Vector3d &pt, double C, size_t index);
 
+  /// Computes the lagrangian gradient based on lagrangian inequality equations. NOTE: Not actually lagrangian. We are using quadratic penalty and not lagrangian because it works better.
   Eigen::Vector3d lagragianGradient(const Eigen::Vector3d &pt, double C, size_t index) const;
 
  protected:
+  /// Returns the sign of the double
   int sgn(double val) { return (double(0) < val) - (val < double(0)); }
 
   // For augmented lagrangian
+  /// Mu is the lagrangian momentum term
   std::vector<double> mus_;
   double z_;
   double lambda_;
--- Libs/Optimize/Constraints/Constraints.h
+++ Libs/Optimize/Constraints/Constraints.h
@@ -14,51 +14,79 @@
 
 namespace shapeworks {
 
+/**
+ * \class Constraints
+ * \ingroup Group-Constraints
+ *
+ * This class contains the set of boundary constraints that are appleid to the shapes in the dataset. They are used to isolate areas of interest on shape surfaces/volumes. These boundary constraints currently take the form of cutting planes, cutting spheres (deprecated), or free-form constraints (FFCs).
+ * Do not confuse these with the ApplyConstraints function found in the "domain" code, they refer to the action of snapping to the surface and do not serve to isolate areas of interest.
+ *
+ */
+
 class Constraints {
  public:
   using Point3 = itk::Point<double, 3>;
 
+  /// Constraints are inactive when initialized
   Constraints() { active_ = false; }
 
   ~Constraints() {}
 
   // Set constraints
+  /// Adds a cutting-plane boundary constraint. Constraints are used to isolate areas of interest on shape surfaces
   void addPlane(const vnl_vector<double> &a, const vnl_vector<double> &b, const vnl_vector<double> &c);
+  /// Adds a sphere boundary constraint. Constraints are used to isolate areas of interest on shape surfaces
   void addSphere(const vnl_vector_fixed<double, DIMENSION> &v, double r);
+  /// Adds the free-form boundary constraint, or FFC. Constraints are used to isolate areas of interest on shape surfaces
   void addFreeFormConstraint(std::shared_ptr<shapeworks::Mesh> mesh);
 
   // Transforms
+  /// Applies transformations to free-form constraints (FFCs)
   bool transformConstraints(const vnl_matrix_fixed<double, 4, 4> &transform);
+  /// Applies transformations to cutting-plane constraints
   bool transformPlanes(const vnl_matrix_fixed<double, 4, 4> &transform);
 
   // Apply functions
+  /// DEPRECATED: Applies constraints by snapping particles back to a projected region. Double vector
   std::stringstream applyBoundaryConstraints(vnl_vector_fixed<double, 3> &gradE, const Point3 &pos);
+  /// DEPRECATED: Applies constraints by snapping particles back to a projected region. Float vector
   std::stringstream applyBoundaryConstraints(vnl_vector_fixed<float, 3> &gradE, const Point3 &pos);
+  /// DEPRECATED: Applies plane constraints by snapping particles back to a projected region. Double vector
   std::stringstream applyPlaneConstraints(vnl_vector_fixed<double, 3> &gradE, const Point3 &pos);
 
   // Write constraints
+  /// Writes out a cutting plane into filename
   bool writePlanes(std::string filename) { return true; }
   bool writeSpheres(std::string filename) { return true; }
   bool writeFreeFormConstraint(std::string filename) { return true; }
 
   // Is defined? functions
+  /// Returns true if at least one cutting plane is defined
   bool isCuttingPlaneDefined() const { return !planeConstraints_.empty(); }
+  /// Returns true if at least one sphere constraint is defined
   bool isCuttingSphereDefined() const { return !sphereConstraints_.empty(); }
 
-  // Plane constraint
+  // Constraint get function
+  /// Returns the vector that constains all plane constraints, of type PlaneConstraint. See class PlaneConstraint for more info
   std::vector<PlaneConstraint> &getPlaneConstraints() { return planeConstraints_; }
+  /// Returns the vector that constains all sphere constraints, of type SphereConstraint. See class SphereConstraint for more info
   std::vector<SphereConstraint> &getSphereConstraints() { return sphereConstraints_; }
+  /// Returns the free form constraint, of type FreeFormConstraint. See class FreeFormConstraint for more info
+  FreeFormConstraint& getFreeformConstraint();
 
-  // Is any constraint violated by point pos?
+  /// Returns true if any constraint is violated by point pos
   bool isAnyViolated(const Point3 &pos);
 
-  // Constraint violations
+  /// Returns true if any cutting-plane constraint is violated by point pt
   std::vector<int> planesViolated(Eigen::Vector3d pt);
 
+  /// Prints all constraints in a neat format. Make sure to disable multithreading if printing within to optimization to avoid jumbled output
   void printAll();
 
+  /// Prints out a comprehensive violation report with each constraint that is violated and the magnitude of such violation by point pos. Index can be 0 if one does not have the index.
   std::string violationReport(const Point3 &pos, size_t index);
 
+  /// Returns the distance to every cosntraint with [0] plane, [1] sphere, and [2] FFC
   std::vector<std::vector<double>> violationReportData(const Point3 &pos);
 
   // ============================
@@ -67,33 +95,38 @@ class Constraints {
   // Energy gradient computations
   vnl_vector_fixed<double, 3> constraintsGradient(const Point3 &pos) const;
 
-  // Lagragian gradient computation
+  /// Returns the constraint gradient. prepos is the current particle, pos is the particle with an optimization update applied, C can be 1, and index is the index of the particle.
   vnl_vector_fixed<double, 3> constraintsLagrangianGradient(const Point3 &pos, const Point3 &prepos, double C, size_t index);
 
-  // Parameters lambda, mu and z initialization
+  ///Initializes lagrangian parameters lambdas and mus
   void InitializeLagrangianParameters(double lambda, std::vector<double> mus);
 
   void UpdateZs(const Point3 &pos, double C);
 
+  /// Updates mus, the momentum variable of the augmented lagrangian
   void UpdateMus(const Point3 &pos, double C, size_t index);
 
+  /// Gets the variable active, which determines whether constraints are being used
   bool GetActive() { return active_; }
+  /// Sets the variable active, which determines whether constraints are being used
   void SetActive(bool ac) { active_ = ac; }
 
+  /// Reads in a constraint json file named filename
   void read(std::string filename);
+  /// Writes out a constraint json file named filename
   void write(std::string filename);
-
-  FreeFormConstraint& getFreeformConstraint();
 
   bool hasConstraints();
 
-  // return a mesh that has been clipped by the constraints
+  /// Clips the mesh that has been passed into it by the constraints stored in this object.
   void clipMesh(Mesh& mesh);
 
  private:
+  /// Vector containing the cutting-plane boundary constraints. Constraints are used to isolate areas of interest on shape surfaces
   std::vector<PlaneConstraint> planeConstraints_;
+  /// Vector containing the sphere boundary constraints. Constraints are used to isolate areas of interest on shape surfaces
   std::vector<SphereConstraint> sphereConstraints_;
-
+  /// The object that constains a free-form boundary constraints. Constraints are used to isolate areas of interest on shape surfaces
   FreeFormConstraint freeFormConstraint_;
 
   // Projections and intersects
