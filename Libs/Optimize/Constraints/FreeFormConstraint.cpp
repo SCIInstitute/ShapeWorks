2a584d58751f05c5c0d6fa47ed29eca197b34dc7
Alan Morris <akenmorris@gmail.com>
2023-08-24 12:46:19 -0600

    Resolve #1517 by adding an option to copy free form constraints from one shape to the others using the alignment transforms.

--- Libs/Optimize/Constraints/FreeFormConstraint.cpp
+++ Libs/Optimize/Constraints/FreeFormConstraint.cpp
@@ -30,6 +30,7 @@ namespace shapeworks {
 //-----------------------------------------------------------------------------
 bool FreeFormConstraint::readyForOptimize() const { return mesh_ != nullptr; }
 
+//-----------------------------------------------------------------------------
 bool FreeFormConstraint::isViolated(const Eigen::Vector3d& pt) const {
   if (constraintEval(pt) >= 0) {
     return false;
@@ -298,7 +299,6 @@ std::vector<Eigen::Matrix3d> FreeFormConstraint::setGradientFieldForFFCs(std::sh
 
   // Flatten the gradient operator so we can quickly compute the gradient at a given point
   face_grad.resize(F.rows());
-  size_t n_insertions = 0;
   for (int k = 0; k < G.outerSize(); k++) {
     for (Eigen::SparseMatrix<double>::InnerIterator it(G, k); it; ++it) {
       const double val = it.value();
@@ -310,7 +310,6 @@ std::vector<Eigen::Matrix3d> FreeFormConstraint::setGradientFieldForFFCs(std::sh
       for (int i = 0; i < 3; i++) {
         if (F(f, i) == c) {
           face_grad[f](axis, i) = val;
-          n_insertions++;
           break;
         }
       }
@@ -353,6 +352,7 @@ std::vector<Eigen::Matrix3d> FreeFormConstraint::setGradientFieldForFFCs(std::sh
   return face_grad;
 }
 
+//-----------------------------------------------------------------------------
 void FreeFormConstraint::convertLegacyFFC(vtkSmartPointer<vtkPolyData> polyData) {
   if (polyData->GetPointData()->GetArray("ffc_paint")) {
     // clear out any old versions of the inout array or else they will get merged in
@@ -439,6 +439,7 @@ void FreeFormConstraint::convertLegacyFFC(vtkSmartPointer<vtkPolyData> polyData)
   createInoutPolyData();
 }
 
+//-----------------------------------------------------------------------------
 vtkSmartPointer<vtkFloatArray> FreeFormConstraint::computeInOutForFFCs(vtkSmartPointer<vtkPolyData> polyData,
                                                                        Eigen::Vector3d query,
                                                                        vtkSmartPointer<vtkPolyData> halfmesh) {
