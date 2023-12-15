bba039d589ac683d9e303e54b46e3a06f1a1e8ba
Alan Morris <akenmorris@gmail.com>
2023-12-15 10:37:38 -0700

    Fix formatting

--- Examples/Python/RunUseCase.py
+++ Examples/Python/RunUseCase.py
@@ -18,11 +18,11 @@ import subprocess
 import sys
 import shutil
 
-#check if current directory is writable
+# check if current directory is writable
 current_dir = os.getcwd()
-if not os.access(current_dir,os.W_OK):
-    raise OSError("You don't have write permission in the current directory. Please copy the Examples folder to a different location to run the use cases")
-
+if not os.access(current_dir, os.W_OK):
+    raise OSError(
+        "You don't have write permission in the current directory. Please copy the Examples folder to a different location to run the use cases")
 
 if __name__ == '__main__':
 
@@ -31,6 +31,7 @@ if __name__ == '__main__':
         import termcolor
         import DatasetUtils
         import shapeworks as sw
+
         print(f"Using shapeworks module from {sw.__file__}")
 
         # make sure the shapeworks executables can be found
@@ -41,29 +42,32 @@ if __name__ == '__main__':
 
     except ImportError as error:
         print("\nError: Unable to import ShapeWorks executables, Python module, or dependencies\n")
-        print("Users: Please make sure you have run \"source install_shapeworks.sh\" (once) and \"conda activate shapeworks\" (each time)")
+        print(
+            "Users: Please make sure you have run \"source install_shapeworks.sh\" (once) and \"conda activate shapeworks\" (each time)")
         print("\tSee http://sciinstitute.github.io/ShapeWorks/users/install.html for more information")
         print("\nDevelopers: Please make sure you have run \"source ./devenv.sh <SOURCE_DIR> <BUILD_DIR>\"")
         sys.exit(1)
 
-
     # parse arguments
     parser = argparse.ArgumentParser(description='Example ShapeWorks Pipeline')
     parser.add_argument("use_case", help="Must specify which of these use cases to run.",
-                        choices=["ellipsoid", "ellipsoid_evaluate", "ellipsoid_mesh", "ellipsoid_fd", "ellipsoid_cut", "ellipsoid_pca", \
-                                 "ellipsoid_multiple_domain","ellipsoid_multiple_domain_mesh", "lumps", "left_atrium", \
-                                 "femur_cut","femur_pvalues","deep_ssm", "supershapes_1mode_contour", "thin_cavity_bean",
-                                 "peanut_shared_boundary", "incremental_supershapes","hip_multiple_domain"])
+                        choices=["ellipsoid", "ellipsoid_evaluate", "ellipsoid_mesh", "ellipsoid_fd", "ellipsoid_cut",
+                                 "ellipsoid_pca", \
+                                 "ellipsoid_multiple_domain", "ellipsoid_multiple_domain_mesh", "lumps", "left_atrium", \
+                                 "femur_cut", "femur_pvalues", "deep_ssm", "supershapes_1mode_contour",
+                                 "thin_cavity_bean",
+                                 "peanut_shared_boundary", "incremental_supershapes", "hip_multiple_domain"])
     parser.add_argument("--use_subsample", help="Run the pipeline for a subset of data", action="store_true")
     parser.add_argument("--num_subsample", help="Size of subset to run on (default: %(default)s)", type=int, default=3)
     parser.add_argument("--interactive", help="Run in interactive mode", action="store_true")
-    parser.add_argument("--use_single_scale", help="Use single scale optimization (default: multi scale)", action="store_true")
-    parser.add_argument("--mesh_mode", help="Run optimization on meshes rather than distance transforms.",action="store_true")
+    parser.add_argument("--use_single_scale", help="Use single scale optimization (default: multi scale)",
+                        action="store_true")
+    parser.add_argument("--mesh_mode", help="Run optimization on meshes rather than distance transforms.",
+                        action="store_true")
     parser.add_argument("--tiny_test", help="Run as a short test", action="store_true")
     parser.add_argument("--verify", help="Run as a full test", action="store_true")
     parser.add_argument("--clean", help="Run from scratch, ignoring intermediate stages", action="store_true")
     args = parser.parse_args()
-
 
     type = ""
     if args.tiny_test:
@@ -79,8 +83,9 @@ if __name__ == '__main__':
     if args.use_subsample:
         dataExists = sw.data.dataset_exists_check(args.use_case)
         args.use_subsample = args.num_subsample
-        if(dataExists==False):
-            print("Please note: For --use_subsample argument the entire dataset will be downloaded. For a quick test use the --tiny_test argument")
+        if (dataExists == False):
+            print(
+                "Please note: For --use_subsample argument the entire dataset will be downloaded. For a quick test use the --tiny_test argument")
             input("Press any key to continue")
 
     # import use case and run
