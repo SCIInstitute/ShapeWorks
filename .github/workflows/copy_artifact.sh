9658ff090d08dda00dd5aec4d6df18bfcc1f3ebe
Alan Morris <akenmorris@gmail.com>
2024-01-24 16:43:01 -0700

    Update URLs for zstd

--- .github/workflows/copy_artifact.sh
+++ .github/workflows/copy_artifact.sh
@@ -14,7 +14,7 @@ if [[ "$PLATFORM" == "windows" ]]; then
     # install rsync on for windows/msys2 (the one from chocolatey doesn't work)
     # (adapted from https://github.com/GuillaumeFalourd/setup-rsync)
     echo "Installing rsync for Windows/msys2"
-    curl.exe --output rsync-3.2.3-2-x86_64.pkg.tar.zst --url https://repo.msys2.org/msys/x86_64/rsync-3.2.3-1-x86_64.pkg.tar.zst
+    curl.exe --output rsync-3.2.3-2-x86_64.pkg.tar.zst --url https://repo.msys2.org/msys/x86_64/rsync-3.2.3-2-x86_64.pkg.tar.zst
     zstd -d rsync-3.2.3-2-x86_64.pkg.tar.zst
     tar -xvf rsync-3.2.3-2-x86_64.pkg.tar
     mv usr/bin/rsync.exe "C:\Program Files\Git\usr\bin\\"
@@ -22,7 +22,7 @@ if [[ "$PLATFORM" == "windows" ]]; then
     zstd -d libzstd-1.5.2-1-x86_64.pkg.tar.zst
     tar -xvf libzstd-1.5.2-1-x86_64.pkg.tar
     mv usr/bin/msys-zstd-1.dll "C:\Program Files\Git\usr\bin\\"
-    curl.exe --output libxxhash-0.8.0-1-x86_64.pkg.tar.zst https://repo.msys2.org/msys/x86_64/libxxhash-0.8.0-1-x86_64.pkg.tar.zst
+    curl.exe --output libxxhash-0.8.0-1-x86_64.pkg.tar.zst https://repo.msys2.org/msys/x86_64/libxxhash-0.8.1-1-x86_64.pkg.tar.zst
     zstd -d libxxhash-0.8.0-1-x86_64.pkg.tar.zst
     tar -xvf libxxhash-0.8.0-1-x86_64.pkg.tar
     mv usr/bin/msys-xxhash-0.8.0.dll "C:\Program Files\Git\usr\bin\\"
