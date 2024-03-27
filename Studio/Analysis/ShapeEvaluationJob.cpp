343f58500c9b2a05580bb3a781c6e36e959d6c1f
Alan Morris <akenmorris@gmail.com>
2024-03-27 14:52:56 -0600

    Restyle/reformat ShapeEvaluationJob

--- Studio/Analysis/ShapeEvaluationJob.cpp
+++ Studio/Analysis/ShapeEvaluationJob.cpp
@@ -1,44 +1,38 @@
-
+#include "ShapeEvaluationJob.h"
 
 #include <Logging.h>
-#include "ShapeEvaluationJob.h"
+
 namespace shapeworks {
 
 //-----------------------------------------------------------------------------
 ShapeEvaluationJob::ShapeEvaluationJob(JobType job_type, ParticleShapeStatistics stats)
-  : job_type_(job_type), stats_(stats)
-{
-  qRegisterMetaType<shapeworks::ShapeEvaluationJob::JobType>(
-    "shapeworks::ShapeEvaluationWorker::JobType");
+    : job_type_(job_type), stats_(stats) {
+  qRegisterMetaType<shapeworks::ShapeEvaluationJob::JobType>("shapeworks::ShapeEvaluationWorker::JobType");
   qRegisterMetaType<Eigen::VectorXd>("Eigen::VectorXd");
 }
 
 //-----------------------------------------------------------------------------
-void ShapeEvaluationJob::run()
-{
+void ShapeEvaluationJob::run() {
   auto callback = std::bind(&ShapeEvaluationJob::receive_progress, this, std::placeholders::_1);
-  switch (this->job_type_) {
-  case JobType::CompactnessType:
-    Q_EMIT result_ready(this->job_type_, this->stats_.get_compactness(callback));
-    break;
-  case JobType::GeneralizationType:
-    Q_EMIT result_ready(this->job_type_, this->stats_.get_generalization(callback));
-    break;
-  case JobType::SpecificityType:
-    Q_EMIT result_ready(this->job_type_, this->stats_.get_specificity(callback));
-    break;
+  switch (job_type_) {
+    case JobType::CompactnessType:
+      Q_EMIT result_ready(job_type_, stats_.get_compactness(callback));
+      break;
+    case JobType::GeneralizationType:
+      Q_EMIT result_ready(job_type_, stats_.get_generalization(callback));
+      break;
+    case JobType::SpecificityType:
+      Q_EMIT result_ready(job_type_, stats_.get_specificity(callback));
+      break;
   }
 }
 
 //-----------------------------------------------------------------------------
-QString ShapeEvaluationJob::name()
-{
-  return "Shape Evaluation";
-}
+QString ShapeEvaluationJob::name() { return "Shape Evaluation"; }
 
 //-----------------------------------------------------------------------------
-void ShapeEvaluationJob::receive_progress(float progress)
-{
-  Q_EMIT report_progress(this->job_type_, progress);
-}
-}
+void ShapeEvaluationJob::receive_progress(float progress) { Q_EMIT report_progress(job_type_, progress); }
+
+//-----------------------------------------------------------------------------
+
+}  // namespace shapeworks
