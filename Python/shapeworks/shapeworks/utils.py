b70065b68f5c432ec5e5ac75578f08e9299c3558
Alan Morris <akenmorris@gmail.com>
2024-04-05 13:24:34 -0600

    Reorganize mesh warping in python

--- Python/shapeworks/shapeworks/utils.py
+++ Python/shapeworks/shapeworks/utils.py
@@ -10,8 +10,11 @@ import shapeworks as sw
 # Global shapeworks logger object (e.g. attached to Studio)
 sw_logger = None
 
+sw_warp = None
+
+
 # helper function to determine the best grid size (rows and columns) given the number of samples in a dataset.
-def postive_factors(num_samples):
+def positive_factors(num_samples):
     factors = []
 
     for whole_number in range(1, num_samples + 1):
@@ -20,22 +23,23 @@ def postive_factors(num_samples):
 
     return factors
 
+
 def num_subplots(num_samples):
-    factors = postive_factors(num_samples)
-    cols    = min(int(np.ceil(np.sqrt(num_samples))),max(factors))
-    rows    = int(np.ceil(num_samples/cols))
+    factors = positive_factors(num_samples)
+    cols = min(int(np.ceil(np.sqrt(num_samples))), max(factors))
+    rows = int(np.ceil(num_samples / cols))
 
     return rows, cols
 
+
 # a helper function that saves a list of shapeworks images in a directory
 # this could be used to save final and intermediate results (if needed)
-def save_images(outDir,        # path to the directory where we want to save the images
-                swImageList,   # list of shapeworks images to be saved
+def save_images(outDir,  # path to the directory where we want to save the images
+                swImageList,  # list of shapeworks images to be saved
                 swImageNames,  # list of image names to be used as filenames
-                extension        = 'nrrd',
-                compressed       = True, # use false to load in paraview
-                verbose          = True):
-
+                extension='nrrd',
+                compressed=True,  # use false to load in paraview
+                verbose=True):
     if (len(swImageList) != len(swImageNames)):
         print('swImageNames list is not consistent with number of images in swImageList')
         return
@@ -55,15 +59,15 @@ def save_images(outDir,        # path to the directory where we want to save the
         filenames.append(filename)
     return filenames
 
+
 # a helper function that saves a list of shapeworks meshes in a directory
 # this could be used to save final and intermediate results (if needed)
-def save_meshes(outDir,        # path to the directory where we want to save the images
-                swMeshList,   # list of shapeworks images to be saved
+def save_meshes(outDir,  # path to the directory where we want to save the images
+                swMeshList,  # list of shapeworks images to be saved
                 swMeshNames,  # list of image names to be used as filenames
-                extension        = 'ply',
-                compressed       = True,
-                verbose          = True):
-
+                extension='ply',
+                compressed=True,
+                verbose=True):
     if (len(swMeshList) != len(swMeshNames)):
         print('swMeshNames list is not consistent with number of images in swMeshList')
         return
@@ -83,16 +87,17 @@ def save_meshes(outDir,        # path to the directory where we want to save the
         filenames.append(filename)
     return filenames
 
-# a helper function to get list of files with specific extensions from a given file list
-def get_file_with_ext(file_list,extension):
 
-    extList =[]
+# a helper function to get list of files with specific extensions from a given file list
+def get_file_with_ext(file_list, extension):
+    extList = []
     for file in file_list:
         ext = file.split(".")[-1]
-        if(ext==extension):
+        if (ext == extension):
             extList.append(file)
     extList = sorted(extList)
     return extList
+
 
 # a helper function to convert absolute paths to a relative path from a base dir
 def get_relative_paths(files, base):
@@ -102,20 +107,22 @@ def get_relative_paths(files, base):
         relative_paths.append(path)
     return relative_paths
 
-def find_reference_image_index(inDataList,domains_per_shape=1):
+
+def find_reference_image_index(inDataList, domains_per_shape=1):
     mesh_list = []
     for img in inDataList:
         mesh = img.toMesh(0.5)
         mesh_list.append(mesh)
     return find_reference_mesh_index(mesh_list, domains_per_shape)
 
-def find_reference_mesh_index(mesh_list,domains_per_shape=1):
-    if(domains_per_shape==1):
+
+def find_reference_mesh_index(mesh_list, domains_per_shape=1):
+    if (domains_per_shape == 1):
         return sw.MeshUtils.findReferenceMesh(mesh_list)
     else:
-        combined_mesh = sw.data.combine_domains(mesh_list,domains_per_shape)
+        combined_mesh = sw.data.combine_domains(mesh_list, domains_per_shape)
         index = sw.MeshUtils.findReferenceMesh(combined_mesh)
-        return index,combined_mesh
+        return index, combined_mesh
 
 
 def save_contour_as_vtp(points, lines, filename):
@@ -130,7 +137,7 @@ def save_contour_as_vtp(points, lines, filename):
     n = points.shape[0]
     for j in range(n):
         x, y, z = points[j]
-        vtk_pts.InsertNextPoint((x,y,z))
+        vtk_pts.InsertNextPoint((x, y, z))
 
     vtk_lines = vtk.vtkCellArray()
     m = lines.shape[0]
@@ -149,6 +156,7 @@ def save_contour_as_vtp(points, lines, filename):
     writer.SetInputData(polydata)
     writer.Write()
 
+
 def compute_line_indices(n, is_closed=True):
     """
     Given a number of points, return indices for lines(as np.ndarray) between successive pairs of points.
@@ -156,19 +164,22 @@ def compute_line_indices(n, is_closed=True):
     n:         number of points
     is_closed: whether or not the last vertex is to to be connected to the first vertex
     """
-    lines = np.zeros((n if is_closed else n-1, 2), dtype=int)
+    lines = np.zeros((n if is_closed else n - 1, 2), dtype=int)
     for i in range(lines.shape[0]):
-        lines[i] = [i, (i+1)%n]
+        lines[i] = [i, (i + 1) % n]
 
     return lines
 
+
 def get_api_version():
     return "6.5"
+
 
 def set_sw_logger(log_object):
     """Set the shapeworks logger object"""
     global sw_logger
     sw_logger = log_object
+
 
 def sw_message(str):
     """If sw_logger is set, use it to log a message, otherwise print to console"""
@@ -178,6 +189,7 @@ def sw_message(str):
     else:
         print(str)
 
+
 def sw_check_abort():
     """If sw_logger is set, use it to check if abort has been called"""
     global sw_logger
@@ -186,11 +198,13 @@ def sw_check_abort():
     else:
         return False
 
+
 def sw_progress(progress, message=""):
     """If sw_logger is set, use it, otherwise do nothing"""
     global sw_logger
     if sw_logger is not None:
         sw_logger.progress(progress, message)
+
 
 def test(name, failure=False):
     if failure:
@@ -214,6 +228,7 @@ def test(name, failure=False):
             print(name.__name__ + " failed (exception): " + str(e))
             return False
 
+
 def expectException(name, etype):
     try:
         name()
@@ -225,27 +240,28 @@ def expectException(name, etype):
         print(name.__name__ + " failed (expected a different kind of exception): " + str(e))
         return False
 
-def getVTKtransform(itkTransform):
 
+def getVTKtransform(itkTransform):
     flippedITKtransform = np.copy(itkTransform)
-    lastColumn = itkTransform[:,-1]
-    lastRow = itkTransform[-1,:]
-    flippedITKtransform[:,-1] = lastRow
-    flippedITKtransform[-1,:] = lastColumn
+    lastColumn = itkTransform[:, -1]
+    lastRow = itkTransform[-1, :]
+    flippedITKtransform[:, -1] = lastRow
+    flippedITKtransform[-1, :] = lastColumn
     vtkTransform = np.linalg.inv(flippedITKtransform)
 
     return vtkTransform
 
-def getITKtransform(vtkTransform):
 
+def getITKtransform(vtkTransform):
     inverseVTKtransform = np.linalg.inv(vtkTransform)
     itkTransform = np.copy(inverseVTKtransform)
-    lastColumn = inverseVTKtransform[:,-1]
-    lastRow = inverseVTKtransform[-1,:]
-    itkTransform[:,-1] = lastRow
-    itkTransform[-1,:] = lastColumn
+    lastColumn = inverseVTKtransform[:, -1]
+    lastRow = inverseVTKtransform[-1, :]
+    itkTransform[:, -1] = lastRow
+    itkTransform[-1, :] = lastColumn
 
     return itkTransform
+
 
 def verify(args, world_point_files):
     # compare against baseline particles
@@ -284,6 +300,7 @@ def verify(args, world_point_files):
         return False
     return True
 
+
 def check_results(args, project_spreadsheet):
     # If tiny test or verify, check results and exit
     particle_dir = project_spreadsheet.replace(".xlsx", "_particles/").replace(".swproj", "_particles/")
@@ -305,6 +322,7 @@ def check_results(args, project_spreadsheet):
         print("Done with test, verification succeeded.")
         exit()
 
+
 def check_results_pattern(args, project_spreadsheet, pattern):
     # If tiny test or verify, check results and exit
     particle_dir = project_spreadsheet.replace(".xlsx", "_particles/").replace(".swproj", "_particles/")
@@ -319,6 +337,7 @@ def check_results_pattern(args, project_spreadsheet, pattern):
         print("Done with test, verification succeeded.")
         exit()
 
+
 def findMeanShape(shapeModelDir):
     fileList = sorted(glob.glob(shapeModelDir + '/*local.particles'))
     for i in range(len(fileList)):
@@ -330,6 +349,7 @@ def findMeanShape(shapeModelDir):
     nmMS = shapeModelDir + '/meanshape_local.particles'
     np.savetxt(nmMS, meanShape)
 
+
 def transformParticles(particles, transform, inverse=False):
     if inverse:
         transform = np.linalg.inv(transform)
@@ -339,6 +359,7 @@ def transformParticles(particles, transform, inverse=False):
     transformed_particles = np.array(transformed_particles)
     return transformed_particles
 
+
 def load_mesh(filename):
     accepted_extensions = ['.vtk', '.vtp', '.ply', '.obj', '.stl']
     extension = os.path.splitext(filename)[1]
@@ -346,3 +367,73 @@ def load_mesh(filename):
         return sw.Mesh(filename)
     else:
         return sw.Image(filename).toMesh(0.5)
+
+
+def initialize_mesh_warper(template_mesh, template_particles):
+    """
+    This function initializes a MeshWarper object using a template mesh and corresponding particles.
+
+    Parameters:
+    template_mesh_filename (shapeworks.Mesh): The template mesh.
+    template_particles (numpy_ndarray): The template particles.
+
+    Returns:
+    None
+    """
+    global sw_warp
+    sw_warp.generateWarp(template_mesh, template_particles)
+
+
+def initialize_mesh_warper_from_files(template_mesh_filename, template_particles):
+    """
+    This function initializes a MeshWarper object using a template mesh and corresponding particles.
+
+    Parameters:
+    template_mesh_filename (str): The filename of the template mesh.
+    template_particles (str): The filename of the corresponding particles.
+
+    Returns:
+    None
+    """
+    sw_mesh = get_mesh_from_file(template_mesh_filename)
+    sw_particles = np.loadtxt(template_particles)
+    initialize_mesh_warper(sw_mesh, sw_particles)
+
+
+def reconstruct_mesh(particles):
+    """
+    This function uses the global MeshWarper object to build a mesh from a given set of particles.
+
+    Parameters:
+    particles (numpy.ndarray): The particles to be used for mesh reconstruction.
+
+    Returns:
+    shapeworks.Mesh: The reconstructed mesh.
+
+    Raises:
+    Exception: If the MeshWarper object has not been initialized.
+    """
+    if sw_warp is None:
+        raise Exception("Mesh warper has not been initialized")
+    return sw_warp.buildMesh(particles)
+
+
+def get_mesh_from_DT(DT_list, mesh_dir):
+    if not os.path.exists(mesh_dir):
+        os.makedirs(mesh_dir)
+    mesh_files = []
+    for input_file in DT_list:
+        print('	' + get_prefix(input_file))
+        output_vtk = mesh_dir + "original_" + get_prefix(input_file) + ".vtk"
+        image = sw.Image(input_file)
+        image.toMesh(isovalue=0).write(output_vtk)
+        mesh_files.append(output_vtk)
+    return sorted(mesh_files)
+
+
+def get_mesh_from_file(filename, iso_value=0):
+    if filename.endswith('.nrrd'):
+        image = sw.Image(filename)
+        return image.toMesh(iso_value)
+    else:
+        return sw.Mesh(filename)
