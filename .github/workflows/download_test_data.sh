37873effb4bafc6ef73f057e185765598ddc031f
Alan Morris <akenmorris@gmail.com>
2024-06-18 11:44:16 -0600

    Try builds without remote host cache.

--- .github/workflows/download_test_data.sh
+++ .github/workflows/download_test_data.sh
@@ -17,7 +17,8 @@ cd /
 # Tiny test data downloads
 for run in {1..5}; do
     if [ ! -f /tmp/tiny_test_cache.tar.gz ] ; then
-	scp runner@${CACHE_HOST}:github/tiny_test_cache.tar.gz /tmp || true
+	cd /tmp
+	wget https://www.sci.utah.edu/~shapeworks/doc-resources/zips/tiny_test_cache.tar.gz || true
 	sleep 1
     fi
 done
