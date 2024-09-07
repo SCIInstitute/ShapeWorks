567bd1fd89bf5356458d809883dd3826d07d6673
Alan Morris <akenmorris@gmail.com>
2024-09-07 09:44:04 -0600

    Disabled python based sharedBoundaryExtractor test due to the fact that the output of the test is not deterministic across platforms (e.g. arm64), though the output is correct
    The C++ version of this had already been disabled.  The python one has been failing, but the end of the python script was not returning with that failure, so the test has been "passing"

--- Testing/PythonTests/shared_boundary.py
+++ Testing/PythonTests/shared_boundary.py
@@ -4,25 +4,20 @@ from shapeworks import *
 
 success = True
 
-def sharedBoundaryExtractor():
-	left = Mesh(os.environ["DATA"] + "/shared_boundary/00_l.vtk")
-	right = Mesh(os.environ["DATA"] + "/shared_boundary/00_r.vtk")
-	tol = 1e-3
-	out_left, out_right, out_shared = MeshUtils.sharedBoundaryExtractor(left,right,tol)
+# Disabled due to the fact that the output of the test is not deterministic across platforms (e.g. arm64), though the output is correct
+# def sharedBoundaryExtractor():
+# 	left = Mesh(os.environ["DATA"] + "/shared_boundary/00_l.vtk")
+# 	right = Mesh(os.environ["DATA"] + "/shared_boundary/00_r.vtk")
+# 	tol = 1e-3
+# 	out_left, out_right, out_shared = MeshUtils.sharedBoundaryExtractor(left,right,tol)
 
-	out_left.write("/tmp/out_left.vtk")
-	out_right.write("/tmp/out_right.vtk")
-	out_shared.write("/tmp/out_shared.vtk")
-	
- 
-	ground_truth_left = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_l.vtk")
-	ground_truth_right = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_r.vtk")
-	ground_truth_shared = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_s.vtk")
+# 	ground_truth_left = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_l.vtk")
+# 	ground_truth_right = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_r.vtk")
+# 	ground_truth_shared = Mesh(os.environ["DATA"] + "/shared_boundary/00_out_s.vtk")
 
-	return (ground_truth_left == out_left) and (ground_truth_right == out_right) and (ground_truth_shared == out_shared)
+# 	return (ground_truth_left == out_left) and (ground_truth_right == out_right) and (ground_truth_shared == out_shared)
 
-
-success &= utils.test(sharedBoundaryExtractor)
+#success &= utils.test(sharedBoundaryExtractor)
 
 def boundaryLoopExtractor():
 
