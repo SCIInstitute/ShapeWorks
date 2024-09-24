a025259133324b632d6e424abb01d88b666ff582
Alan Morris <akenmorris@gmail.com>
2024-09-24 13:23:47 -0600

    Add skeleton segmentation tool

---
+++ Studio/Data/SegmentationToolPanel.cpp
@@ -0,0 +1,51 @@
+// qt
+#include <QMessageBox>
+#include <QThread>
+
+// shapeworks
+#include <Data/Session.h>
+#include <Data/ShapeWorksWorker.h>
+#include <Data/Worker.h>
+#include <Interface/ShapeWorksStudioApp.h>
+#include <Interface/Style.h>
+#include <Job/GroupPvalueJob.h>
+#include <Job/ParticleNormalEvaluationJob.h>
+#include <Job/StatsGroupLDAJob.h>
+#include <Logging.h>
+#include <QMeshWarper.h>
+#include <Shape.h>
+#include <StudioMesh.h>
+#include <Utils/AnalysisUtils.h>
+#include <jkqtplotter/graphs/jkqtpboxplot.h>
+#include <jkqtplotter/graphs/jkqtpstatisticsadaptors.h>
+#include <jkqtplotter/jkqtplotter.h>
+#include <ui_SegmentationToolPanel.h>
+
+#include "SegmentationToolPanel.h"
+
+namespace shapeworks {
+
+//---------------------------------------------------------------------------
+SegmentationToolPanel::SegmentationToolPanel(QWidget* parent) : QWidget(parent), ui_(new Ui_SegmentationToolPanel) {
+  ui_->setupUi(this);
+
+  connect(ui_->open_button, &QPushButton::toggled, ui_->content, &QWidget::setVisible);
+  connect(ui_->header, &QPushButton::clicked, ui_->open_button, &QPushButton::toggle);
+
+  ui_->header_label->setAttribute(Qt::WA_TransparentForMouseEvents);
+  ui_->open_button->setChecked(false);
+}
+
+//---------------------------------------------------------------------------
+SegmentationToolPanel::~SegmentationToolPanel() {}
+
+//---------------------------------------------------------------------------
+void SegmentationToolPanel::set_session(QSharedPointer<Session> session) {
+  session_ = session;
+  reset();
+}
+
+//---------------------------------------------------------------------------
+void SegmentationToolPanel::reset() {}
+
+}  // namespace shapeworks
---
+++ Studio/Data/SegmentationToolPanel.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <Shape.h>
+
+#include <QSharedPointer>
+#include <QWidget>
+
+class Ui_SegmentationToolPanel;
+
+namespace shapeworks {
+
+class Session;
+class Lightbox;
+class ShapeWorksStudioApp;
+
+class SegmentationToolPanel : public QWidget {
+  Q_OBJECT;
+
+ public:
+  SegmentationToolPanel(QWidget* parent = 0);
+  ~SegmentationToolPanel();
+
+  //! set the pointer to the session
+  void set_session(QSharedPointer<Session> session);
+  void reset();
+
+ public Q_SLOTS:
+
+ Q_SIGNALS:
+
+  void update_view();
+
+ private:
+  QSharedPointer<Session> session_;
+
+  Ui_SegmentationToolPanel* ui_;
+};
+}  // namespace shapeworks
