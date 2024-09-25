594057797dfd628e817a0ff56e2459c79a02e6cd
nawazishkhan1-nk <nawazishkhan1.nk@gmail.com>
2024-09-24 23:59:04 -0600

    more util functions

--- Studio/ShapeWorksMONAI/MonaiLabelUtils.cpp
+++ Studio/ShapeWorksMONAI/MonaiLabelUtils.cpp
@@ -3,8 +3,12 @@
 #include <QDir>
 #include <QFile>
 #include <QString>
+#include <QFileInfo>
+#include <QSharedPointer>
 
+#include <Data/Session.h>
 
+using namespace shapeworks;
 
 namespace monailabel {
 
@@ -19,7 +23,26 @@ bool MonaiLabelUtils::copySegmentation(const QString& sourcePath,
   if (destFile.exists()) {
     destFile.remove();
   }
-  return QFile::copy(sourcePath, destinationPath); 
+  return QFile::copy(sourcePath, destinationPath);
+}
+
+bool MonaiLabelUtils::deleteTempFile(const QString& filePath) {
+  QFile file(filePath);
+  if (file.exists())
+    return file.remove();
+
+  else
+    return false;
+}
+
+//---------------------------------------------------------------------------
+std::string MonaiLabelUtils::getFeatureName(QSharedPointer<Session> session) {
+  std::string feature_name = session->get_image_name();
+  auto image_names = session->get_project()->get_image_names();
+  if (image_names.size() > 0) {
+    feature_name = image_names[0];
+  }
+  return feature_name;
 }
 
 }  // namespace monailabel
--- Studio/ShapeWorksMONAI/MonaiLabelUtils.h
+++ Studio/ShapeWorksMONAI/MonaiLabelUtils.h
@@ -1,15 +1,51 @@
 #pragma once
 
+#include <QLineEdit>
 #include <QString>
+#include <QUrl>
+#include <QWidget>
+#include <QValidator>
+#include <QSharedPointer>
+#include <Data/Session.h>
 
+namespace shapeworks {
+    class Session;
+}
 namespace monailabel {
+    
+class UrlValidator : public QValidator {
+ public:
+  UrlValidator(QObject* parent = nullptr) : QValidator(parent) {}
 
+  QValidator::State validate(QString& input, int& pos) const override {
+    Q_UNUSED(pos);
+    QUrl url(input);
+    if (!url.isValid() || (url.scheme() != "http" && url.scheme() != "https")) {
+      return QValidator::Invalid;
+    }
+    // Allow '0.0.0.0'
+    if (url.host() == "0.0.0.0") {
+      if (url.port() == -1) {
+        return QValidator::Invalid;
+      }
+      return QValidator::Acceptable;
+    }
+    if (url.host().isEmpty()) {
+      return QValidator::Invalid;
+    }
+    if (url.port() == -1) {
+      return QValidator::Invalid;
+    }
+    return QValidator::Acceptable;
+  }
+};
 class MonaiLabelUtils {
  public:
   static bool createDir(const QString& dirPath);
   static bool copySegmentation(const QString& sourcePath,
                                const QString& destinationPath);
-//   static std::string getFeatureName();
-//   static std::string setSubjectImageFileName();
+  static bool deleteTempFile(const QString& filePath);
+  static std::string getFeatureName(QSharedPointer<shapeworks::Session> session);
 };
+
 }  // namespace monailabel
