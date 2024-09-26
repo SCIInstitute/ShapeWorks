dc0ce03034d4075a914b8c7261ceccac5d55858d
Alan Morris <akenmorris@gmail.com>
2024-09-26 01:09:45 -0600

    Reformat

--- Libs/Image/ImageUtils.cpp
+++ Libs/Image/ImageUtils.cpp
@@ -4,21 +4,16 @@
 
 namespace shapeworks {
 
-PhysicalRegion ImageUtils::boundingBox(const std::vector<std::string>& filenames,
-                                       Image::PixelType isoValue)
-{
-  if (filenames.empty())
-    throw std::invalid_argument("No filenames provided to compute a bounding box");
+PhysicalRegion ImageUtils::boundingBox(const std::vector<std::string>& filenames, Image::PixelType isoValue) {
+  if (filenames.empty()) throw std::invalid_argument("No filenames provided to compute a bounding box");
 
   Image img(filenames[0]);
   PhysicalRegion bbox;
-  Dims dims(img.dims()); // images must all be the same size
+  Dims dims(img.dims());  // images must all be the same size
 
-  for (auto filename : filenames)
-  {
+  for (auto filename : filenames) {
     Image img(filename);
-    if (img.dims() != dims)
-    {
+    if (img.dims() != dims) {
       throw std::invalid_argument("Image sizes do not match (" + filename + ")");
     }
 
@@ -29,18 +24,14 @@ PhysicalRegion ImageUtils::boundingBox(const std::vector<std::string>& filenames
 }
 
 PhysicalRegion ImageUtils::boundingBox(const std::vector<std::reference_wrapper<const Image>>& images,
-                                       Image::PixelType isoValue)
-{
-  if (images.empty())
-    throw std::invalid_argument("No images provided to compute a bounding box");
+                                       Image::PixelType isoValue) {
+  if (images.empty()) throw std::invalid_argument("No images provided to compute a bounding box");
 
   PhysicalRegion bbox;
-  Dims dims(images[0].get().dims()); // images must all be the same size
+  Dims dims(images[0].get().dims());  // images must all be the same size
 
-  for (auto img : images)
-  {
-    if (img.get().dims() != dims)
-      throw std::invalid_argument("Image sizes do not match");
+  for (auto img : images) {
+    if (img.get().dims() != dims) throw std::invalid_argument("Image sizes do not match");
 
     bbox.expand(img.get().physicalBoundingBox(isoValue));
   }
@@ -50,8 +41,7 @@ PhysicalRegion ImageUtils::boundingBox(const std::vector<std::reference_wrapper<
 
 ImageUtils::TPSTransform::Pointer ImageUtils::createWarpTransform(const std::string& source_landmarks_file,
                                                                   const std::string& target_landmarks_file,
-                                                                  const int stride)
-{ 
+                                                                  const int stride) {
   typedef TPSTransform::PointSetType PointSet;
 
   // Read the source and target sets of landmark points
@@ -64,7 +54,7 @@ ImageUtils::TPSTransform::Pointer ImageUtils::createWarpTransform(const std::str
   std::ifstream intargetfile;
   insourcefile.open(source_landmarks_file);
   intargetfile.open(target_landmarks_file);
-  if (!insourcefile.is_open() || !intargetfile.is_open())  {
+  if (!insourcefile.is_open() || !intargetfile.is_open()) {
     throw std::invalid_argument("Error: unable to open landmark files. Check paths.");
   }
 
@@ -72,16 +62,14 @@ ImageUtils::TPSTransform::Pointer ImageUtils::createWarpTransform(const std::str
   Point3 src, tgt;
   int count{0};
 
-  while (!insourcefile.eof() && !intargetfile.eof())
-  {
+  while (!insourcefile.eof() && !intargetfile.eof()) {
     insourcefile >> src[0] >> src[1] >> src[2];
     intargetfile >> tgt[0] >> tgt[1] >> tgt[2];
 
-    if (count % stride == 0)
-    {
+    if (count % stride == 0) {
       // swap src and tgt b/c ITK transforms must be inverted on creation since some do not provide an invert function
-      sourceLandMarkContainer->InsertElement( id, tgt );
-      targetLandMarkContainer->InsertElement( id, src );
+      sourceLandMarkContainer->InsertElement(id, tgt);
+      targetLandMarkContainer->InsertElement(id, src);
       id++;
     }
     count++;
@@ -98,4 +86,4 @@ ImageUtils::TPSTransform::Pointer ImageUtils::createWarpTransform(const std::str
   return tps;
 }
 
-} //shapeworks
+}  // namespace shapeworks
