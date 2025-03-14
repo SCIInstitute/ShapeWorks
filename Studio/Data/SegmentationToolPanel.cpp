78d2bfa3aa0aa04ae5cc6bc34c53262e7879783c
Alan Morris <akenmorris@gmail.com>
2025-03-14 12:07:43 -0600

    Hide include/exclude for 6.6 release (#2356)

--- Studio/Data/SegmentationToolPanel.cpp
+++ Studio/Data/SegmentationToolPanel.cpp
@@ -42,8 +42,8 @@ SegmentationToolPanel::SegmentationToolPanel(QWidget* parent) : QWidget(parent),
   ui_->open_button->setChecked(false);
 
   // for 6.6 release we don't have include/exclude
-  //ui_->included_mode_->hide();
-  //ui_->excluded_mode_->hide();
+  ui_->included_mode_->hide();
+  ui_->excluded_mode_->hide();
 }
 
 //---------------------------------------------------------------------------
