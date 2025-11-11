b873e10dee8bdbd59771feecfa728a2eb4452e39
Alan Morris <akenmorris@gmail.com>
2025-11-11 10:25:19 -0700

    Updating style of GradientDescentOptimizer

--- Libs/Optimize/GradientDescentOptimizer.cpp
+++ Libs/Optimize/GradientDescentOptimizer.cpp
@@ -2,8 +2,6 @@
 
 #include "GradientDescentOptimizer.h"
 
-const int global_iteration = 1;
-
 #include <Logging.h>
 #include <Profiling.h>
 #include <tbb/parallel_for.h>
@@ -21,9 +19,13 @@ const int global_iteration = 1;
 #include "Libs/Optimize/Utils/MemoryUsage.h"
 
 namespace shapeworks {
+
+const int GLOBAL_ITERATION = 1;
+
+//---------------------------------------------------------------------------
 GradientDescentOptimizer::GradientDescentOptimizer() : early_stopping_() {}
 
-//---------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 void GradientDescentOptimizer::reset_time_step_vectors() {
   // Make sure the time step vector is the right size
   while (time_steps_.size() != particle_system_->GetNumberOfDomains()) {
@@ -43,14 +45,14 @@ void GradientDescentOptimizer::reset_time_step_vectors() {
   }
 }
 
-//---------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 void GradientDescentOptimizer::set_early_stopping_config(const EarlyStoppingConfig& config) {
   early_stopping_.SetConfigParams(config.frequency, config.window_size, config.threshold, config.strategy,
                                   config.ema_alpha, config.enable_logging, config.logger_name, config.warmup_iters);
   early_stopping_enabled_ = config.enabled;
 }
 
-//---------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 void GradientDescentOptimizer::initialize_early_stopping_score_function(const ParticleSystem* p) {
   bool early_stopping_status = early_stopping_.SetControlShapes(p);
   if (early_stopping_status == false) {
@@ -63,7 +65,7 @@ void GradientDescentOptimizer::initialize_early_stopping_score_function(const Pa
   early_stopping_enabled_ = early_stopping_status;
 }
 
-//---------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 void GradientDescentOptimizer::start_adaptive_gauss_seidel_optimization() {
   TIME_SCOPE("GradientDescentOptimizer");
   /// uncomment this to run single threaded
@@ -84,27 +86,30 @@ void GradientDescentOptimizer::start_adaptive_gauss_seidel_optimization() {
   // gradient_function_->SetParticleSystem(particle_system_);
 
   reset_time_step_vectors();
-  double minimumTimeStep = 1.0;
+  double minimum_time_step = 1.0;
 
   unsigned int num_domains = particle_system_->GetNumberOfDomains();
 
   unsigned int counter = 0;
 
   double max_change = 0.0;
-  if (early_stopping_enabled_) early_stopping_.reset();  // reset early stopping cache before starting optimization
-  while (stop_optimization_ == false)                    // iterations loop
-  {
+  if (early_stopping_enabled_) {
+    early_stopping_.reset();  // reset early stopping cache before starting optimization
+  }
+  while (stop_optimization_ == false) {
+    // iterations loop
     TIME_SCOPE("optimizer_iteration");
     double dampening = 1;
-    int startDampening = max_iterations_ / 2;
-    if (number_of_iterations_ > startDampening) {
-      dampening = exp(-double(number_of_iterations_ - startDampening) * 5.0 / double(max_iterations_ - startDampening));
+    int start_dampening = max_iterations_ / 2;
+    if (number_of_iterations_ > start_dampening) {
+      dampening =
+          exp(-double(number_of_iterations_ - start_dampening) * 5.0 / double(max_iterations_ - start_dampening));
     }
-    minimumTimeStep = dampening;
+    minimum_time_step = dampening;
 
     max_change = 0.0;
 
-    const auto accTimerBegin = std::chrono::steady_clock::now();
+    const auto acc_timer_begin = std::chrono::steady_clock::now();
     gradient_function_->SetParticleSystem(particle_system_.GetPointer());
     if (early_stopping_enabled_ && !early_stopping_score_ready_) {
       bool early_stopping_status = early_stopping_.SetControlShapes(particle_system_.GetPointer());
@@ -118,126 +123,134 @@ void GradientDescentOptimizer::start_adaptive_gauss_seidel_optimization() {
     }
 
     TIME_START("gradient_before_iteration");
-    if (counter % global_iteration == 0) gradient_function_->BeforeIteration();
+    if (counter % GLOBAL_ITERATION == 0) {
+      gradient_function_->BeforeIteration();
+    }
     TIME_STOP("gradient_before_iteration");
     counter++;
 
     TIME_START("parallel_sampling");
     // Iterate over each domain
     const auto domains_per_shape = particle_system_->GetDomainsPerShape();
-    tbb::parallel_for(tbb::blocked_range<size_t>{0, num_domains / domains_per_shape},
-                      [&](const tbb::blocked_range<size_t>& r) {
-                        for (size_t shape = r.begin(); shape < r.end(); ++shape) {
-                          for (int shape_dom_idx = 0; shape_dom_idx < domains_per_shape; shape_dom_idx++) {
-                            auto dom = shape * domains_per_shape + shape_dom_idx;
+    tbb::parallel_for(
+        tbb::blocked_range<size_t>{0, num_domains / domains_per_shape}, [&](const tbb::blocked_range<size_t>& r) {
+          for (size_t shape = r.begin(); shape < r.end(); ++shape) {
+            for (int shape_dom_idx = 0; shape_dom_idx < domains_per_shape; shape_dom_idx++) {
+              auto dom = shape * domains_per_shape + shape_dom_idx;
 
-                            // skip any flagged domains
-                            if (particle_system_->GetDomainFlag(dom) == true) {
-                              // note that this is really a 'continue' statement for the loop, but using TBB,
-                              // we are in an anonymous function, not a loop, so return is equivalent to continue here
-                              return;
-                            }
+              // skip any flagged domains
+              if (particle_system_->GetDomainFlag(dom) == true) {
+                // note that this is really a 'continue' statement for the loop, but using TBB,
+                // we are in an anonymous function, not a loop, so return is equivalent to continue here
+                return;
+              }
 
-                            const ParticleDomain* domain = particle_system_->GetDomain(dom);
+              const ParticleDomain* domain = particle_system_->GetDomain(dom);
 
-                            // must clone this as we are in a thread and the gradient function is not thread-safe
-                            VectorFunction::Pointer localGradientFunction = gradient_function_->Clone();
+              // must clone this as we are in a thread and the gradient function is not thread-safe
+              VectorFunction::Pointer local_gradient_function = gradient_function_->Clone();
 
-                            // Tell function which domain we are working on.
-                            localGradientFunction->SetDomainNumber(dom);
+              // Tell function which domain we are working on.
+              local_gradient_function->SetDomainNumber(dom);
 
-                            // Iterate over each particle position
-                            for (auto k = 0; k < particle_system_->GetPositions(dom)->GetSize(); k++) {
-                              if (time_steps_[dom][k] < minimumTimeStep) {
-                                time_steps_[dom][k] = minimumTimeStep;
-                              }
-                              // Compute gradient update.
-                              double energy = 0.0;
-                              localGradientFunction->BeforeEvaluate(k, dom, particle_system_.GetPointer());
-                              // maximumUpdateAllowed is set based on some fraction of the distance between particles
-                              // This is to avoid particles shooting past their neighbors
-                              double maximumUpdateAllowed;
-                              VectorType original_gradient = localGradientFunction->Evaluate(
-                                  k, dom, particle_system_.GetPointer(), maximumUpdateAllowed, energy);
+              // Iterate over each particle position
+              for (auto k = 0; k < particle_system_->GetPositions(dom)->GetSize(); k++) {
+                if (time_steps_[dom][k] < minimum_time_step) {
+                  time_steps_[dom][k] = minimum_time_step;
+                }
 
-                              PointType pt = particle_system_->GetPositions(dom)->Get(k);
+                // Compute gradient update.
+                double energy = 0.0;
+                local_gradient_function->BeforeEvaluate(k, dom, particle_system_.GetPointer());
+                // maximum_update_allowed is set based on some fraction of the distance between particles
+                // This is to avoid particles shooting past their neighbors
+                double maximum_update_allowed;
+                VectorType original_gradient = local_gradient_function->Evaluate(k, dom, particle_system_.GetPointer(),
+                                                                                 maximum_update_allowed, energy);
 
-                              // Step 1 Project the gradient vector onto the tangent plane
-                              VectorType original_gradient_projectedOntoTangentSpace =
-                                  domain->ProjectVectorToSurfaceTangent(original_gradient, pt, k);
+                PointType pt = particle_system_->GetPositions(dom)->Get(k);
 
-                              double newenergy, gradmag;
-                              while (true) {
-                                // Step A scale the projected gradient by the current time step
-                                VectorType gradient = original_gradient_projectedOntoTangentSpace * time_steps_[dom][k];
+                // Step 1 Project the gradient vector onto the tangent plane
+                VectorType original_gradient_projected_onto_tangent_space =
+                    domain->ProjectVectorToSurfaceTangent(original_gradient, pt, k);
 
-                                // Step B Constrain the gradient so that the resulting position will not violate any
-                                // domain constraints
-                                if (particle_system_->GetDomain(dom)->GetConstraints()->GetActive()) {
-                                  augmented_lagrangian_constraints(gradient, pt, dom, maximumUpdateAllowed, k);
-                                }
+                double new_energy, grad_mag;
+                while (true) {
+                  // Step A scale the projected gradient by the current time step
+                  VectorType gradient = original_gradient_projected_onto_tangent_space * time_steps_[dom][k];
 
-                                gradmag = gradient.magnitude();
+                  // Step B Constrain the gradient so that the resulting position will not violate any
+                  // domain constraints
+                  if (particle_system_->GetDomain(dom)->GetConstraints()->GetActive()) {
+                    augmented_lagrangian_constraints(gradient, pt, dom, maximum_update_allowed, k);
+                  }
 
-                                // Step C if the magnitude is larger than the Sampler allows, scale the gradient down to
-                                // an acceptable magnitude
-                                if (gradmag > maximumUpdateAllowed) {
-                                  gradient = gradient * maximumUpdateAllowed / gradmag;
-                                  gradmag = gradient.magnitude();
-                                }
+                  grad_mag = gradient.magnitude();
 
-                                // Step D compute the new point position
-                                PointType newpoint = domain->UpdateParticlePosition(pt, k, gradient);
+                  // Step C if the magnitude is larger than the Sampler allows, scale the gradient down to
+                  // an acceptable magnitude
+                  if (grad_mag > maximum_update_allowed) {
+                    gradient = gradient * maximum_update_allowed / grad_mag;
+                    grad_mag = gradient.magnitude();
+                  }
 
-                                // Step F update the point position in the particle system
-                                particle_system_->SetPosition(newpoint, k, dom);
+                  // Step D compute the new point position
+                  PointType new_point = domain->UpdateParticlePosition(pt, k, gradient);
 
-                                // Step G compute the new energy of the particle system
-                                newenergy = localGradientFunction->Energy(k, dom, particle_system_.GetPointer());
+                  // Step F update the point position in the particle system
+                  particle_system_->SetPosition(new_point, k, dom);
 
-                                if (newenergy < energy)  // good move, increase timestep for next time
-                                {
-                                  time_steps_[dom][k] *= factor;
-                                  if (gradmag > max_change) max_change = gradmag;
-                                  break;
-                                } else {  // bad move, reset point position and back off on timestep
-                                  if (time_steps_[dom][k] > minimumTimeStep) {
-                                    domain->ApplyConstraints(pt, k);
-                                    particle_system_->SetPosition(pt, k, dom);
-                                    domain->InvalidateParticlePosition(k);
+                  // Step G compute the new energy of the particle system
+                  new_energy = local_gradient_function->Energy(k, dom, particle_system_.GetPointer());
 
-                                    time_steps_[dom][k] /= factor;
-                                  } else  // keep the move with timestep 1.0 anyway
-                                  {
-                                    if (gradmag > max_change) max_change = gradmag;
-                                    break;
-                                  }
-                                }
-                              }  // end while(true)
-                            }  // for each particle
-                          }
-                        }  // for each domain
-                      });
+                  if (new_energy < energy) {
+                    // good move, increase timestep for next time
+                    time_steps_[dom][k] *= factor;
+                    if (grad_mag > max_change) {
+                      max_change = grad_mag;
+                    }
+                    break;
+                  } else {  // bad move, reset point position and back off on timestep
+                    if (time_steps_[dom][k] > minimum_time_step) {
+                      domain->ApplyConstraints(pt, k);
+                      particle_system_->SetPosition(pt, k, dom);
+                      domain->InvalidateParticlePosition(k);
+
+                      time_steps_[dom][k] /= factor;
+                    } else {
+                      // keep the move with timestep 1.0 anyway
+                      if (grad_mag > max_change) {
+                        max_change = grad_mag;
+                      }
+                      break;
+                    }
+                  }
+                }  // end while(true)
+              }    // for each particle
+            }
+          }  // for each domain
+        });
 
     TIME_STOP("parallel_sampling");
     number_of_iterations_++;
     gradient_function_->AfterIteration();
 
-    const auto accTimerEnd = std::chrono::steady_clock::now();
-    const auto msElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(accTimerEnd - accTimerBegin).count();
+    const auto acc_timer_end = std::chrono::steady_clock::now();
+    const auto ms_elapsed =
+        std::chrono::duration_cast<std::chrono::milliseconds>(acc_timer_end - acc_timer_begin).count();
 
     if (verbosity_ > 2) {
-      std::cout << number_of_iterations_ << ". " << msElapsed << "ms";
+      std::cout << number_of_iterations_ << ". " << ms_elapsed << "ms";
 #ifdef LOG_MEMORY_USAGE
-      double vmUsage, residentSet;
-      process_mem_usage(vmUsage, residentSet);
-      std::cout << " | Mem=" << residentSet << "KB";
+      double vm_usage, resident_set;
+      process_mem_usage(vm_usage, resident_set);
+      std::cout << " | Mem=" << resident_set << "KB";
 #endif
       std::cout << std::endl;
     } else if (verbosity_ > 1) {
       if (number_of_iterations_ % (max_iterations_ / 10) == 0) {
         std::cerr << "Iteration " << number_of_iterations_ << ", maxchange = " << max_change
-                  << ", minimumTimeStep = " << minimumTimeStep << std::endl;
+                  << ", minimum_time_step = " << minimum_time_step << std::endl;
       }
     }
 
@@ -270,35 +283,35 @@ void GradientDescentOptimizer::start_adaptive_gauss_seidel_optimization() {
   }  // end while stop optimization
 }
 
-//---------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 void GradientDescentOptimizer::augmented_lagrangian_constraints(VectorType& gradient, const PointType& pt,
                                                                 const size_t& dom, const double& maximum_update_allowed,
                                                                 size_t index) {
   // Step B 2: Augmented lagrangian constraint method
-  double gradmag = gradient.magnitude();
+  double grad_mag = gradient.magnitude();
 
-  if (gradmag > maximum_update_allowed) {
-    gradient = gradient * maximum_update_allowed / gradmag;
-    gradmag = gradient.magnitude();
+  if (grad_mag > maximum_update_allowed) {
+    gradient = gradient * maximum_update_allowed / grad_mag;
+    grad_mag = gradient.magnitude();
   }
 
-  PointType upd_pt;
+  PointType updated_pt;
   for (size_t n = 0; n < Dimension; n++) {
-    upd_pt[n] = pt[n] - gradient[n];
+    updated_pt[n] = pt[n] - gradient[n];
   }
 
   double c = 1e0;
   double multiplier = 2;
   VectorType constraint_energy =
-      particle_system_->GetDomain(dom)->GetConstraints()->constraintsLagrangianGradient(upd_pt, pt, c, index);
-  if (constraint_energy.magnitude() > multiplier * gradmag) {
-    constraint_energy *= multiplier * gradmag / constraint_energy.magnitude();
+      particle_system_->GetDomain(dom)->GetConstraints()->constraintsLagrangianGradient(updated_pt, pt, c, index);
+  if (constraint_energy.magnitude() > multiplier * grad_mag) {
+    constraint_energy *= multiplier * grad_mag / constraint_energy.magnitude();
   }
   // particle_system_->GetDomain(dom)->GetConstraints()->ViolationReport(pt);
   for (size_t n = 0; n < Dimension; n++) {
     gradient[n] += constraint_energy[n];
   }
-  particle_system_->GetDomain(dom)->GetConstraints()->UpdateMus(upd_pt, c, index);
+  particle_system_->GetDomain(dom)->GetConstraints()->UpdateMus(updated_pt, c, index);
 }
 
 }  // namespace shapeworks
--- Libs/Optimize/GradientDescentOptimizer.h
+++ Libs/Optimize/GradientDescentOptimizer.h
@@ -12,27 +12,27 @@
 namespace shapeworks {
 
 /**
- * @class ParticleGradientDescentPositionOptimizer
+ * @class GradientDescentOptimizer
+ * @brief Optimizes particle system positions using gradient descent
  *
  * This class optimizes a list of particle system positions with respect to a
- * specified energy function using a simple gradient descent strategy.  A
+ * specified energy function using a simple gradient descent strategy. A
  * function which computes the gradient of the function with respect to
- * particle position must be specified.  The optimization performs Jacobi
+ * particle position must be specified. The optimization performs Jacobi
  * updates (each particle position is changed as soon as its new position is
  * computed).
- *
  */
 class GradientDescentOptimizer {
  public:
   static constexpr unsigned int Dimension = 3;
 
-  /** Numerical vector type. */
+  /// Numerical vector type
   using VectorType = VectorFunction::VectorType;
 
-  /** Point Type */
+  /// Point type
   using PointType = ParticleSystem::PointType;
 
-  /** Start the optimization. */
+  /// Start the optimization
   void start_optimization() { start_adaptive_gauss_seidel_optimization(); }
   void start_adaptive_gauss_seidel_optimization();
   void set_early_stopping_config(const EarlyStoppingConfig& config);
@@ -41,8 +41,8 @@ class GradientDescentOptimizer {
   void augmented_lagrangian_constraints(VectorType& gradient, const PointType& pt, const size_t& dom,
                                         const double& maximum_update_allowed, size_t index);
 
-  /** Stop the optimization.  This method sets a flag that aborts the
-      start_optimization method after the current iteration. */
+  /// Stop the optimization
+  /// @brief Sets a flag that aborts the start_optimization method after the current iteration
   void stop_optimization() { stop_optimization_ = true; }
 
   void abort_processing() {
@@ -50,34 +50,53 @@ class GradientDescentOptimizer {
     abort_processing_ = true;
   }
 
-  /** Get/Set the number of iterations performed by the solver. */
+  /// Get the number of iterations performed by the solver
   unsigned int get_number_of_iterations() const { return number_of_iterations_; }
+
+  /// Set the number of iterations performed by the solver
   void set_number_of_iterations(unsigned int val) { number_of_iterations_ = val; }
 
+  /// Set the verbosity level
   void set_verbosity(unsigned int val) { verbosity_ = val; }
+
+  /// Get the verbosity level
   unsigned int get_verbosity() const { return verbosity_; }
 
-  /** Get/Set a time step parameter for the update.  Each update is simply
-      scaled by this value. */
+  /// Get the time step parameter for the update
+  /// @brief Each update is scaled by this value
   double get_time_step() const { return time_step_; }
+
+  /// Set the time step parameter for the update
   void set_time_step(double val) { time_step_ = val; }
 
-  /** Get/Set the maximum iterations to allow this solver to use. */
+  /// Get the maximum number of iterations allowed
   unsigned int get_maximum_number_of_iterations() const { return max_iterations_; }
+
+  /// Set the maximum number of iterations allowed
   void set_maximum_number_of_iterations(unsigned int val) { max_iterations_ = val; }
 
-  /** Get/Set the precision of the solution. */
+  /// Get the precision/tolerance of the solution
   double get_tolerance() const { return tolerance_; }
+
+  /// Set the precision/tolerance of the solution
   void set_tolerance(double val) { tolerance_ = val; }
 
-  /** Get/Set the ParticleSystem modified by this optimizer. */
+  /// Get the ParticleSystem modified by this optimizer
   ParticleSystem* get_particle_system() { return particle_system_.GetPointer(); }
+
+  /// Get the ParticleSystem modified by this optimizer (const version)
   const ParticleSystem* get_particle_system() const { return particle_system_.GetPointer(); }
+
+  /// Set the ParticleSystem modified by this optimizer
   void set_particle_system(ParticleSystem* val) { particle_system_ = val; }
 
-  /** Get/Set the gradient function used by this optimizer. */
+  /// Get the gradient function used by this optimizer
   VectorFunction* get_gradient_function() { return gradient_function_.GetPointer(); }
+
+  /// Get the gradient function used by this optimizer (const version)
   const VectorFunction* get_gradient_function() const { return gradient_function_.GetPointer(); }
+
+  /// Set the gradient function used by this optimizer
   void set_gradient_function(VectorFunction* val) { gradient_function_ = val; }
 
   /// Determines if this is an initialization (true) or an optimization (false)
@@ -95,13 +114,17 @@ class GradientDescentOptimizer {
   /// Get the current iteration callback
   std::function<void()> get_iteration_callback() const { return iteration_callback_; }
 
-  // Constructor and destructor are now public for std::shared_ptr usage
+  /// Constructor
   GradientDescentOptimizer();
+
+  /// Destructor
   ~GradientDescentOptimizer() = default;
 
  private:
-  // Disable copy and assignment
+  /// Disable copy constructor
   GradientDescentOptimizer(const GradientDescentOptimizer&) = delete;
+
+  /// Disable assignment operator
   GradientDescentOptimizer& operator=(const GradientDescentOptimizer&) = delete;
 
   void reset_time_step_vectors();
